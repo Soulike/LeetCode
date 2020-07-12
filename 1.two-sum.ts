d317f9af87e9f741c4eb02a65838cf579f642294
Soulike <soulike@qq.com>
2020-07-12 21:16:49 +0800

    1

---
+++ 1.two-sum.ts
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=1 lang=typescript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[]
+{
+    // brute force
+    for (let i = 0; i < nums.length - 1; i++)
+    {
+        for (let j = i + 1; j < nums.length; j++)
+        {
+            if (nums[i] + nums[j] === target)
+            {
+                return [i, j];
+            }
+        }
+    }
+    return [];  // typescript requirement
+};
+// @lc code=end
+
