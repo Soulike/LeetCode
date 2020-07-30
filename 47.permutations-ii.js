746fc5a6ad73050889faa8f501de584550578904
Soulike <soulike@qq.com>
2020-07-30 09:11:23 +0800

    47

---
+++ 47.permutations-ii.js
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=47 lang=javascript
+ *
+ * [47] Permutations II
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function (nums)
+{
+    nums.sort((a, b) => a - b);
+    const numsLength = nums.length;
+    if (numsLength === 0)
+    {
+        return [];
+    }
+    if (numsLength === 1)
+    {
+        return [nums];
+    }
+    const results = [];
+    let subResults = [];
+    for (let i = 0; i < numsLength; i++)
+    {
+        if (i !== 0 && nums[i] === nums[i - 1])
+        {
+            continue;
+        }
+        subResults = permuteUnique([...nums.slice(0, i), ...nums.slice(i + 1)]);
+        for (const subResult of subResults)
+        {
+            results.push([
+                nums[i], ...subResult
+            ]);
+        }
+    }
+    return results;
+};
+// @lc code=end
+
