68b23db4ff23f09c30dd8253a96b0ec9ec3bbb82
Soulike <soulike@qq.com>
2020-07-31 10:34:55 +0800

    55

--- 55.jump-game.js
+++ 55.jump-game.js
@@ -12,23 +12,30 @@
 const canJump = function (nums) 
 {
     const numsLength = nums.length;
-    for (let i = 0;nums[i]+i < numsLength - 1;)
+    let currentNum = 0;
+    let farestIndex = 0;
+    for (let i = 0; ;)
     {
-        if (nums[i] === 0)
+        currentNum = nums[i];
+        farestIndex = currentNum + i;
+        if (farestIndex >= numsLength - 1)
+        {
+            return true;
+        }
+        if (currentNum === 0)
         {
             return false;
         }
         for (let j = i + 1; j < numsLength; j++)
         {
-            if (j + nums[j] >= i + nums[i])
+            if (j + nums[j] >= farestIndex)
             {
                 i = j;
                 break;
             }
         }
     }
-    return true;
 };
 // @lc code=end
 
-console.log(canJump([2,3,1,1,1,1,1,1,4]))
+console.log(canJump([2, 3, 1, 1, 1, 1, 1, 1, 4]));
