a131fd696bb1de246eb48bce1692d109d4508a72
Soulike <soulike@qq.com>
2021-07-26 09:53:03 +0800

    198-2

--- 198.house-robber.js
+++ 198.house-robber.js
@@ -16,42 +16,17 @@ const rob = function (nums)
     {
         return nums[0];
     }
-    /** 
-    * 从下标 i 位置的房屋开始抢，最多抢到多少钱
-    * @type {number[]} */
-    const cache = [];
-    return Math.max(
-        robHelper(nums, 0, cache),
-        robHelper(nums, 1, cache));
+    // 从 0 到 i 可以抢到的最大值
+    const dp = [
+        nums[0],
+        Math.max(nums[0], nums[1]),
+    ];
+    for (let i = 2; i < nums.length; i++)
+    {
+        const rob = nums[i] + dp[i - 2];
+        const noRob = dp[i - 1];
+        dp[i] = Math.max(rob, noRob);
+    }
+    return dp[dp.length - 1];
 };
-
-/**
- * @param {number[]} nums
- * @param {number} left - 开始抢劫的房屋下标
- * @param {number[]} cache
- * @return {number}
- */
-function robHelper(nums, left, cache)
-{
-    const LENGTH = nums.length;
-    if (left === LENGTH - 1)
-    {
-        cache[left] = nums[LENGTH - 1];
-        return nums[LENGTH - 1];
-    }
-    if (cache[left])
-    {
-        return cache[left];
-    }
-    let max = 0;
-    for (let i = left + 2; i < LENGTH; i++)
-    {
-        if (nums[i] !== 0)
-        {
-            max = Math.max(max, robHelper(nums, i, cache));
-        }
-    }
-    cache[left] = max + nums[left];
-    return max + nums[left];
-}
 // @lc code=end
