437fb357ff71debf7ca42904ae441a2524bee8e8
Soulike <soulike@qq.com>
2021-07-31 09:17:09 +0800

    647-2

--- 647.palindromic-substrings.js
+++ 647.palindromic-substrings.js
@@ -11,38 +11,47 @@
  */
 const countSubstrings = function (s) 
 {
-    let count = 0;
-    let nextOdd = true;
-    let nextEven = true;
-    for (let i = 0; i < s.length; i++)
+    const LENGTH = s.length;
+    /**
+     * 从 s[i] 到 s[j] 是否是回文
+     * @type {boolean[][]} */
+    const dp = new Array(LENGTH);
+    for (let i = 0; i < LENGTH; i++)
     {
-        nextOdd = true;
-        nextEven = true;
-        for (let j = 0; i - j >= 0 && i + j < s.length; j++)
+        dp[i] = new Array(LENGTH);
+        dp[i].fill(false);
+    }
+
+    let count = 0;
+    for (let i = LENGTH-1; i >= 0; i--)
+    {
+        for (let j = i; j < LENGTH; j++)
         {
-            if (nextOdd && s.charAt(i - j) === s.charAt(i + j))
+            if (s[i] === s[j])
             {
-                count++;
-                nextOdd = true;
-            }
-            else
-            {
-                nextOdd = false;
-            }
-
-            if (i + j + 1 < s.length && s.charAt(i - j) === s.charAt(i + j + 1) && nextEven)
-            {
-                nextEven = true;
-                count++;
-            }
-            else
-            {
-                nextEven = false;
-            }
-
-            if (!nextEven && !nextOdd)
-            {
-                break;
+                if (i === j)
+                {
+                    count++;
+                    dp[i][j] = true;
+                }
+                else
+                {
+                    if (j === i + 1)
+                    {
+                        count++;
+                        dp[i][j] = true;
+                    }
+                    if (j === i + 2)
+                    {
+                        count++;
+                        dp[i][j] = true;
+                    }
+                    else if (dp[i + 1][j - 1] === true)
+                    {
+                        count++;
+                        dp[i][j] = true;
+                    }
+                }
             }
         }
     }
@@ -50,3 +59,4 @@ const countSubstrings = function (s)
 };
 // @lc code=end
 
+console.log(countSubstrings('aaaaa'))
