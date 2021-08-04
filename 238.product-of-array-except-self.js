f25f3c3a9af07d3929506f9d3e83e4230c7777a3
Soulike <soulike@qq.com>
2021-08-04 09:38:20 +0800

    238-2

--- 238.product-of-array-except-self.js
+++ 238.product-of-array-except-self.js
@@ -12,28 +12,19 @@
 const productExceptSelf = function (nums) 
 {
     const LENGTH = nums.length;
-    /** 下标 i （不含）之前所有数的乘积 */
-    const beforeProduct = new Array(LENGTH);
-    beforeProduct[0] = 1;
-    /** 下标 i （不含）之后所有数的乘积 */
-    const afterProduct = new Array(LENGTH);
-    afterProduct[LENGTH - 1] = 1;
+    const output = new Array(LENGTH);
+    output.fill(1);
 
     for (let i = 0; i < LENGTH - 1; i++)
     {
-        beforeProduct[i + 1] = beforeProduct[i] * nums[i];
+        output[i + 1] = output[i] * nums[i];
     }
 
-    for (let i = LENGTH - 1; i >= 1; i--)
+    let rightProduct = 1;
+    for (let i = LENGTH - 1; i >= 0; i--)
     {
-        afterProduct[i - 1] = afterProduct[i] * nums[i];
-    }
-    
-    const output = new Array(LENGTH);
-
-    for (let i = 0; i < LENGTH; i++)
-    {
-        output[i] = beforeProduct[i] * afterProduct[i];
+        output[i] *= rightProduct;
+        rightProduct *= nums[i];
     }
 
     return output;
