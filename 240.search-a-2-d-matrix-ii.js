10f8bc1691fc97fe76b34409ef0b513e7988c38e
Soulike <soulike@qq.com>
2021-08-26 08:53:55 +0800

    240-2

--- 240.search-a-2-d-matrix-ii.js
+++ 240.search-a-2-d-matrix-ii.js
@@ -12,54 +12,27 @@
  */
 const searchMatrix = function (matrix, target) 
 {
-    const ROW_LENGTH = matrix[0].length;
-
-    for (const row of matrix)
+    const ROW_AMOUNT = matrix.length;
+    const COL_AMOUNT = matrix[0].length;
+    let x = ROW_AMOUNT - 1;
+    let y = 0;
+    while (x >= 0 && y < COL_AMOUNT)
     {
-        if (row[0] === target || row[ROW_LENGTH - 1] === target)
+        if (matrix[x][y] === target)
         {
             return true;
         }
-        else if (row[0] < target && row[ROW_LENGTH - 1] > target)
+        else if (matrix[x][y] > target)
         {
-            if (binarySearch(row, target))
-            {
-                return true;
-            }
+            x--;
+        }
+        else
+        {
+            y++;
         }
     }
     return false;
 };
-
-/**
- * 
- * @param {number[]} numbers 
- * @param {number} target 
- * @returns {boolean}
- */
-function binarySearch(numbers, target)
-{
-    let left = 0;
-    let right = numbers.length - 1;
-    let mid = Math.floor((left + right) / 2);
-    while (left <= right)
-    {
-        mid = Math.floor((left + right) / 2);
-        if (numbers[mid] === target)
-        {
-            return true;
-        }
-        else if (numbers[mid] < target)
-        {
-            left = mid + 1;
-        }
-        else
-        {
-            right = mid - 1;
-        }
-    }
-    return false;
-}
 // @lc code=end
 
 console.log(searchMatrix([
@@ -67,4 +40,4 @@ console.log(searchMatrix([
     [2, 5, 8, 12, 19],
     [3, 6, 9, 16, 22],
     [10, 13, 14, 17, 24],
-    [18, 21, 23, 26, 30]], 13));
+    [18, 21, 23, 26, 30]], 2));
