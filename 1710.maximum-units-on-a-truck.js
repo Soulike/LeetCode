02aa0b089a9dabd0141b4bc4665659676212265c
Soulike <soulike@qq.com>
2021-09-25 09:07:41 +0800

    1710

---
+++ 1710.maximum-units-on-a-truck.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=1710 lang=javascript
+ *
+ * [1710] Maximum Units on a Truck
+ */
+
+// @lc code=start
+/**
+ * @param {[number, number][]} boxTypes
+ * @param {number} truckSize
+ * @return {number}
+ */
+const maximumUnits = function (boxTypes, truckSize)
+{
+    boxTypes.sort((a, b) => b[1] - a[1]);
+    let unitCount = 0;
+    for (const [numberOfBoxes, numberOfUnitsPerBox] of boxTypes)
+    {
+        if (numberOfBoxes >= truckSize)
+        {
+            unitCount += truckSize * numberOfUnitsPerBox;
+            truckSize = 0;
+        }
+        else
+        {
+            unitCount += numberOfUnitsPerBox * numberOfBoxes;
+            truckSize -= numberOfBoxes;
+        }
+
+        if (truckSize === 0)
+        {
+            break;
+        }
+    }
+
+    return unitCount;
+};
+// @lc code=end
