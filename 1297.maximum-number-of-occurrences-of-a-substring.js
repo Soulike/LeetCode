7afd88a14f52b736b499c4250f7708040e7a6a70
Soulike <soulike@qq.com>
2021-10-04 08:19:46 +0800

    1297

--- 1297.maximum-number-of-occurrences-of-a-substring.js
+++ 1297.maximum-number-of-occurrences-of-a-substring.js
@@ -19,44 +19,40 @@ const maxFreq = function (s, maxLetters, minSize, maxSize)
     /** @type {Map<string, number>} */
     const subStringToLetterAmount = new Map();
 
-    for (let currentSize = minSize; currentSize <= maxSize; currentSize++)
-    {
-        let left = 0;
-        let right = currentSize - 1;
+    let left = 0;
+    let right = minSize - 1;
 
-        for (let i = left; i <= right; i++)
+    for (let i = left; i <= right; i++)
+    {
+        subStringToLetterAmount.set(s[i],
+            (subStringToLetterAmount.get(s[i]) ?? 0) + 1);
+    }
+    if (subStringToLetterAmount.size <= maxLetters)
+    {
+        const subString = s.slice(left, right + 1);
+        subStringToAmount.set(subString, (subStringToAmount.get(subString) ?? 0) + 1);
+    }
+
+    while (right < s.length - 1)
+    {
+        left++;
+        right++;
+        const letterRemoved = s[left - 1];
+        const letterAdded = s[right];
+        subStringToLetterAmount.set(letterRemoved,
+            (subStringToLetterAmount.get(letterRemoved) ?? 1) - 1);
+        if (subStringToLetterAmount.get(letterRemoved) === 0)
         {
-            subStringToLetterAmount.set(s[i],
-                (subStringToLetterAmount.get(s[i]) ?? 0) + 1);
+            subStringToLetterAmount.delete(letterRemoved);
         }
+        subStringToLetterAmount.set(letterAdded,
+            (subStringToLetterAmount.get(letterAdded) ?? 0) + 1);
+
         if (subStringToLetterAmount.size <= maxLetters)
         {
             const subString = s.slice(left, right + 1);
             subStringToAmount.set(subString, (subStringToAmount.get(subString) ?? 0) + 1);
         }
-
-        while (right < s.length - 1)
-        {
-            left++;
-            right++;
-            const letterRemoved = s[left - 1];
-            const letterAdded = s[right];
-            subStringToLetterAmount.set(letterRemoved,
-                (subStringToLetterAmount.get(letterRemoved) ?? 1) - 1);
-            if (subStringToLetterAmount.get(letterRemoved) === 0)
-            {
-                subStringToLetterAmount.delete(letterRemoved);
-            }
-            subStringToLetterAmount.set(letterAdded,
-                (subStringToLetterAmount.get(letterAdded) ?? 0) + 1);
-
-            if (subStringToLetterAmount.size <= maxLetters)
-            {
-                const subString = s.slice(left, right + 1);
-                subStringToAmount.set(subString, (subStringToAmount.get(subString) ?? 0) + 1);
-            }
-        }
-        subStringToLetterAmount.clear();
     }
 
     if (subStringToAmount.size === 0)
@@ -70,7 +66,3 @@ const maxFreq = function (s, maxLetters, minSize, maxSize)
     return subStringAmounts[0];
 };
 // @lc code=end
-
-console.log(maxFreq('abcde', 2, 3, 3)); // 0
-
-console.log(maxFreq('aababcaab', 2, 3, 4));
