0dedc7153918046fecfd3931c0950bc1079ad8e4
Soulike <soulike@qq.com>
2021-10-11 08:24:05 +0800

    1344

---
+++ 1344.angle-between-hands-of-a-clock.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=1344 lang=javascript
+ *
+ * [1344] Angle Between Hands of a Clock
+ */
+
+// @lc code=start
+/**
+ * @param {number} hour
+ * @param {number} minutes
+ * @return {number}
+ */
+const angleClock = function (hour, minutes)
+{
+    const anglePerCircle = 360;
+    const hoursPerCircle = 12;
+    const anglePerHour = anglePerCircle / hoursPerCircle;
+    const minutesPerHour = 60;
+
+    const hourAngle = (hour % hoursPerCircle + minutes / minutesPerHour) * anglePerHour;
+    const minutesAngle = minutes / minutesPerHour * anglePerCircle;
+
+    const diff = Math.abs(hourAngle - minutesAngle);
+    return Math.min(diff, anglePerCircle - diff);
+};
+// @lc code=end
