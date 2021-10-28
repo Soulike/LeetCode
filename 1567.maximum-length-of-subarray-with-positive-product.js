a4b1fc5dc94acc02b7f488956ce136585e950ecb
Soulike <soulike@qq.com>
2021-10-28 10:09:22 +0800

    1567-2

--- 1567.maximum-length-of-subarray-with-positive-product.js
+++ 1567.maximum-length-of-subarray-with-positive-product.js
@@ -5,37 +5,15 @@
  */
 
 // @lc code=start
-
-class Part
-{
-    /**
-     * @param {number} start 
-     * @param {number} end 
-     * @param {number} negativeCount 
-     * @param {number} firstNegativeIndex 
-     * @param {number} lastNegativeIndex 
-     */
-    constructor(start, end, negativeCount, firstNegativeIndex, lastNegativeIndex)
-    {
-        this.start = start;
-        this.end = end;
-        this.negativeCount = negativeCount;
-        this.firstNegativeIndex = firstNegativeIndex;
-        this.lastNegativeIndex = lastNegativeIndex;
-    }
-}
-
 /**
  * @param {number[]} nums
  * @return {number}
  */
 const getMaxLen = function (nums)
 {
-    /**
-     * @type {Part[]}
-     */
-    const parts = [];
-    
+    let maxLen = 0;
+
+    // 当前部分的信息
     let start = 0;
     let end = 0;
     let negativeCount = 0;
@@ -67,25 +45,20 @@ const getMaxLen = function (nums)
         }
         end = currentIndex - 1;
 
-        parts.push(new Part(start, end, negativeCount, firstNegativeIndex, lastNegativeIndex));
+        if (!(negativeCount % 2))
+        {
+            maxLen = Math.max(maxLen, end - start + 1);
+        }
+        else
+        {
+            const cutLen = Math.min(end - lastNegativeIndex + 1, firstNegativeIndex - start + 1);
+            maxLen = Math.max(maxLen, end - start + 1 - cutLen);
+        }
 
         firstNegativeIndex = -1;
         negativeCount = 0;
     }
 
-    let maxLen = 0;
-    for (const part of parts)
-    {
-        if (!(part.negativeCount % 2))
-        {
-            maxLen = Math.max(maxLen, part.end - part.start + 1);
-        }
-        else
-        {
-            const cutLen = Math.min(part.end - part.lastNegativeIndex + 1, part.firstNegativeIndex - part.start + 1);
-            maxLen = Math.max(maxLen, part.end - part.start + 1 - cutLen);
-        }
-    }
     return maxLen;
 };
 // @lc code=end
