e61c08b2c2b73787a15c35f6ac790d6d785c2aa8
Soulike <soulike@qq.com>
2021-11-05 10:03:08 +0800

    1535

--- 1535.find-the-winner-of-an-array-game.js
+++ 1535.find-the-winner-of-an-array-game.js
@@ -12,21 +12,26 @@
  */
 const getWinner = function (arr, k)
 {
+    // 如果一个数已经赢了所有剩下的数，那么就不需要再继续比较下去
     k = Math.min(arr.length - 1, k);
 
-    const numberToConsecutiveWinCount = new Map();
-    let index1 = 0;
-    let index2 = 1;
+    let lastWinningNumber = NaN;
+    let lastWinningNumberConsecutiveCount = 0;
+
+    let index1 = 0; // 被比较的前数下标
+    let index2 = 1; // 被比较的后数下标
     let winningNumber = 0;
     let losingNumber = 0;
     while (true)
     {
+        // 前数赢，后数下标后移
         if (arr[index1] > arr[index2])
         {
             winningNumber = arr[index1];
             losingNumber = arr[index2];
-            index2 = (index2 + 1) % arr.length;
+            index2 = (index2 + 1) % arr.length; // 注意成环
         }
+        // 后数赢，前数下标移动到后数下标+1，然后交换
         else
         {
             winningNumber = arr[index2];
@@ -34,10 +39,18 @@ const getWinner = function (arr, k)
             index1 = (index2 + 1) % arr.length;
             [index1, index2] = [index2, index1];
         }
-        numberToConsecutiveWinCount.set(winningNumber,
-            (numberToConsecutiveWinCount.get(winningNumber) ?? 0) + 1);
-        numberToConsecutiveWinCount.set(losingNumber, 0);
-        if (numberToConsecutiveWinCount.get(winningNumber) === k)
+
+        if (winningNumber === lastWinningNumber)
+        {
+            lastWinningNumberConsecutiveCount++;
+        }
+        else
+        {
+            lastWinningNumber = winningNumber;
+            lastWinningNumberConsecutiveCount = 1;
+        }
+        
+        if (lastWinningNumberConsecutiveCount === k)
         {
             return winningNumber;
         }
