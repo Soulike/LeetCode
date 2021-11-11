76336651478e3e5cd2233c0036f8b79b37bd2471
Soulike <soulike@qq.com>
2021-11-11 09:43:26 +0800

    1413

---
+++ 1413.minimum-value-to-get-positive-step-by-step-sum.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=1413 lang=javascript
+ *
+ * [1413] Minimum Value to Get Positive Step by Step Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const minStartValue = function (nums)
+{
+    let stepSum = 0;
+    let minStepSum = 0;
+    for (const num of nums)
+    {
+        stepSum += num;
+        minStepSum = Math.min(minStepSum, stepSum);
+    }
+    if (minStepSum >= 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return -minStepSum + 1;
+    }
+};
+// @lc code=end
