7f6a3318a2780af84a6e11502d7518e3d75724ab
Soulike <soulike@qq.com>
2021-11-30 09:44:31 +0800

    371-2

--- 371.sum-of-two-integers.js
+++ 371.sum-of-two-integers.js
@@ -12,45 +12,12 @@
  */
 const getSum = function (a, b)
 {
-    let result = 0;
-    let processNextBit = 0b0;   // 进位
-    for (let i = 0; i < 32; i++)
+    if (b === 0)
     {
-        const currentMask = 0b1 << i;
-
-        const aMasked = (a & currentMask);
-        const bMasked = (b & currentMask);
-
-        if (((aMasked & bMasked) & (0b1 << i)) !== 0)
-        {
-            if (processNextBit === 0b1) // a b 进位 都是 1
-            {
-                processNextBit = 0b1;
-                result = result | (0b1 << i);
-            }
-            else    // a b 都是 1，进位是 0
-            {
-                processNextBit = 0b1;
-            }
-        }
-        else if (((aMasked | bMasked) & (0b1 << i)) !== 0)
-        {
-            if (processNextBit === 0b1) // a b 有一个 1，进位是 1
-            {
-                processNextBit = 0b1;
-            }
-            else    // a b 有一个 1，进位是 0
-            {
-                result = result | (0b1 << i);
-                processNextBit = 0b0;
-            }
-        }
-        else    // a 和 b 都是 0
-        {
-            result = result | (processNextBit << i);
-            processNextBit = 0b0;
-        }
+        return a;
     }
-    return result;
+    const sum = a ^ b;
+    const carry = (a & b & 0x7fffffff) << 1;
+    return getSum(sum, carry);
 };
 // @lc code=end
