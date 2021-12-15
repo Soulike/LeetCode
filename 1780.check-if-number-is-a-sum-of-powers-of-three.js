704eaa672538841c7924de2491d5300b9395fbd2
Soulike <soulike@qq.com>
2021-12-15 11:26:30 +0800

    1780

---
+++ 1780.check-if-number-is-a-sum-of-powers-of-three.js
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=1780 lang=javascript
+ *
+ * [1780] Check if Number is a Sum of Powers of Three
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+const checkPowersOfThree = function (n)
+{
+    const cache = new Map();
+    return helper(n, 0, cache);
+};
+
+function helper(n, minExp, cache)
+{
+    const cached = cache.get(n);
+    if (cached !== undefined)
+    {
+        return cached;
+    }
+
+    let result = false;
+    for (let i = minExp; ; i++)
+    {
+        const threePower = 3 ** i;
+        if (threePower === n)
+        {
+            result = true;
+        }
+        else if (threePower > n)
+        {
+            break;
+        }
+        else
+        {
+            result = helper(n - threePower, i + 1, cache);
+            if (result)
+            {
+                break;
+            }
+        }
+    }
+
+    cache.set(n, result);
+    return result;
+}
+// @lc code=end
