ef5ad8c611d7384745afc51d6d593fef9333edc9
Soulike <soulike@qq.com>
2021-12-20 08:39:59 +0800

    1347

---
+++ 1347.minimum-number-of-steps-to-make-two-strings-anagram.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=1347 lang=javascript
+ *
+ * [1347] Minimum Number of Steps to Make Two Strings Anagram
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+const minSteps = function (s, t)
+{
+    const letterToCount = new Map();
+    for (const char of s)
+    {
+        letterToCount.set(char, (letterToCount.get(char) ?? 0) + 1);
+    }
+
+    for (const char of t)
+    {
+        letterToCount.set(char, (letterToCount.get(char) ?? 0) - 1);
+    }
+
+    let result = 0;
+
+    for (const [, count] of letterToCount)
+    {
+        if (count > 0)
+        {
+            result += count;
+        }
+    }
+
+    return result;
+};
+// @lc code=end
