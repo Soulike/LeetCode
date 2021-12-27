ebeb808929890f3231dfbfca17fe1317b9ffd4ae
Soulike <soulike@qq.com>
2021-12-27 09:40:25 +0800

    496

---
+++ 496.next-greater-element-i.js
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode id=496 lang=javascript
+ *
+ * [496] Next Greater Element I
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+const nextGreaterElement = function (nums1, nums2)
+{
+    const numToIndexOfNums2 = new Map();
+
+    for (let i = 0; i < nums2.length; i++)
+    {
+        numToIndexOfNums2.set(nums2[i], i);
+    }
+
+    const nextGreaterElementIndexOfNums2 = new Array(nums2.length);
+    nextGreaterElementIndexOfNums2.fill(-1);
+
+    const monostackOfElementIndexInNums2 = [];  // 栈底到栈顶，单调递减
+    for (let i = 0; i < nums2.length; i++)
+    {
+        if (monostackOfElementIndexInNums2.length === 0)
+        {
+            monostackOfElementIndexInNums2.push(i);
+        }
+        else
+        {
+            let topIndex = monostackOfElementIndexInNums2[monostackOfElementIndexInNums2.length - 1];
+            let topTemp = nums2[topIndex];
+            while (topTemp < nums2[i])   // 遇到了一个上升的温度
+            {
+                nextGreaterElementIndexOfNums2[topIndex] = i;
+                monostackOfElementIndexInNums2.pop();
+                if (monostackOfElementIndexInNums2.length === 0)
+                {
+                    break;
+                }
+
+                topIndex = monostackOfElementIndexInNums2[monostackOfElementIndexInNums2.length - 1];
+                topTemp = nums2[topIndex];
+            }
+            monostackOfElementIndexInNums2.push(i);
+        }
+    }
+
+    const result = [];
+    for (const num of nums1)
+    {
+        const numIndexInNums2 = numToIndexOfNums2.get(num);
+        const nextGreaterIndexInNums2 = nextGreaterElementIndexOfNums2[numIndexInNums2];
+        result.push(nextGreaterIndexInNums2 === -1 ? -1 : nums2[nextGreaterIndexInNums2]);
+    }
+
+    return result;
+};
+// @lc code=end
