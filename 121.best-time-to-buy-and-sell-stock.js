3125587872d4e5ae323ce30224e6eacdd0187798
Soulike <soulike@qq.com>
2021-12-28 11:35:45 +0800

    122

--- 121.best-time-to-buy-and-sell-stock.js
+++ 121.best-time-to-buy-and-sell-stock.js
@@ -11,24 +11,8 @@
  */
 const maxProfit = function (prices)
 {
-    let max = 0;
-    let sum = 0;
-    let diff = 0;
-    const LENGTH = prices.length;
-    for (let i = 0; i < LENGTH - 1; i++)
-    {
-        diff = prices[i + 1] - prices[i];
-        sum += diff;
-        if (sum < 0)
-        {
-            sum = 0;
-        }
-        else if (sum > max)
-        {
-            max = sum;
-        }
-    }
-    return max;
+    
 };
 // @lc code=end
 
+console.log(maxProfit([7, 1, 5, 3, 6, 4]))
--- 122.best-time-to-buy-and-sell-stock-ii.js
+++ 122.best-time-to-buy-and-sell-stock-ii.js
@@ -11,17 +11,42 @@
  */
 const maxProfit = function (prices) 
 {
-    let maxProfit = 0;
-    const length = prices.length;
-    for (let i = 0; i < length - 1; i++)
+    const DAY_COUNT = prices.length;
+    /**
+     * dp[i][0 or 1] 在第 i 天，买入(1)或者不买(0)当天的股票所取得的最大利润
+     */
+    const dp = new Array(DAY_COUNT);
+    for (let i = 0; i < DAY_COUNT; i++)
     {
-        const profit = prices[i + 1] - prices[i];
-        if (profit > 0)
-        {
-            maxProfit += profit;
-        }
+        dp[i] = new Array(2);
     }
-    return maxProfit;
+    /**
+     * 在第 i 天，
+     * 前一天买了股票
+     * 今天要卖出
+     * dp[i][0] = dp[i-1][1] + prices[i]
+     * 今天摸了
+     * dp[i][1] = dp[i-1][1]
+     * 
+     * 前一天没买股票
+     * 今天要买入
+     * dp[i][1] = dp[i-1][0] - prices[i]
+     * 今天摸了
+     * dp[i][0] = dp[i-1][0]
+     * 
+     * 第 0 天
+     * dp[0][0] = 0
+     * dp[0][1] = -prices[0];
+     */
+    dp[0][0] = 0;
+    dp[0][1] = -prices[0];
+
+    for (let i = 1; i < DAY_COUNT; i++)
+    {
+        dp[i][0] = Math.max(dp[i - 1][1] + prices[i], dp[i - 1][0]);
+        dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+    }
+
+    return dp[DAY_COUNT - 1][0];
 };
 // @lc code=end
-
