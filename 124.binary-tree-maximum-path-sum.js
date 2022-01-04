57c648f051b78069f94677fabe36788d1256f2c7
Soulike <soulike@qq.com>
2022-01-04 13:32:21 +0800

    124-2

--- 124.binary-tree-maximum-path-sum.js
+++ 124.binary-tree-maximum-path-sum.js
@@ -20,75 +20,30 @@
 var maxPathSum = function (root)
 {
     let maxSum = Number.NEGATIVE_INFINITY;
-    const nodeQueue = [root];
-    for (let i = 0; i < nodeQueue.length; i++)
+    /**
+     * 比较 root 的左右子树最大路径之和，并返回大的那个
+     */
+    function maxSideSum(root)
     {
-        const node = nodeQueue[i];
-        maxSum = Math.max(maxSum, maxRootSum(node));
-        if (node.left !== null)
+        if (root === null)
         {
-            nodeQueue.push(node.left);
+            return 0;
         }
-        if (node.right !== null)
-        {
-            nodeQueue.push(node.right);
-        }
+        const leftSideMaxSum = Math.max(maxSideSum(root.left), 0);
+        const rightSideMaxSum = Math.max(maxSideSum(root.right), 0);
+
+        // 看看以 root 为根结点能得到的最大路径值
+        maxSum = Math.max(maxSum, root.val + leftSideMaxSum + rightSideMaxSum);
+
+        const sideSum = Math.max(leftSideMaxSum, rightSideMaxSum);
+
+        const result = root.val + sideSum;
+        return result;
     }
+
+
+    maxSideSum(root);
     return maxSum;
 };
-
-/**
- * 比较 root 的左右子树最大路径之和，并返回大的那个
- */
-function maxSideSum(root)
-{
-    let sideSum;
-    if (root.left !== null && root.right !== null)
-    {
-        sideSum = Math.max(maxSideSum(root.left), maxSideSum(root.right));
-    }
-    else if (root.left !== null && root.right === null)
-    {
-        sideSum = maxSideSum(root.left);
-    }
-    else if (root.left === null && root.right !== null)
-    {
-        sideSum = maxSideSum(root.right);
-    }
-    else if (root.left === null && root.right === null)
-    {
-        sideSum = 0;
-    }
-
-    return root.val + (sideSum > 0 ? sideSum : 0);
-}
-
-/**
- * 返回经过 root 的最大和
- */
-function maxRootSum(root)
-{
-    let sideSum;
-    if (root.left !== null && root.right !== null)
-    {
-        const leftSideSum = maxSideSum(root.left);
-        const rightSideSum = maxSideSum(root.right);
-        sideSum = (leftSideSum > 0 ? leftSideSum : 0) + (rightSideSum > 0 ? rightSideSum : 0)
-    }
-    else if (root.left !== null && root.right === null)
-    {
-        sideSum = maxSideSum(root.left);
-    }
-    else if (root.left === null && root.right !== null)
-    {
-        sideSum = maxSideSum(root.right);
-    }
-    else if (root.left === null && root.right === null)
-    {
-        sideSum = 0;
-    }
-
-    return root.val + (sideSum > 0 ? sideSum : 0);
-}
 // @lc code=end
 
