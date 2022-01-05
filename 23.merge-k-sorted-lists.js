e143a48dfeb16fcffbc441cebc2caad709e0b3ce
Soulike <soulike@qq.com>
2022-01-05 10:36:03 +0800

    23

---
+++ 23.merge-k-sorted-lists.js
@@ -0,0 +1,237 @@
+/*
+ * @lc app=leetcode id=23 lang=javascript
+ *
+ * [23] Merge k Sorted Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+class Heap
+{
+    treeNodes;
+    compareFunction;
+
+    /**
+     * @param compareFunction - 比较函数。如果返回值小于 0，则 a 在 b 前面，大于 0 则相反
+     */
+    constructor(compareFunction, elements)
+    {
+        this.treeNodes = [];
+        this.compareFunction = compareFunction;
+        if (elements !== undefined)
+        {
+            this.add(...elements);
+        }
+    }
+
+    size()
+    {
+        return this.treeNodes.length;
+    }
+
+    add(...elements)
+    {
+        for (const element of elements)
+        {
+            this.addOne(element);
+        }
+    }
+
+    /**
+     * 给堆添加一个元素。
+     * 先将元素放到堆末尾，再和父结点比较。如果不符合顺序，就交换它们，直到子结点与父结点符合顺序或没有父结点。
+     */
+    addOne(element)
+    {
+        let elementIndex = this.treeNodes.length;
+        let parentIndex = Heap.getParentIndex(elementIndex);
+        this.treeNodes.push(element);
+
+        while (elementIndex > 0
+            && this.compareFunction(this.treeNodes[elementIndex],
+                this.treeNodes[parentIndex]) < 0)
+        {
+            Heap.swap(this.treeNodes, elementIndex, parentIndex);
+            elementIndex = parentIndex;
+            parentIndex = Heap.getParentIndex(elementIndex);
+        }
+    }
+
+    /**
+     * @throws {RangeError} 
+     * */
+    getRoot()
+    {
+        if (this.treeNodes.length === 0)
+        {
+            throw new RangeError('Heap has no element');
+        }
+        else
+        {
+            return this.treeNodes[0];
+        }
+    }
+
+    /**
+     * 删除堆顶。
+     * 先将堆顶与最后一个元素交换，然后删除最后一个元素。
+     * 然后，从堆顶开始，比较父结点与左右子结点。如果子结点的最小值与父结点不符合顺序要求，交换它们，直到符合顺序要求
+     * @throws {RangeError}
+     * */
+    deleteRoot()
+    {
+        if (this.treeNodes.length === 0)
+        {
+            throw new RangeError('Heap has no element');
+        }
+        else
+        {
+            const lastIndex = this.treeNodes.length - 1;
+            Heap.swap(this.treeNodes, 0, lastIndex);
+            this.treeNodes.length--;
+
+            let elementIndex = 0;
+            let minChildIndex = this.getMinChildIndex(elementIndex);
+            if (minChildIndex === -1)
+            {
+                return;
+            }
+
+            while (this.compareFunction(this.treeNodes[minChildIndex],
+                this.treeNodes[elementIndex]) < 0)
+            {
+                Heap.swap(this.treeNodes, elementIndex, minChildIndex);
+                elementIndex = minChildIndex;
+
+                minChildIndex = this.getMinChildIndex(elementIndex);
+                if (minChildIndex === -1)
+                {
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * 获取当前结点值靠前的子结点，如果不存在子结点，返回 -1
+     */
+    getMinChildIndex(elementIndex)
+    {
+        const LENGTH = this.treeNodes.length;
+        let leftChildIndex = Heap.getLeftChildIndex(elementIndex);
+        let rightChildIndex = Heap.getRightChildIndex(elementIndex);
+
+        let minChildIndex;
+
+        if (leftChildIndex < LENGTH && rightChildIndex < LENGTH)
+        {
+            minChildIndex = this.compareFunction(this.treeNodes[leftChildIndex],
+                this.treeNodes[rightChildIndex]) < 0
+                ? leftChildIndex
+                : rightChildIndex;
+        }
+        else if (leftChildIndex < LENGTH)
+        {
+            minChildIndex = leftChildIndex;
+        }
+        else if (rightChildIndex < LENGTH)
+        {
+            minChildIndex = rightChildIndex;
+        }
+        else
+        {
+            minChildIndex = -1;
+        }
+        return minChildIndex;
+    }
+
+    static getLeftChildIndex(rootIndex)
+    {
+        return 2 * rootIndex + 1;
+    }
+
+    static getRightChildIndex(rootIndex)
+    {
+        return 2 * rootIndex + 2;
+    }
+
+    static getParentIndex(childIndex)
+    {
+        if (childIndex % 2) // odd
+        {
+            return (childIndex - 1) / 2;
+        }
+        else    // even
+        {
+            return (childIndex - 2) / 2;
+        }
+    }
+
+    static swap(array, index1, index2)
+    {
+        [
+            array[index1],
+            array[index2]
+        ] = [
+                array[index2],
+                array[index1]
+            ];
+    }
+}
+
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function (lists)
+{
+    if (lists.length === 0)
+    {
+        return null;
+    }
+    const fakeHead = new ListNode();
+    let tail = fakeHead;
+
+    const heap = new Heap(comp, lists);
+
+    function comp(a, b)
+    {
+        if (a !== null && b !== null)
+        {
+            return a.val - b.val;
+        }
+        if (a === null)
+        {
+            return 1;
+        }
+        if (b === null)
+        {
+            return -1;
+        }
+    }
+    
+    while (true)
+    {
+        const nextNode = heap.getRoot();
+        if (nextNode === null)
+        {
+            break;
+        }
+        heap.deleteRoot();
+        heap.addOne(nextNode.next);
+        
+        tail.next = nextNode;
+        nextNode.next = null;
+        tail = nextNode;
+    }
+
+    return fakeHead.next;
+};
+// @lc code=end
+
