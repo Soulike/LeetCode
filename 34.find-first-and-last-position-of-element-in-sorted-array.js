4d3853e2123293487a81da34e430fb9566194c00
Soulike <soulike@qq.com>
2022-01-06 10:58:39 +0800

    34-2

--- 34.find-first-and-last-position-of-element-in-sorted-array.js
+++ 34.find-first-and-last-position-of-element-in-sorted-array.js
@@ -12,7 +12,8 @@
  */
 const searchRange = function (nums, target) 
 {
-    if (nums.length === 0 || target < nums[0] || target > nums[nums.length - 1])
+    if (nums.length === 0
+        || target < nums[0] || target > nums[nums.length - 1])
     {
         return [-1,-1];
     }
@@ -21,7 +22,7 @@ const searchRange = function (nums, target)
         return nums[0] === target ? [0, 0] : [-1, -1];
     }
     // nums.length >= 2
-    const index = binarySearch(nums, 0, nums.length, target);
+    const index = binarySearch();
     if (index === -1)
     {
         return [-1, -1];
@@ -29,51 +30,41 @@ const searchRange = function (nums, target)
     // index !== -1
     let left = index - 1;
     let right = index + 1;
-    while (nums[left] === target || nums[right] === target)
+    while (nums[left] === target)
     {
-        if (nums[left] === target)
-        {
-            left--;
-        }
-        if (nums[right] === target)
-        {
-            right++;
-        }
+        left--;
+    }
+    while (nums[right] === target)
+    {
+        right++;
     }
     return [left + 1, right - 1];
-};
 
-/**
- * @param {number[]} nums 
- * @param {number} start 开始下标
- * @param {number} end 结束下标，不在范围中
- * @param {number} target 
- * @return {number}
- */
-function binarySearch(nums, start, end, target)
-{
-    if (end === start)
+    function binarySearch()
     {
+        let left = 0;
+        let right = nums.length - 1;
+        let mid = Math.floor((left + right) / 2);
+
+        while (left <= right)
+        {
+            if (target > nums[mid])
+            {
+                left = mid + 1;
+            }
+            else if (target < nums[mid])
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                return mid;
+            }
+            mid = Math.floor((left + right) / 2);
+        }
+
         return -1;
     }
-    if (end - start === 1)
-    {
-        return nums[start] === target ? start : -1;
-    }
-    const mid = Math.floor((start + end) / 2);
-    const midNum = nums[mid];
-    if (midNum === target)
-    {
-        return mid;
-    }
-    else if (midNum > target)
-    {
-        return binarySearch(nums, start, mid, target);
-    }
-    else    // midNum < target
-    {
-        return binarySearch(nums, mid + 1, end, target);
-    }
-}
+};
 // @lc code=end
 
