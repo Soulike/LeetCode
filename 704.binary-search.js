c17e2dcf4c1ea433ed919e12a662ec55df2abeb8
Soulike <soulike@qq.com>
2022-01-06 10:53:50 +0800

    704

---
+++ 704.binary-search.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=704 lang=javascript
+ *
+ * [704] Binary Search
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target)
+{
+    let left = 0;
+    let right = nums.length-1;
+    let mid = Math.floor((left + right) / 2);
+
+    while (left <= right)
+    {
+        if (target > nums[mid])
+        {
+            left = mid + 1;
+        }
+        else if(target < nums[mid])
+        {
+            right = mid - 1;
+        }
+        else
+        {
+            return mid;
+        }
+        mid = Math.floor((left + right) / 2);
+    }
+
+    return -1;
+};
+// @lc code=end
+
