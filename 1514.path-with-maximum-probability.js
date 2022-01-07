5cbafd1247cb1fa2990fcc25b72beeb05a896255
Soulike <soulike@qq.com>
2022-01-07 11:08:30 +0800

    743

---
+++ 1514.path-with-maximum-probability.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=1514 lang=javascript
+ *
+ * [1514] Path with Maximum Probability
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number[]} succProb
+ * @param {number} start
+ * @param {number} end
+ * @return {number}
+ */
+var maxProbability = function (n, edges, succProb, start, end)
+{
+    
+};
+// @lc code=end
+
+console.log(maxProbability(3,
+    [[0, 1]],
+    [0.5],
+    0,
+    2));
--- 743.network-delay-time.js
+++ 743.network-delay-time.js
@@ -13,49 +13,71 @@
  */
 var networkDelayTime = function (times, n, k)
 {
-    const delayMatrix = new Array(n);
+    /** 从 start 到结点 i 的最短路径 */
+    const minDelaysFromStart = new Array(n);
+    minDelaysFromStart.fill(Number.POSITIVE_INFINITY);
+    minDelaysFromStart[k - 1] = 0;  // start 到自己是 0
+
+    /** 邻接矩阵 */
+    const matrix = new Array(n);
     for (let i = 0; i < n; i++)
     {
-        delayMatrix[i] = new Array(n);
-        delayMatrix[i].fill(Number.POSITIVE_INFINITY);
-        delayMatrix[i][i] = 0;
+        matrix[i] = new Array(n);
+        matrix[i].fill(Number.POSITIVE_INFINITY);
+        matrix[i][i] = 0;
     }
+
     for (const [from, to, delay] of times)
     {
-        delayMatrix[from - 1][to - 1] = delay;
-    }
-
-    let hasChange = true;
-
-    while (hasChange)
-    {
-        hasChange = false;
-        for (let from = 0; from < n; from++)
+        matrix[from - 1][to - 1] = delay;
+        if (from === k)
         {
-            for (let to = 0; to < n; to++)
-            {
-                if (from !== to)
-                {
-                    for (let mid = 0; mid < n; mid++)
-                    {
-                        if (mid !== from && mid !== to)
-                        {
-                            const delayThroughMid = delayMatrix[from][mid] + delayMatrix[mid][to];
-                            if (delayThroughMid < delayMatrix[from][to])
-                            {
-                                delayMatrix[from][to] = delayThroughMid;
-                                hasChange = true;
-                            }
-                        }
-                    }
-                }
-            }
+            minDelaysFromStart[to - 1] = delay;
         }
     }
 
-    const maxDelay = Math.max(...delayMatrix[k - 1]);
+    const processedNodes = new Set([k - 1]);
+    const unprocessedNodes = new Set();
+    for (let i = 0; i < n; i++)
+    {
+        if (i !== k - 1)
+        {
+            unprocessedNodes.add(i);
+        }
+    }
 
+    while (processedNodes.size < n)
+    {
+        // 在已处理结点集合中寻找邻接的结点 n，使得 start 到 n 的距离最短
+        let minFrom = -1;
+        let minTo = -1;
+        let minDelay = Number.POSITIVE_INFINITY;
+        for (const from of processedNodes)
+        {
+            const toDelays = matrix[from];
+            for (let to = 0; to < n; to++)
+            {
+                if (!processedNodes.has(to) && minDelaysFromStart[from] + toDelays[to] < minDelay)
+                {
+                    minFrom = from;
+                    minTo = to;
+                    minDelay = minDelaysFromStart[from] + toDelays[to];
+                }
+            }
+        }
+
+        // 图中有孤立结点
+        if (minDelay === Number.POSITIVE_INFINITY)
+        {
+            break;
+        }
+
+        minDelaysFromStart[minTo] = Math.min(minDelaysFromStart[minTo], minDelay);
+        unprocessedNodes.delete(minTo);
+        processedNodes.add(minTo);
+    }
+
+    const maxDelay = Math.max(...minDelaysFromStart);
     return maxDelay === Number.POSITIVE_INFINITY ? -1 : maxDelay;
 };
 // @lc code=end
-
