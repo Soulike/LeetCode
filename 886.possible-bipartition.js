8156b9794b9a856759a09f546de7facf290fb972
Soulike <soulike@qq.com>
2022-02-06 15:35:38 +0800

    886

--- 886.possible-bipartition.js
+++ 886.possible-bipartition.js
@@ -22,71 +22,47 @@ var possibleBipartition = function (n, dislikes)
     {
         personToDislikes.get(person).push(personDislikes);
     }
-    const peopleSet1 = new Set();
-    const peopleSet2 = new Set();
-    // 进行过 BFS 的人
-    const visitedPeopleSet = new Set();
 
-    // 偶数加入 1，奇数加入 2
-    let setTurn = 1;
+    // 当前子图的涂色情况
+    const personToColor = new Map();
+    const visitedPersons = new Set();
 
-    // current 和 last 中的人必须在不同集合中
-    let lastPeople = new Set();
-    let currentPeople = new Set();
-
-    for (let i = 1; i <= n; i++)
+    function traverse(person, expectedColor)
     {
-        if (visitedPeopleSet.has(i))
+        visitedPersons.add(person);
+        const personCurrentColor = personToColor.get(person);
+        if (personCurrentColor === undefined)
         {
-            continue;
-        }
+            personToColor.set(person, expectedColor);
 
-        peopleSet1.clear();
-        peopleSet2.clear();
-        lastPeople.clear();
-        currentPeople.clear();
-
-        lastPeople.add(i);
-
-        peopleSet1.add(i);
-        visitedPeopleSet.add(i);
-
-        while (lastPeople.size > 0)
-        {
-            // BFS
-            for (const person of lastPeople)
+            const dislikePersons = personToDislikes.get(person);
+            for (const person of dislikePersons)
             {
-                visitedPeopleSet.add(person);
-
-                const personDislikes = personToDislikes.get(person);
-                
-                for (const dislike of personDislikes)
-                {
-                    currentPeople.add(dislike);
-                }
-            }
-
-            const [currentPeopleSet, addedPeopleSet] =
-                setTurn % 2
-                    ? [peopleSet1, peopleSet2]
-                    : [peopleSet2, peopleSet1];
-
-            for (const person of currentPeople)
-            {
-                // 出现在同一个集合中
-                if (currentPeopleSet.has(person))
+                if (!traverse(person, !expectedColor))
                 {
                     return false;
                 }
-                addedPeopleSet.add(person);
             }
-
-            lastPeople = currentPeople;
-            currentPeople = new Set();
-            setTurn++;
+            return true;
+        }
+        else
+        {
+            return personCurrentColor === expectedColor;
         }
     }
 
+    for (let person = 1; person <= n; person++)
+    {
+        if (!visitedPersons.has(person))
+        {
+            // 新子图，重新开始涂色
+            personToColor.clear();
+            if (!traverse(person, false))
+            {
+                return false;
+            }
+        }
+    }
     return true;
 };
 // @lc code=end
