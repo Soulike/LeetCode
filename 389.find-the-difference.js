90bd5c765fc2ed0ea5675f1d1bb3cfd2d940c2ee
Soulike <soulike@qq.com>
2022-02-07 09:15:48 +0800

    389

---
+++ 389.find-the-difference.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=389 lang=javascript
+ *
+ * [389] Find the Difference
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+var findTheDifference = function (s, t)
+{
+    let c = 0;
+    for (let i = 0; i < s.length; i++)
+    {
+        c ^= s.charCodeAt(i);
+    }
+    for (let i = 0; i < t.length; i++)
+    {
+        c ^= t.charCodeAt(i);
+    }
+
+    return String.fromCharCode(c);
+};
+// @lc code=end
+
