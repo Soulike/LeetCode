2bd86efe7728c4ec03125f807187884ca9b4b2d5
Soulike <soulike@qq.com>
2022-02-08 11:29:56 +0800

    146-2

--- 146.lru-cache.js
+++ 146.lru-cache.js
@@ -5,21 +5,136 @@
  */
 
 // @lc code=start
-
-class LRUCache
+class ListNode
 {
-    keyToValue;
+    key;
+    value;
+    prev;
+    next;
+
+    constructor(key, value)
+    {
+        this.key = key;
+        this.value = value;
+        this.prev = null;
+        this.next = null;
+    }
+}
+class LinkedHashMap
+{
+    /**
+     * 越靠近链表尾部越新
+     */
+
+    fakeHead;
+    tail;
     capacity;
+    keyToListNode;
 
     constructor(capacity)
     {
         this.capacity = capacity;
-        this.keyToValue = new Map();
+
+        this.fakeHead = new ListNode(-1, -1);
+        this.tail = this.fakeHead;
+        this.keyToListNode = new Map();
     }
 
-    #size()
+    size()
     {
-        return this.keyToValue.size;
+        return this.keyToListNode.size;
+    }
+
+    removeOldest()
+    {
+        if (this.size() !== 0)
+        {
+            const removedNode = this.fakeHead.next;
+            const prevNode = removedNode.prev;
+            const nextNode = removedNode.next;
+
+            prevNode.next = nextNode;
+            if (nextNode !== null)
+            {
+                nextNode.prev = prevNode;
+            }
+
+            if (removedNode === this.tail)
+            {
+                this.tail = prevNode;
+            }
+
+            this.keyToListNode.delete(removedNode.key);
+        }
+    }
+
+    add(key, value)
+    {
+        if (!this.keyToListNode.has(key))
+        {
+            if (this.size() === this.capacity)
+            {
+                this.removeOldest();
+            }
+            const newNode = new ListNode(key, value);
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+            this.keyToListNode.set(key, newNode);
+        }
+        else
+        {
+            this.touch(key);
+            this.keyToListNode.get(key).value = value;
+        }
+    }
+
+    getValue(key)
+    {
+        if (this.keyToListNode.has(key))
+        {
+            this.touch(key);
+            return this.keyToListNode.get(key).value;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    touch(key)
+    {
+        const touchedNode = this.keyToListNode.get(key);
+        if (touchedNode !== undefined)
+        {
+            // 已经是 tail 则无需调整
+            if (this.tail !== touchedNode)
+            {
+                const prevNode = touchedNode.prev;
+                const nextNode = touchedNode.next;
+
+                prevNode.next = nextNode;
+                if (nextNode !== null)
+                {
+                    nextNode.prev = prevNode;
+                }
+
+                this.tail.next = touchedNode;
+                touchedNode.prev = this.tail;
+                touchedNode.next = null;
+                this.tail = touchedNode;
+            }
+        }
+    }
+}
+
+class LRUCache
+{
+    linkedHashMap;
+
+    constructor(capacity)
+    {
+        this.linkedHashMap = new LinkedHashMap(capacity);
     }
 
     /**
@@ -28,17 +143,7 @@ class LRUCache
      */
     get(key)
     {
-        const value = this.keyToValue.get(key);
-        if (value !== undefined)
-        {
-            this.keyToValue.delete(key);
-            this.keyToValue.set(key, value);
-            return value;
-        }
-        else
-        {
-            return -1;
-        }
+        return this.linkedHashMap.getValue(key);
     }
     /**
      * @param {number} key
@@ -47,21 +152,7 @@ class LRUCache
      */
     put(key, value)
     {
-        if (!this.keyToValue.has(key))
-        {
-            if (this.#size() === this.capacity)
-            {
-                const iterator = this.keyToValue.keys();
-                const oldestKey = iterator.next().value;
-                this.keyToValue.delete(oldestKey);
-            }
-            this.keyToValue.set(key, value);
-        }
-        else
-        {
-            this.keyToValue.delete(key);
-            this.keyToValue.set(key, value);
-        }
+        return this.linkedHashMap.add(key, value);
     }
 }
 
@@ -72,3 +163,15 @@ class LRUCache
  * obj.put(key,value)
  */
 // @lc code=end
+
+/*
+["LRUCache","put","put","get","put","get","put","get","get","get"]
+[[2],[1,1],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4]]
+
+*/
+
+const lruCache = new LRUCache(1);
+lruCache.put(2, 1);
+lruCache.get(2);
+lruCache.put(3, 2);
+lruCache.get(2);
