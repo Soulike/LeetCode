51395995deb277a4f22e101083a1f0a685b941f4
Soulike <soulike@qq.com>
2022-02-11 10:19:06 +0800

    42

--- 42.trapping-rain-water.js
+++ 42.trapping-rain-water.js
@@ -12,60 +12,87 @@
  */
 var trap = function (height)
 {
-    let water = 0;
-    let left = 0;
-    let right = 0;
     const LENGTH = height.length;
-    let level =0;
+    if (LENGTH < 3)
+    {
+        return 0;
+    }
 
+    let water = 0;
+    // 极大值点下标
+    let indexes = [];
+
+    // 查找极大值点
+    // 头
+    if (height[0] >= height[1])
+    {
+        indexes.push(0);
+    }
+    // 中间
+    for (let i = 1; i < LENGTH - 1; i++)
+    {
+        if (height[i] >= height[i - 1]
+            && height[i] >= height[i + 1])
+        {
+            indexes.push(i);
+        }
+    }
+    // 尾
+    if (height[LENGTH - 2] <= height[LENGTH - 1])
+    {
+        indexes.push(LENGTH - 1);
+    }
+
+    // 至少需要两个极值点才能有水
+    if (indexes.length < 2)
+    {
+        return 0;
+    }
+
+    // 删掉被两个更大极值包围的极值点
+    let removedIndexesIndex = [];
     while (true)
     {
-        left = -1;
-        right = -1;
-
-        for (let i = 0; i < LENGTH; i++)
+        for (let i = 1; i < indexes.length - 1; i++)
         {
-            if (height[i] > level)
+            const currentHeight = height[indexes[i]];
+            let leftHeight = height[indexes[i - 1]];
+            let rightHeight = height[indexes[i + 1]];
+
+            if (currentHeight <= leftHeight
+                && currentHeight <= rightHeight)
             {
-                left = i;
-                break;
+                removedIndexesIndex.push(i);
             }
         }
-        if (left === -1)
+
+        if (removedIndexesIndex.length === 0)
         {
             break;
         }
 
-        for (let i = left + 1; i < LENGTH; i++)
+        for (let j = removedIndexesIndex.length - 1; j >= 0; j--)
         {
-            if (height[i] > level)
-            {
-                right = i;
-                break;
-            }
+            indexes.splice(removedIndexesIndex[j], 1);
         }
+        removedIndexesIndex = [];
+    }
 
-        if (right === -1)
+    for (let i = 0; i < indexes.length - 1; i++)
+    {
+        const j = i + 1;
+        const leftHeight = height[indexes[i]];
+        const rightHeight = height[indexes[j]];
+        const waterHeight = Math.min(leftHeight, rightHeight);
+        let intervalHeightSum = 0;
+        for (let k = indexes[i] + 1; k < indexes[j]; k++)
         {
-            break;
+            // 删掉水下的被占部分
+            intervalHeightSum += Math.min(height[k], waterHeight);
         }
-
-        while (right !== -1)
-        {
-            water += right - left - 1;
-            left = right;
-            right = -1;
-            for (let i = left + 1; i < LENGTH; i++)
-            {
-                if (height[i] > level)
-                {
-                    right = i;
-                    break;
-                }
-            }
-        }
-
-        level++;
+        water += waterHeight
+            * (indexes[j] - indexes[i] - 1)
+            - intervalHeightSum;
     }
 
     return water;
