5e949d407078f885c51267d27a16c3c94d27a947
Soulike <soulike@qq.com>
2022-02-15 09:04:55 +0800

    136

---
+++ 528.random-pick-with-weight.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=528 lang=javascript
+ *
+ * [528] Random Pick with Weight
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} w
+ */
+class Solution
+{
+    constructor(w)
+    {
+
+    }
+    /**
+     * @return {number}
+     */
+    pickIndex()
+    {
+        
+    }
+}
+
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(w)
+ * var param_1 = obj.pickIndex()
+ */
+// @lc code=end
+
