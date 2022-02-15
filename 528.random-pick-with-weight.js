80fcfbf5dfa12acb70814a004b9fece683c78f2c
Soulike <soulike@qq.com>
2022-02-15 10:06:04 +0800

    528

--- 528.random-pick-with-weight.js
+++ 528.random-pick-with-weight.js
@@ -10,16 +10,55 @@
  */
 class Solution
 {
+    preSum;
+
     constructor(w)
     {
+        this.preSum = [0];
 
+        // 累加和
+        w.reduce((prev, curr) =>
+        {
+            this.preSum.push(prev + curr);
+            return prev + curr;
+        }, 0);
     }
     /**
      * @return {number}
      */
     pickIndex()
     {
-        
+        // 扔石头，看扔到哪个区间里面，用二分搜索找到这个区间的右边界
+        const randomNum = this.generateRandom(1, this.preSum[this.preSum.length - 1]);
+
+        let leftIndex = 0;
+        let rightIndex = this.preSum.length - 1;
+
+        while (true)
+        {
+            const midIndex = leftIndex + Math.floor((rightIndex - leftIndex) / 2);
+            const midPreSum = this.preSum[midIndex];
+            if (midPreSum >= randomNum)
+            {
+                if (this.preSum[midIndex - 1] < randomNum)
+                {
+                    return midIndex - 1;
+                }
+                else
+                {
+                    rightIndex = midIndex - 1;
+                }
+            }
+            else
+            {
+                leftIndex = midIndex + 1;
+            }
+        }
+    }
+
+    generateRandom(start, end)
+    {
+        return Math.floor(start + Math.random() * ((end - start + 1)));
     }
 }
 
@@ -30,4 +69,3 @@ class Solution
  * var param_1 = obj.pickIndex()
  */
 // @lc code=end
-
