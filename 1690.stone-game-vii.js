4bea2a7639ff7c155c3581194dde629b45c894ed
Soulike <soulike@qq.com>
2022-02-20 19:57:38 +0800

    1690

--- 1690.stone-game-vii.js
+++ 1690.stone-game-vii.js
@@ -24,30 +24,41 @@ var stoneGameVII = function (stones)
         return prefixSum[end] - prefixSum[start] + stones[start];
     }
 
-    const cache = new Map();
     /**
-     * 如果先手在 [left,right] 上选择，最多能比后手多选择多少数量的石子
+     * dp[i][j] 如果先手在 [i,j] 的石子堆上选择，能比后手选择的人多拿多少个石子
+     * 
+     * base case 
+     * dp[i][i] = 0
+     * 
+     * dp[i][j] = 
+     * 拿左边
+     * sum(i+1,j) - dp[i+1][j]
+     * 拿右边
+     * sum(i,j-1) - dp[i][j-1]
+     * 取最大值
+     * 
+     * 解释：不管是 A 还是 B，只要自己拿的多，对方拿的少，A 能加大差距，B 能缩小差距。
+     * sum(i+1,j) - dp[i+1][j]：如果拿左侧石子，那么在 [i+1,j] 范围上就是对方先手，对方会比自己多拿 dp[i+1][j] 个石子。让自己的分数减去对方多拿的石子，就是自己在 [i,j] 上比对方多拿的石子。
      */
-    function helper(left, right)
-    {
-        const cacheKey = `${left}-${right}`;
-        if (cache.has(cacheKey))
-        {
-            return cache.get(cacheKey);
-        }
-        if (left >= right)
-        {
-            return 0;
-        }
 
-        const leftDiff = getSum(left + 1, right) - helper(left + 1, right);
-        const rightDiff = getSum(left, right - 1) - helper(left, right - 1);
-        
-        const result = Math.max(leftDiff, rightDiff);
-        cache.set(cacheKey, result)
-        return result;
+    const dp = new Array(n);
+    for (let i = 0; i < n; i++)
+    {
+        dp[i] = new Array(n);
+        dp[i][i] = 0;
     }
 
-    return helper(0, n - 1);
+    for (let i = n - 2; i >= 0; i--)
+    {
+        for (let j = i + 1; j < n; j++)
+        {
+            dp[i][j] = Math.max(
+                getSum(i + 1, j) - dp[i + 1][j],
+                getSum(i, j - 1) - dp[i][j - 1]
+            );
+        }
+    }
+
+    return dp[0][n - 1];
 };
 // @lc code=end
