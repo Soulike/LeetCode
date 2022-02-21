11ede2df25691951a5511f8147693fbed08e90d9
Soulike <soulike@qq.com>
2022-02-21 13:15:47 +0800

    337

--- 337.house-robber-iii.js
+++ 337.house-robber-iii.js
@@ -16,28 +16,62 @@ function TreeNode(val, left, right)
  */
 const rob = function (root) 
 {
-    return Math.max(...helper(root));
-};
-
-/**
- * @param {TreeNode} root
- * @returns {[number, number]} [不抢 root 取得的最大值，抢 root 取得的最大值]
- */
-function helper(root)
-{
-    /** @type {[number,number]} */
-    const result = [0, 0];
-    if (root === null)
+    const cache = new Map();
+    /**
+     * 如果抢或者不抢 root，最多能抢到多少钱
+     */
+    function helper(root, doRob)
     {
+        if (root === null)
+        {
+            return 0;
+        }
+
+        if (cache.has(root))
+        {
+            const rootCached = cache.get(root);
+            if (rootCached[doRob] !== undefined)
+            {
+                return rootCached[doRob];
+            }
+        }
+
+        let result;
+
+        if (doRob)
+        {
+            result = root.val
+                + helper(root.left, false)
+                + helper(root.right, false);
+        }
+        else
+        {
+            result = Math.max(
+                helper(root.left, true),
+                helper(root.left, false))
+                + Math.max(
+                    helper(root.right, true),
+                    helper(root.right, false));
+        }
+
+        if (cache.has(root))
+        {
+            const rootCached = cache.get(root);
+            rootCached[doRob] = result;
+        }
+        else
+        {
+            cache.set(root, {[doRob]: result});
+        }
+
         return result;
     }
-    const leftResult = helper(root.left);
-    const rightResult = helper(root.right);
-    // 当前的没抢，子树可抢可不抢
-    result[0] = Math.max(...leftResult) + Math.max(...rightResult);
-    // 当前的抢了，子树不能抢
-    result[1] = root.val + leftResult[0] + rightResult[0];
-    return result;
-}
+
+    return Math.max(
+        helper(root, true),
+        helper(root, false)
+    );
+};
+
 // @lc code=end
 
