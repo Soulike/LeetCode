0c9ee9443b46727dd9238142aaa7ba44803f7527
Soulike <soulike@qq.com>
2022-02-21 12:49:49 +0800

    650

--- 650.2-keys-keyboard.js
+++ 650.2-keys-keyboard.js
@@ -41,34 +41,42 @@ var minSteps = function (n)
      * 返回 dp[1][0]
      */
 
-    const dp = new Array(n + 1);
-    for (let i = 0; i < dp.length; i++)
-    {
-        dp[i] = new Array(n + 1);
-    }
-    dp[n].fill(0);
+    /**
+     * 优化
+     * dp[i] 输入 i 个字符所需的最小操作次数
+     * 
+     * base case
+     * dp[1] = 0 
+     * 
+     * k from 1 to i-1
+     * if (i % k === 0)
+         min = Math.min(min,
+                dp[k]   // 得到被复制部分
+                + 1     // 复制
+                + (i / k - 1) // 粘贴这么多次
+            );
+     * 
+     * 返回 dp[n]
+     */
 
-    for (let i = n - 1; i >= 1; i--)
+    const dp = new Array(n + 1);
+    dp[1] = 0;
+
+    for (let i = 2; i <= n; i++)
     {
-        for (let j = n; j >= 0; j--)
+        let min = Infinity;
+        for (let k = 1; k <= i - 1; k++)
         {
-            if (j === 0)
+            if (i % k === 0)
             {
-                dp[i][j] = 1 + dp[i][i];
-            }
-            else if (j < i)
-            {
-                dp[i][j] = 1 + Math.min(dp[i][i],
-                    i + j > n ? Infinity : dp[i + j][j]);
-            }
-            else
-            {
-                dp[i][j] = 1 +
-                    (i + j > n ? Infinity : dp[i + j][j]);
+                min = Math.min(min,
+                    dp[k] + i / k 
+                );
             }
         }
+        dp[i] = min;
     }
 
-    return dp[1][0];
+    return dp[n];
 };
 // @lc code=end
