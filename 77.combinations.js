88da22d97748ee5e255ffa62a4576b78e7da845a
Soulike <soulike@qq.com>
2022-02-23 13:41:31 +0800

    77

--- 77.combinations.js
+++ 77.combinations.js
@@ -9,62 +9,32 @@
  * @param {number} n
  * @param {number} k
  * @return {number[][]}
- * 基本思路：递归算法，从小到大，从范围中拿掉一个数 n，从比 n 大的剩余数中递归得到所有取出 k-1 个数的结果数组 recursiveResult，再把当前数放进 recursiveResult 中的每个结果数组中。
  */
 const combine = function (n, k) 
 {
-    return helper(1, n - 1, k);
-};
+    const results = [];
+    let current = [];
 
-/**
- * @description 组合范围改成 [base...base+n]
- * @param {number} base
- * @param {number} n
- * @param {number} k
- * @return {number[][]}
- */
-function helper(base, n, k)
-{
-    if (k === 0)
+    function backtrack(startIndex)
     {
-        return [];
-    }
-    /**@type {number[][]} */
-    const result = [];
-    const target = base + n;
-    // k<=n+1
-    if (k === n + 1)  // 注意 [base...base+n] 中有 n+1 个数
-    {
-        result.push([]);
-        for (let i = base; i <= target; i++)
+        if (current.length === k)
         {
-            result[0].push(i);
+            results.push([...current]);
         }
-    }
-    else if (k === 1)   // k < n+1
-    {
-        for (let i = base; i <= target; i++)
+        else
         {
-            result.push([i]);
-        }
-    }
-    else    // 1 < k < n+1
-    {
-        let recursiveResult = [];
-        for (let i = 0; i <= n; i++)
-        {
-            // 注意递归后实际有 n - i - 1 + 1 个数字，因此检查 n - i >= k - 1
-            if (n - i >= k - 1)
+            for (let i = startIndex; i <= n; i++)
             {
-                // base+n 的值是不变的
-                recursiveResult = helper(base + i + 1, n - i - 1, k - 1);
-                for (const array of recursiveResult)
-                {
-                    result.push([base + i, ...array]);
-                }
+                current.push(i);
+                backtrack(i + 1);
+                current.pop();
             }
         }
     }
-    return result;
-}
+
+    backtrack(1);
+
+    return results;
+};
+
 // @lc code=end
