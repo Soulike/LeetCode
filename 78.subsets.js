f77284ca87dd69cd49d39a6f2eb488fb75e797b7
Soulike <soulike@qq.com>
2022-02-23 13:33:11 +0800

    78

--- 78.subsets.js
+++ 78.subsets.js
@@ -11,39 +11,29 @@
  */
 const subsets = function (nums) 
 {
-    /**@type {number[][]} */
-    const results = [[]];
-    /**@type {number[][]} */
-    let lastResults = [];   // 上一轮迭代的结果
-    /**@type {number[][]} */
-    let currentResults = [];    // 这一轮迭代的结果
-    const LENGTH = nums.length;
-    for (let i = 1; i <= LENGTH; i++)   // i 是这一次迭代结果中所有 number 数组的长度
+    let currentSubSet = [];
+    const subSets = [];
+
+    function backtrack(index)
     {
-        if (i === 1)    // 特殊情况，单独处理
+        if (index === nums.length)
         {
-            currentResults = nums.map(num => [num]);
+            subSets.push([...currentSubSet]);
         }
-        else    // i > 1
+        else
         {
-            // 遍历上一次迭代结果，在其中添加一个数字
-            for (const result of lastResults)
-            {
-                // 集合始终满足从小到大顺序，防止重复
-                for (const num of nums)
-                {
-                    if (num < result[0])    // i>1，result[0] 一定存在
-                    {
-                        currentResults.push([num, ...result]);
-                    }
-                }
-            }
+            // 要当前这个数字
+            currentSubSet.push(nums[index]);
+            backtrack(index + 1);
+            currentSubSet.pop();
+
+            // 不要当前这个数字
+            backtrack(index + 1);
         }
-        // 把这一轮的结果保存下来，重置 currentResults
-        results.push(...currentResults);
-        lastResults = currentResults;
-        currentResults = [];
     }
-    return results;
+
+    backtrack(0);
+
+    return subSets;
 };
 // @lc code=end
