5eb50c48a65ba281a77f89111a64cd1d0c7ea762
Soulike <soulike@qq.com>
2022-02-24 12:25:34 +0800

    204

---
+++ 204.count-primes.js
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=204 lang=javascript
+ *
+ * [204] Count Primes
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function (n)
+{
+    if (n <= 2)
+    {
+        return 0;
+    }
+    const arr = new Array(n);
+    arr.fill(true);
+    let count = n - 2;
+
+    for (let i = 2; i < Math.sqrt(n); i++)
+    {
+        if (arr[i])
+        {
+            for (let j = i; i * j < n; j++)
+            {
+                const index = i * j;
+                if (arr[index])
+                {
+                    arr[index] = false;
+                    count--;
+                }
+            }
+        }
+    }
+
+    return count;
+};
+// @lc code=end
+
+countPrimes(3);
