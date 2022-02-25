b6b754d36d6904487b953c34e34661c8ce11852f
Soulike <soulike@qq.com>
2022-02-25 20:38:37 +0800

    877

--- 877.stone-game.js
+++ 877.stone-game.js
@@ -12,51 +12,36 @@
 const stoneGame = function (piles)
 {
     /**
-     * dp[i][j] = [
-     *  范围 [i,j] 的石子堆，先手拿最多能拿到多少石子
-     *  范围 [i,j] 的石子堆，后手拿最多能拿到多少石子
-     * ]
+     * dp[i][j] 当面对 [i,j] 的石头堆时，先手拿可以比对手多拿多少石头
      * 
-     * base case
-     * dp[i][i] = [piles[i],0]
+     * base case 
+     * dp[i][i] = piles[i]
      * 
-     * 
-     * 返回 dp[0][piles.length-1][0] > dp[0][piles.length-1][1]
+     * dp[i][j] = Math.max(
+     *  piles[i] - dp[i+1][j],
+     *  piles[j] - dp[i][j-1]
+     * )
      */
 
-    const dp = new Array(piles.length);
-    for (let i = 0; i < dp.length; i++)
+    const n = piles.length;
+    const dp = new Array(n);
+    for (let i = 0; i < n; i++)
     {
-        dp[i] = new Array(piles.length);
-        for (let j = 0; j < dp[i].length; j++)
-        {
-            dp[i][j] = new Array(2);
-        }
-        dp[i][i][0] = piles[i];
-        dp[i][i][1] = 0;
+        dp[i] = new Array(n);
+        dp[i][i] = piles[i];
     }
 
-    for (let i = piles.length - 2; i >= 0; i--)
+    for (let i = n - 2; i >= 0; i--)
     {
-        for (let j = i + 1; j < piles.length; j++)
+        for (let j = i + 1; j < n; j++)
         {
-            // 拿开头
-            const left = piles[i] + dp[i + 1][j][1];
-            // 拿结尾
-            const right = piles[j] + dp[i][j - 1][1];
-            if (left > right)   // 先手肯定要最大的
-            {
-                dp[i][j][0] = left;
-                dp[i][j][1] = dp[i + 1][j][0];  // 左边肯定被拿走了，相当于右边先手
-            }
-            else
-            {
-                dp[i][j][0] = right;
-                dp[i][j][1] = dp[i][j-1][0];
-            }
+            dp[i][j] = Math.max(
+                piles[i] - dp[i + 1][j],
+                piles[j] - dp[i][j - 1]
+            );
         }
     }
 
-    return dp[0][piles.length - 1][0] > dp[0][piles.length - 1][1];
+    return dp[0][n - 1] > 0;
 };
 // @lc code=end
