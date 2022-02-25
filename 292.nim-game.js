696db1fc3384737bd739ee9dca9d5f8f52926a2c
Soulike <soulike@qq.com>
2022-02-25 20:33:09 +0800

    292

---
+++ 292.nim-game.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=292 lang=javascript
+ *
+ * [292] Nim Game
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var canWinNim = function (n)
+{
+    return n % 4 !== 0;
+};
+// @lc code=end
