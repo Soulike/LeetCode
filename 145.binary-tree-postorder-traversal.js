e7b2fd34a2a09bb558b90d931ef929731a4fdbbb
Soulike <soulike@qq.com>
2022-03-02 20:06:28 +0800

    145

--- 145.binary-tree-postorder-traversal.js
+++ 145.binary-tree-postorder-traversal.js
@@ -3,58 +3,61 @@
  *
  * [145] Binary Tree Postorder Traversal
  */
+function TreeNode(val, left, right)
+{
+    this.val = (val === undefined ? 0 : val);
+    this.left = (left === undefined ? null : left);
+    this.right = (right === undefined ? null : right);
+}
 
 // @lc code=start
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
+
 /**
  * @param {TreeNode} root
  * @return {number[]}
  */
 var postorderTraversal = function (root)
 {
-    const postorder = [];
+    const result = [];
 
+    /**@type {TreeNode[]} */
     const stack = [];
-    let visited = new TreeNode();
 
-    function traverseLeft(root)
-    {
-        while (root !== null)
-        {
-            // 先序遍历
-            stack.push(root);
-            root = root.left;
-        }
-    }
+    traverseLeft(root, stack);
 
-    traverseLeft(root);
+    let visited = null;
 
     while (stack.length > 0)
     {
+        // 中序遍历代码
         const top = stack[stack.length - 1];
-        if ((top.left === null || top.left === visited)
-            && top.right !== visited)
+        if (top.right !== visited && top.right !== null)
         {
-            // 中序遍历
-            traverseLeft(top.right);
+            traverseLeft(top.right, stack);
         }
-
-        if (top.right === null || top.right === visited)
+        else
         {
-            // 后序遍历
-            postorder.push(top.val);
             visited = stack.pop();
+            // 后续遍历代码
+            result.push(top.val);
         }
     }
 
-    return postorder;
+    return result;
 };
-// @lc code=end
 
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode[]} stack 
+ */
+function traverseLeft(root, stack)
+{
+    let currentNode = root;
+    while (currentNode !== null)
+    {
+        // 前序遍历代码
+        stack.push(currentNode);
+        currentNode = currentNode.left;
+    }
+}
+// @lc code=end
