022c23c1e79d3dff19fbb6310582f160ed267ce4
Soulike <soulike@qq.com>
2022-03-02 20:59:50 +0800

    912

--- 912.sort-an-array.js
+++ 912.sort-an-array.js
@@ -11,76 +11,50 @@
  */
 const sortArray = function (nums)
 {
-    mergeSort(nums);
+    quickSort(nums);
     return nums;
 };
 
-function mergeSort(nums)
+function quickSort(nums)
 {
-    helper(0, nums.length - 1);
-
     function helper(left, right)
     {
-        if (left === right)
+        if (left >= right)
         {
             return;
         }
 
-        const start1 = left;
-        const end1 = Math.floor((left + right) / 2);
-        const start2 = end1 + 1;
-        const end2 = right;
+        const leftCopy = left;
+        const rightCopy = right;
+        const randomIndex = left + Math.round(Math.random() * (right - left));
 
-        helper(start1, end1);
-        helper(start2, end2);
-        merge(start1, end1, end2);
+        [nums[leftCopy], nums[randomIndex]] = [nums[randomIndex], nums[leftCopy]];
+        const comparedNum = nums[leftCopy];
+
+        while (left < right)
+        {
+            while (nums[right] >= comparedNum && left < right)
+            {
+                right--;
+            }
+
+            while (nums[left] <= comparedNum && left < right)
+            {
+                left++;
+            }
+
+            [nums[left], nums[right]] = [nums[right], nums[left]];
+        }
+
+        [nums[leftCopy], nums[right]] = [nums[right], nums[leftCopy]];
+
+        helper(leftCopy, right - 1);
+        helper(right + 1, rightCopy);
     }
 
-    function merge(start1, end1, end2)
-    {
-        const n = end2 - start1 + 1;
-        const arr = new Array(n);
-        const start2 = end1 + 1;
-
-        let currentIndex = 0;
-        let currentIndex1 = start1;
-        let currentIndex2 = start2;
-
-        while (currentIndex1 <= end1 && currentIndex2 <= end2)
-        {
-            if (nums[currentIndex1] < nums[currentIndex2])
-            {
-                arr[currentIndex] = nums[currentIndex1];
-                currentIndex1++
-            }
-            else
-            {
-                arr[currentIndex] = nums[currentIndex2];
-                currentIndex2++;
-            }
-            currentIndex++;
-        }
-
-        while (currentIndex1 <= end1)
-        {
-            arr[currentIndex] = nums[currentIndex1];
-            currentIndex1++
-            currentIndex++;
-        }
-
-        while (currentIndex2 <= end2)
-        {
-            arr[currentIndex] = nums[currentIndex2];
-            currentIndex2++;
-            currentIndex++;
-        }
-
-        for (let i = 0; i < n; i++)
-        {
-            nums[start1 + i] = arr[i];
-        }
-    }
+    helper(0, nums.length - 1);
 }
+
 // @lc code=end
 
-console.log(sortArray([3,2,1]))
+console.log(sortArray([3,5,8,7,5]));
