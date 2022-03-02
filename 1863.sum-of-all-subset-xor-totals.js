5e822e8e470f36d044939d7e659289841315e96b
Soulike <soulike@qq.com>
2022-03-02 12:55:45 +0800

    1863

---
+++ 1863.sum-of-all-subset-xor-totals.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=1863 lang=javascript
+ *
+ * [1863] Sum of All Subset XOR Totals
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var subsetXORSum = function (nums)
+{
+    let currentXORSum = 0;
+    let result = 0;
+
+    function backtrack(startIndex)
+    {
+        result += currentXORSum;
+
+        for (let i = startIndex; i < nums.length; i++)
+        {
+            currentXORSum ^= nums[i];
+            backtrack(i + 1);
+            currentXORSum ^= nums[i];
+        }
+    }
+
+    backtrack(0);
+
+    return result;
+};
+// @lc code=end
