d2e978fa7467f9b4811b4e7d936f71b1b1a1237e
Soulike <soulike@qq.com>
2022-03-02 10:04:20 +0800

    40

--- 40.combination-sum-ii.js
+++ 40.combination-sum-ii.js
@@ -13,45 +13,37 @@
 const combinationSum2 = function (candidates, target) 
 {
     candidates.sort((a, b) => a - b);
-    return helper(candidates, candidates.length - 1, [], target);
+
+    const results = [];
+    const current = [];
+
+    function backtrack(startIndex, target)
+    {
+        if (target === 0)
+        {
+            results.push([...current]);
+        }
+        else if (target < 0)
+        {
+            return;
+        }
+        else
+        {
+            for (let i = startIndex; i < candidates.length; i++)
+            {
+                if (i === startIndex
+                    || candidates[i - 1] !== candidates[i])
+                {
+                    current.push(candidates[i]);
+                    backtrack(i + 1, target - candidates[i]);
+                    current.pop();
+                }
+            }
+        }
+    }
+
+    backtrack(0, target);
+
+    return results;
 };
-
-/**
- * @param {number[]} candidates
- * @param {number} candidatesRight
- * @param {number[]} lastArray
- * @param {number} target
- * @return {number[][]}
- */
-function helper(candidates, candidatesRight, lastArray, target)
-{
-    if (target === 0)
-    {
-        return [lastArray];
-    }
-    if (candidatesRight === -1)
-    {
-        return [];
-    }
-    // target > 0
-    /**@type {number[][]} */
-    const result = [];
-
-    let currentNum = 0;
-    let lastNum = -1;
-    for (let i = candidatesRight; i >= 0; i--)
-    {
-        currentNum = candidates[i];
-        if (i < candidatesRight)
-        {
-            lastNum = candidates[i + 1];
-        }
-        if (currentNum <= target && currentNum !== lastNum)
-        {
-            result.push(
-                ...helper(candidates, i-1, [...lastArray, currentNum], target - currentNum));
-        }
-    }
-    return result;
-}
 // @lc code=end
