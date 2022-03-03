65158f4c7dd8201e6a70acc49f375441d049bc93
Soulike <soulike@qq.com>
2022-03-03 15:58:56 +0800

    1528

---
+++ 1528.shuffle-string.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=1528 lang=javascript
+ *
+ * [1528] Shuffle String
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function (s, indices)
+{
+    const result = [];
+    for (let i = 0; i < indices.length; i++)
+    {
+        result[indices[i]] = s[i];
+    }
+
+    return result.join('');
+};
+// @lc code=end
+
