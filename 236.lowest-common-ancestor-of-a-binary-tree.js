834fa348e2571938200584ba7b4a656b69f45f33
Soulike <soulike@qq.com>
2022-03-03 10:28:12 +0800

    236

--- 236.lowest-common-ancestor-of-a-binary-tree.js
+++ 236.lowest-common-ancestor-of-a-binary-tree.js
@@ -15,6 +15,9 @@ class TreeNode
 
 // @lc code=start
 /**
+ * 如果 LCA 存在于二叉树 root 当中，返回该 LCA
+ * 如果只有 p 或者 q 存在于二叉树 root 当中，返回 p 或者 q
+ * 否则，返回 null
  * @param {TreeNode} root
  * @param {TreeNode} p
  * @param {TreeNode} q
@@ -22,6 +25,32 @@ class TreeNode
  */
 const lowestCommonAncestor = function (root, p, q)
 {
-    
+    if (root === null)
+    {
+        return null;
+    }
+    if (root === p || root === q)
+    {
+        return root;
+    }
+    const left = lowestCommonAncestor(root.left, p, q);
+    const right = lowestCommonAncestor(root.right, p, q);
+
+    // 一边一个，当前的就是 LCA
+    if (left === p && right === q
+        || left === q && right === p)
+    {
+        return root;
+    }
+    // 两边都没有
+    else if (left === null && right === null)
+    {
+        return null;
+    }
+    // 有一侧返回了 LCA，直接返回
+    else
+    {
+        return left ?? right;
+    }
 };
 // @lc code=end
