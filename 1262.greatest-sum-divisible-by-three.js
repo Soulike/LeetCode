d878395b5c9f9f7d083d3a7ee85c11d9477adac2
Soulike <soulike@qq.com>
2022-03-05 16:18:31 +0800

    1262

--- 1262.greatest-sum-divisible-by-three.js
+++ 1262.greatest-sum-divisible-by-three.js
@@ -12,105 +12,32 @@
 var maxSumDivThree = function (nums)
 {
     /**
-     * 求和
-     * 如果是 3 的倍数，返回
-     * 如果余数为 1，删掉最小的一个余数为 1 的数字，或删掉两个余数为 2 的数字
-     * 如果余数为 2，删掉最小的一个余数为 2 的数字，或者删掉两个余数为 1 的数字
+     * dp[i]
+     * 余数为 i 的最大和
+     * 
+     * base case
+     * dp[0] = 0
+     * dp[1] = 0
+     * dp[2] = 0
+     * 
      */
 
-    const sum = nums.reduce((prev, curr) => prev + curr);
+    const dp = [0, 0, 0];
 
-    if (sum % 3 === 0)
+    for (let i = 0; i < nums.length; i++)
     {
-        return sum;
-    }
-    else
-    {
-        nums.sort((a, b) => a - b);
-        if (sum % 3 === 1)
+        const sums = [
+            dp[0] + nums[i],
+            dp[1] + nums[i],
+            dp[2] + nums[i],
+        ];
+        for (const sum of sums)
         {
-            let numWithRemainder1 = 0;
-            let num1WithRemainder2 = 0;
-            let num2WithRemainder2 = 0;
-
-            for (const num of nums)
-            {
-                if (numWithRemainder1 !== 0
-                    && num1WithRemainder2 !== 0
-                    && num2WithRemainder2 !== 0)
-                {
-                    break;
-                }
-                if (num % 3 === 1 && numWithRemainder1 === 0)
-                {
-                    numWithRemainder1 = num;
-                }
-                else if (num % 3 === 2)
-                {
-                    if (num1WithRemainder2 === 0)
-                    {
-                        num1WithRemainder2 = num;
-                    }
-                    else if (num2WithRemainder2 === 0)
-                    {
-                        num2WithRemainder2 = num;
-                    }
-                }
-            }
-
-            if (num2WithRemainder2 === 0)
-            {
-                return sum - numWithRemainder1;
-            }
-            else
-            {
-                return sum - (numWithRemainder1 < num1WithRemainder2 + num2WithRemainder2
-                    ? numWithRemainder1
-                    : num1WithRemainder2 + num2WithRemainder2);
-            }
-        }
-        else if (sum % 3 === 2)
-        {
-            let numWithRemainder2 = 0;
-            let num1WithRemainder1 = 0;
-            let num2WithRemainder1 = 0;
-
-            for (const num of nums)
-            {
-                if (numWithRemainder2 !== 0
-                    && num1WithRemainder1 !== 0
-                    && num2WithRemainder1 !== 0)
-                {
-                    break;
-                }
-                if (num % 3 === 2 && numWithRemainder2 === 0)
-                {
-                    numWithRemainder2 = num;
-                }
-                else if (num % 3 === 1)
-                {
-                    if (num1WithRemainder1 === 0)
-                    {
-                        num1WithRemainder1 = num;
-                    }
-                    else if (num2WithRemainder1 === 0)
-                    {
-                        num2WithRemainder1 = num;
-                    }
-                }
-            }
-
-            if (num2WithRemainder1 === 0)
-            {
-                return sum - numWithRemainder2;
-            }
-            else
-            {
-                return sum - (numWithRemainder2 < num1WithRemainder1 + num2WithRemainder1
-                    ? numWithRemainder2
-                    : num1WithRemainder1 + num2WithRemainder1);
-            }
+            const remainder = sum % 3;
+            dp[remainder] = Math.max(dp[remainder], sum);
         }
     }
+
+    return dp[0];
 };
 // @lc code=end
