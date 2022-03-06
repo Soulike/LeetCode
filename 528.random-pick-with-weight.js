f28ef4875e53559213c0ef2c91a056a2a6c3c6ce
Soulike <soulike@qq.com>
2022-03-06 22:52:30 +0800

    528

--- 528.random-pick-with-weight.js
+++ 528.random-pick-with-weight.js
@@ -14,7 +14,7 @@ class Solution
 
     constructor(w)
     {
-        this.preSum = [0];
+        this.preSum = [];
 
         // 累加和
         w.reduce((prev, curr) =>
@@ -29,6 +29,8 @@ class Solution
     pickIndex()
     {
         // 扔石头，看扔到哪个区间里面，用二分搜索找到这个区间的右边界
+        // 这里从 1 开始扔是因为我们取右边界
+        // 比如第一个数的比重是 100，第二个是 10，我们能取到 [1,100] 这 100 个数和 [101,110] 这 10 个数，这才是公平的
         const randomNum = this.generateRandom(1, this.preSum[this.preSum.length - 1]);
 
         let leftIndex = 0;
@@ -40,9 +42,10 @@ class Solution
             const midPreSum = this.preSum[midIndex];
             if (midPreSum >= randomNum)
             {
-                if (this.preSum[midIndex - 1] < randomNum)
+                if (midIndex === 0
+                    || this.preSum[midIndex - 1] < randomNum)
                 {
-                    return midIndex - 1;
+                    return midIndex;
                 }
                 else
                 {
@@ -69,3 +72,7 @@ class Solution
  * var param_1 = obj.pickIndex()
  */
 // @lc code=end
+
+const solution = new Solution([1,3,2,1]);
+
+console.log(solution.pickIndex());
