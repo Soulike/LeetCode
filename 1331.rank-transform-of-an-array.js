df2f9ea4c039425a77eab9c1a896927cc760df8a
Soulike <soulike@qq.com>
2022-03-09 22:27:56 +0800

    1331

---
+++ 1331.rank-transform-of-an-array.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=1331 lang=javascript
+ *
+ * [1331] Rank Transform of an Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var arrayRankTransform = function (arr)
+{
+    const nums = [...new Set(arr)];
+    nums.sort((a, b) => a - b);
+    const numToRank = new Map(
+        nums.map((val, index) => [val, index + 1])
+    );
+
+    const result = new Array(arr.length);
+    for (let i = 0; i < arr.length; i++)
+    {
+        result[i] = numToRank.get(arr[i]);
+    }
+
+    return result;
+};
+// @lc code=end
+
