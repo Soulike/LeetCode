326963e24b7cf83c8cb9a3433a905a56c70ad4e0
Soulike <soulike@qq.com>
2022-03-09 21:55:16 +0800

    1403

---
+++ 1403.minimum-subsequence-in-non-increasing-order.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=1403 lang=javascript
+ *
+ * [1403] Minimum Subsequence in Non-Increasing Order
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var minSubsequence = function (nums)
+{
+    const sum = nums.reduce((prev, curr) => prev + curr);
+    const halfSum = sum / 2;
+
+    nums.sort((a, b) => b - a);
+
+    let currentSum = 0;
+    let seq = [];
+
+    for (const num of nums)
+    {
+        currentSum += num;
+        seq.push(num);
+        if (currentSum > halfSum)
+        {
+            return seq;
+        }
+    }
+};
+// @lc code=end
