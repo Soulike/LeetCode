4f8f36e6a785afdae46d807a837daf16fb0efcbb
Soulike <soulike@qq.com>
2022-03-09 13:58:51 +0800

    1784

--- 1784.check-if-binary-string-has-at-most-one-segment-of-ones.js
+++ 1784.check-if-binary-string-has-at-most-one-segment-of-ones.js
@@ -11,47 +11,6 @@
  */
 var checkOnesSegment = function (s)
 {
-    let left = 0;
-    let right = 0;
-
-    for (let i = 0; i <= s.length; i++)
-    {
-        if (i === s.length || s[i] === '1')
-        {
-            left = i;
-            right = i;
-            break;
-        }
-    }
-
-    let segmentCount = 0;
-
-    while (left < s.length)
-    {
-        while (right < s.length && s[right] === '1')
-        {
-            right++;
-        }
-
-        segmentCount++;
-        if (segmentCount === 2)
-        {
-            return false;
-        }
-
-        left = right;
-
-        for (let i = left; i <= s.length; i++)
-        {
-            if (i === s.length || s[i] === '1')
-            {
-                left = i;
-                right = i;
-                break;
-            }
-        }
-    }
-
-    return segmentCount === 1;
+    return !s.includes('01');
 };
 // @lc code=end
