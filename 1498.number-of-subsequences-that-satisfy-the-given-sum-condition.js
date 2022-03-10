f221d778fdda27a6b637de356890e521da512af8
Soulike <soulike@qq.com>
2022-03-10 13:53:35 +0800

    1498

---
+++ 1498.number-of-subsequences-that-satisfy-the-given-sum-condition.js
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode id=1498 lang=javascript
+ *
+ * [1498] Number of Subsequences That Satisfy the Given Sum Condition
+ */
+
+// @lc code=start
+const MOD = 10 ** 9 + 7;
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var numSubseq = function (nums, target)
+{
+    nums.sort((a, b) => a - b);
+    let count = 0;
+
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right)
+    {
+        if (nums[left] + nums[right] > target)
+        {
+            right--;
+        }
+        else
+        {
+            count += mod2Pow(right - left);
+            count %= MOD;
+            left++;
+        }
+    }
+
+    return count;
+};
+
+const cache = new Map();
+function mod2Pow(exp)
+{
+    if (exp === 0)
+    {
+        return 1;
+    }
+
+    if (cache.has(exp))
+    {
+        return cache.get(exp);
+    }
+    const result = (mod2Pow(exp - 1) * 2) % MOD;
+    cache.set(exp, result);
+    return result;
+}
+// @lc code=end
+
