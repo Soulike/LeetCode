f70dc367b12b34c42829fbc1b29d15ad9b64e541
Soulike <soulike@qq.com>
2022-03-10 12:31:42 +0800

    515

---
+++ 1461.check-if-a-string-contains-all-binary-codes-of-size-k.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1461 lang=javascript
+ *
+ * [1461] Check If a String Contains All Binary Codes of Size K
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {boolean}
+ */
+var hasAllCodes = function(s, k) {
+    
+};
+// @lc code=end
+
---
+++ 1825.finding-mk-average.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=1825 lang=javascript
+ *
+ * [1825] Finding MK Average
+ */
+
+// @lc code=start
+/**
+ * @param {number} m
+ * @param {number} k
+ */
+var MKAverage = function(m, k) {
+    
+};
+
+/** 
+ * @param {number} num
+ * @return {void}
+ */
+MKAverage.prototype.addElement = function(num) {
+    
+};
+
+/**
+ * @return {number}
+ */
+MKAverage.prototype.calculateMKAverage = function() {
+    
+};
+
+/** 
+ * Your MKAverage object will be instantiated and called as such:
+ * var obj = new MKAverage(m, k)
+ * obj.addElement(num)
+ * var param_2 = obj.calculateMKAverage()
+ */
+// @lc code=end
+
