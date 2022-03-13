1c6d93ace37bc01946b213dcd4c6ef6580a20e03
Soulike <soulike@qq.com>
2022-03-13 13:10:34 +0800

    1227

---
+++ 1227.airplane-seat-assignment-probability.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1227 lang=javascript
+ *
+ * [1227] Airplane Seat Assignment Probability
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var nthPersonGetsNthSeat = function (n)
+{
+    return n === 1 ? 1 : 0.5;
+};
+// @lc code=end
+
