1476672467da0d2cf769599af1ed3fc58d82f328
Soulike <soulike@qq.com>
2022-03-16 09:38:45 +0800

    946

---
+++ 1203.sort-items-by-groups-respecting-dependencies.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=1203 lang=javascript
+ *
+ * [1203] Sort Items by Groups Respecting Dependencies
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @param {number} m
+ * @param {number[]} group
+ * @param {number[][]} beforeItems
+ * @return {number[]}
+ */
+var sortItems = function(n, m, group, beforeItems) {
+    
+};
+// @lc code=end
+
---
+++ 946.validate-stack-sequences.js
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=946 lang=javascript
+ *
+ * [946] Validate Stack Sequences
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} pushed
+ * @param {number[]} popped
+ * @return {boolean}
+ */
+var validateStackSequences = function (pushed, popped)
+{
+    /**
+     * 从 pushed[0] 开始入栈
+     * 每次入栈和出栈，检查 popped[0]
+     *  如果符合，出栈 popped.shift()
+     *  如果不符合，继续入栈
+     *  如果不符合且 pushed 已经用完，返回 false
+     */
+
+    /** @type {number[]} */
+    const stack = [];
+
+    let poppedIndex = 0;
+
+    for (let i = 0; i < pushed.length; i++)
+    {
+        stack.push(pushed[i]);
+
+        while (stack.length > 0
+            && stack[stack.length - 1] === popped[poppedIndex])
+        {
+            poppedIndex++;
+            stack.pop();
+        }
+    }
+
+    return stack.length === 0;
+};
+// @lc code=end
