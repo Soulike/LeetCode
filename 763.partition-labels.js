3571d798c380e3097d79d0ba49d21b937b650162
Soulike <soulike@qq.com>
2022-03-21 10:29:07 +0800

    763

--- 763.partition-labels.js
+++ 763.partition-labels.js
@@ -12,44 +12,39 @@
 const partitionLabels = function (s) 
 {
     /**
-     * 在 'i' 的后面添加一个分隔符，即某一组的最后一个字母的下标
+     * 某一组的最后一个字母的下标
      * @type {number[]} */
     const splitIndexes = [-1];
-    /**@type {Set<string>} */
-    const checkedLetter = new Set();
 
-    // 找到第一个字符
+    /**
+     * 某个字母的最后一个下标
+     * @type {Map<string, number>}
+     */
+    const letterLastIndexes = new Map();
+
     for (let i = 0; i < s.length; i++)
     {
-        const leftChar = s.charAt(i);
-        if (!checkedLetter.has(leftChar))
+        letterLastIndexes.set(s[i], i);
+    }
+
+    for (let i = 0; i < s.length; i++)
+    {
+        // 新的一组
+        if (i > splitIndexes[splitIndexes.length - 1])
         {
-            // 找到最后一个相同字符
-            for (let j = s.length - 1; j >= i; j--)
-            {
-                const rightChar = s.charAt(j);
-                if (leftChar === rightChar)
-                {
-                    if (i <= splitIndexes[splitIndexes.length - 1])
-                    {
-                        if (j > splitIndexes[splitIndexes.length - 1])  // 超出原本范围，扩大分组
-                        {
-                            splitIndexes[splitIndexes.length - 1] = j;
-                        }
-                        break;
-                    }
-                    else    // 不在上一个范围内，创建新分组
-                    {
-                        splitIndexes.push(j);
-                        break;
-                    }
-                }
-            }
+            splitIndexes.push(i);
+        }
+
+        const letterLastIndex = letterLastIndexes.get(s[i]);
+        // 扩充当前分组
+        if (letterLastIndex > splitIndexes[splitIndexes.length - 1])
+        {
+            splitIndexes[splitIndexes.length - 1] = letterLastIndex;
         }
     }
 
     const result = [];
-    for (let i = 0; i < splitIndexes.length-1; i++)
+    for (let i = 0; i < splitIndexes.length - 1; i++)
     {
         result.push(splitIndexes[i + 1] - splitIndexes[i]);
     }
