3c3c228edbb89d94cd7b23e64b385aaea418bc40
Soulike <soulike@qq.com>
2022-03-22 10:24:23 +0800

    1816

---
+++ 1816.truncate-sentence.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=1816 lang=javascript
+ *
+ * [1816] Truncate Sentence
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var truncateSentence = function (s, k)
+{
+    for (let i = 0; i < s.length; i++)
+    {
+        if (s[i] === ' ')
+        {
+            k--;
+            if (k === 0)
+            {
+                return s.slice(0, i);
+            }
+        }
+    }
+    return s;
+};
+// @lc code=end
+
