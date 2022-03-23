d165ad8d6bd5b18a0eff5cd3ced715aab7f9ac1e
Soulike <soulike@qq.com>
2022-03-23 11:33:20 +0800

    991

---
+++ 991.broken-calculator.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=991 lang=javascript
+ *
+ * [991] Broken Calculator
+ */
+
+// @lc code=start
+/**
+ * @param {number} startValue
+ * @param {number} target
+ * @return {number}
+ */
+var brokenCalc = function (startValue, target)
+{
+    let step = 0;
+    while (target > startValue)
+    {
+        step++;
+        if (target % 2 === 0)
+        {
+            target /= 2;
+        }
+        else
+        {
+            target += 1;
+        }
+    }
+
+    return step + startValue - target;
+};
+// @lc code=end
