ca9fc14e0d3b10dd098208d630323e2ccc25b6bb
Soulike <soulike@qq.com>
2022-04-26 11:22:05 +0800

    1513

--- 1513.number-of-substrings-with-only-1-s.js
+++ 1513.number-of-substrings-with-only-1-s.js
@@ -13,62 +13,22 @@ var numSub = function (s)
 {
     const MOD = 10 ** 9 + 7;
     let numSubCount = 0;
-    let leftIndex = -1; // 连续 1 的最左侧下标
-    let rightIndex = -1;    // 连续 1 的最右侧的下一个下标
+    let currentSubCount = 0;
 
-    for (let i = 0; i < s.length; i++)
+    for (const c of s)
     {
-        if (s[i] === '1')
+        if (c === '1')
         {
-            leftIndex = i;
-            rightIndex = i + 1;
-            break;
-        }
-    }
-
-    if (leftIndex === -1)
-    {
-        return 0;
-    }
-
-    while (true)
-    {
-        while (rightIndex < s.length
-            && s[rightIndex] === '1')
-        {
-            rightIndex++;
-        }
-        numSubCount += getSunStrNum(rightIndex - leftIndex) % MOD;
-        numSubCount %= MOD;
-
-        if (rightIndex === s.length)
-        {
-            return numSubCount;
+            currentSubCount++;
+            numSubCount += currentSubCount;
         }
         else
         {
-            leftIndex = rightIndex;
-
-            while (leftIndex < s.length
-                && s[leftIndex] === '0')
-            {
-                leftIndex++;
-            }
-
-            if (leftIndex === s.length)
-            {
-                return numSubCount;
-            }
-            else
-            {
-                rightIndex = leftIndex + 1;
-            }
+            currentSubCount = 0;
         }
     }
+
+    return numSubCount % MOD;
 };
 
-function getSunStrNum(n)
-{
-    return n * (n + 1) / 2;
-}
 // @lc code=end
