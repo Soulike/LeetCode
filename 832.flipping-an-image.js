b4c989ebcac11bcd1db09bddd4269ca78bee8025
Soulike <soulike@qq.com>
2022-05-03 11:15:16 +0800

    832

---
+++ 832.flipping-an-image.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=832 lang=javascript
+ *
+ * [832] Flipping an Image
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} image
+ * @return {number[][]}
+ */
+var flipAndInvertImage = function (image)
+{
+    for (const row of image)
+    {
+        row.reverse();
+        for (let i = 0; i < row.length; i++)
+        {
+            row[i] = ~row[i] & 0b1;
+        }
+    }
+
+    return image;
+};
+// @lc code=end
+
