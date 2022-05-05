71252a2a405126ff74fd1fcba339edd2bee2b0bd
Soulike <soulike@qq.com>
2022-05-05 09:03:52 +0800

    1929

---
+++ 1929.concatenation-of-array.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1929 lang=javascript
+ *
+ * [1929] Concatenation of Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function (nums)
+{
+    return [...nums, ...nums];
+};
+// @lc code=end
+
