27971cbc05e600f4149e3075c37286342c63fec2
Soulike <soulike@qq.com>
2022-05-17 10:01:00 +0800

    1379

---
+++ 1379.find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=1379 lang=javascript
+ *
+ * [1379] Find a Corresponding Node of a Binary Tree in a Clone of That Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} original
+ * @param {TreeNode} cloned
+ * @param {TreeNode} target
+ * @return {TreeNode}
+ */
+var getTargetCopy = function (original, cloned, target)
+{
+    if (original === null)
+    {
+        return null;
+    }
+
+    if (original === target)
+    {
+        return cloned;
+    }
+    else
+    {
+        return getTargetCopy(original.left, cloned.left, target)
+            ?? getTargetCopy(original.right, cloned.right, target)
+    }
+};
+// @lc code=end
+
