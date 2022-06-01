d0bea78ae0384069e83c302d00ec5f109e100258
Soulike <soulike@qq.com>
2022-06-01 10:52:05 +0800

    1480

---
+++ 1480.running-sum-of-1-d-array.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=1480 lang=javascript
+ *
+ * [1480] Running Sum of 1d Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function (nums)
+{
+    for (let i = 1; i < nums.length; i++)
+    {
+        nums[i] += nums[i - 1];
+    }
+    return nums;
+};
+// @lc code=end
+
