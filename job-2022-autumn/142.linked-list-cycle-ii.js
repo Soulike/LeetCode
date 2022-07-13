ef6b6680e94f85596b1ebf90be2f78de7822a491
Soulike <git_anonymous@foxmail.com>
2022-07-13 08:24:14 +0800

    142

---
+++ job-2022-autumn/142.linked-list-cycle-ii.js
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode id=142 lang=javascript
+ *
+ * [142] Linked List Cycle II
+ */
+
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
+// @lc code=start
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode | null}
+ */
+var detectCycle = function (head) {
+    if (head === null) {
+        return null;
+    }
+    let slow = head;
+    let fast = head;
+
+    while (true) {
+        slow = slow.next;
+
+        fast = fast.next;
+        if (fast === null) {
+            return null;
+        }
+        fast = fast.next;
+        if (fast === null) {
+            return null;
+        }
+        if (fast === slow) {
+            break;
+        }
+    }
+    let currentNode = head;
+    while (currentNode !== slow) {
+        currentNode = currentNode.next;
+        slow = slow.next;
+    }
+
+    return currentNode;
+};
+// @lc code=end
