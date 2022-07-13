e22e4c08d3eaa24ad64dd212917aae5b63e4c4c9
Soulike <git_anonymous@foxmail.com>
2022-07-13 08:45:29 +0800

    86

---
+++ job-2022-autumn/86.partition-list.js
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=86 lang=javascript
+ *
+ * [86] Partition List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode | null}
+ */
+var partition = function (head, x) {
+    if (head === null) {
+        return null;
+    }
+    const ltLinkedListHead = new ListNode();
+    const geqLinkedListHead = new ListNode();
+
+    let ltLinkedListCurr = ltLinkedListHead;
+    let geqLinkedListCurr = geqLinkedListHead;
+
+    let curr = head;
+    let next = head.next;
+
+    while (curr !== null) {
+        if (curr.val < x) {
+            ltLinkedListCurr.next = curr;
+            ltLinkedListCurr = curr;
+        } else {
+            geqLinkedListCurr.next = curr;
+            geqLinkedListCurr = curr;
+        }
+        curr.next = null;
+
+        curr = next;
+        next = curr?.next;
+    }
+
+    ltLinkedListCurr.next = geqLinkedListHead.next;
+
+    return ltLinkedListHead.next;
+};
+// @lc code=end
