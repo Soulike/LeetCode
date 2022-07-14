5e2d4c6abfe631442d698d5befb11f7ff7445149
Soulike <git_anonymous@foxmail.com>
2022-07-14 08:00:04 +0800

    206

---
+++ job-2022-autumn/206.reverse-linked-list.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=206 lang=javascript
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode | null} head
+ * @return {ListNode | null}
+ */
+var reverseList = function (head) {
+    if (head === null) {
+        return null;
+    }
+    if (head.next === null) {
+        return head;
+    }
+
+    const restReversedTail = head.next;
+    const restReversedHead = reverseList(head.next);
+    head.next = null;
+    restReversedTail.next = head;
+    return restReversedHead;
+};
+// @lc code=end
