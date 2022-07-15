2d1c39fe99de2b0ba7e96b353e61bd14ea1e2c65
Soulike <git_anonymous@foxmail.com>
2022-07-15 08:37:29 +0800

    27

---
+++ job-2022-autumn/27.remove-element.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=27 lang=javascript
+ *
+ * [27] Remove Element
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+    let left = 0;
+    let right = 0;
+
+    while (right < nums.length) {
+        if (nums[right] !== val) {
+            nums[left] = nums[right];
+            left++;
+        }
+        right++;
+    }
+
+    return left;
+};
+// @lc code=end
