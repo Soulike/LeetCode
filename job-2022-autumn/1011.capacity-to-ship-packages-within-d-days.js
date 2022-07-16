33eecf55c44f74291725df0f473159153c6dbe99
Soulike <git_anonymous@foxmail.com>
2022-07-16 22:13:21 +0800

    1011

---
+++ job-2022-autumn/1011.capacity-to-ship-packages-within-d-days.js
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode id=1011 lang=javascript
+ *
+ * [1011] Capacity To Ship Packages Within D Days
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} weights
+ * @param {number} days
+ * @return {number}
+ */
+var shipWithinDays = function (weights, days) {
+    let capacityLeft = 1;
+    let capacityRight = 5 * 1000 * 500;
+
+    const cache = new Map();
+
+    while (capacityLeft <= capacityRight) {
+        const capacityMid =
+            capacityLeft + Math.floor((capacityRight - capacityLeft) / 2);
+        const currentDays = getDaysByCapacity(capacityMid, weights, cache);
+
+        if (currentDays > days) {
+            capacityLeft = capacityMid + 1;
+        } else if (currentDays < days) {
+            capacityRight = capacityMid - 1;
+        } else {
+            if (
+                capacityMid === 1 ||
+                getDaysByCapacity(capacityMid - 1, weights, cache) > days
+            ) {
+                return capacityMid;
+            } else {
+                capacityRight = capacityMid - 1;
+            }
+        }
+    }
+    return capacityLeft;
+};
+
+/**
+ * When the capacity is set, how many days are needed to ship all cargos
+ * @param {number} capacity
+ * @param {number[]} weights
+ * @param {Map<number, number>} cache
+ * @returns {number}
+ */
+function getDaysByCapacity(capacity, weights, cache) {
+    if (cache.has(capacity)) {
+        return cache.get(capacity);
+    }
+    let days = 0;
+    let leftCapacity = capacity;
+    for (let i = 0; i < weights.length; i++) {
+        if (capacity < weights[i]) {
+            return Infinity;
+        }
+        if (leftCapacity < weights[i]) {
+            days++;
+            leftCapacity = capacity;
+            i--;
+        } else if (leftCapacity >= weights[i]) {
+            leftCapacity -= weights[i];
+        }
+    }
+
+    if (leftCapacity !== capacity) days++;
+
+    cache.set(capacity, days);
+    return days;
+}
+// @lc code=end
+
+console.log(shipWithinDays([1, 2, 3, 1, 1], 4));
