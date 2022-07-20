1c99f6ca11870a79a2aefd596a51be6e787a945d
Soulike <git_anonymous@foxmail.com>
2022-07-20 08:29:16 +0800

    118, 119

---
+++ job-2022-autumn/118.pascals-triangle.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=118 lang=javascript
+ *
+ * [118] Pascal's Triangle
+ */
+
+// @lc code=start
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+    const result = [];
+    let lastRow = [1];
+
+    for (let i = 0; i < numRows; i++) {
+        result.push(lastRow);
+
+        const currentRow = [1];
+        for (let j = 0; j < lastRow.length - 1; j++) {
+            currentRow.push(lastRow[j] + lastRow[j + 1]);
+        }
+        currentRow.push(1);
+        lastRow = currentRow;
+    }
+
+    return result;
+};
+// @lc code=end
---
+++ job-2022-autumn/119.pascals-triangle-ii.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=119 lang=javascript
+ *
+ * [119] Pascal's Triangle II
+ */
+
+// @lc code=start
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function (rowIndex) {
+    const result = new Array(rowIndex + 1);
+    result.fill(0);
+    result[0] = 1;
+
+    for (let i = 1; i <= rowIndex; i++) {
+        for (let j = i; j > 0; j--) {
+            result[j] += result[j - 1];
+        }
+    }
+
+    return result;
+};
+// @lc code=end
