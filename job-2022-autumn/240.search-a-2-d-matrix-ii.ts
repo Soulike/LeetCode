a762365ea28ab2c848ca02af20e9294e60d40e46
Soulike <git_anonymous@foxmail.com>
2022-07-25 18:52:44 +0800

    240, 652

---
+++ job-2022-autumn/240.search-a-2-d-matrix-ii.ts
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=240 lang=typescript
+ *
+ * [240] Search a 2D Matrix II
+ */
+
+// @lc code=start
+function searchMatrix(matrix: number[][], target: number): boolean {
+    const m = matrix.length;
+    const n = matrix[0].length;
+
+    let x = m - 1;
+    let y = 0;
+
+    while (x >= 0 && y < n) {
+        if (matrix[x][y] > target) {
+            x--;
+        } else if (matrix[x][y] < target) {
+            y++;
+        } else {
+            return true;
+        }
+    }
+
+    return false;
+}
+// @lc code=end
---
+++ job-2022-autumn/652.find-duplicate-subtrees.ts
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=652 lang=typescript
+ *
+ * [652] Find Duplicate Subtrees
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function findDuplicateSubtrees(root: TreeNode | null): Array<TreeNode | null> {
+    const traverseResults = new Set<string>();
+    const loggedSubtreeTraverseResults = new Set<string>();
+    const duplicateSubtrees: TreeNode[] = [];
+
+    helper(
+        root,
+        traverseResults,
+        loggedSubtreeTraverseResults,
+        duplicateSubtrees,
+    );
+
+    return duplicateSubtrees;
+}
+
+function helper(
+    root: TreeNode | null,
+    traverseResults: Set<string>,
+    loggedSubtreeTraverseResults: Set<string>,
+    duplicateSubtrees: TreeNode[],
+): string {
+    if (root === null) return '#';
+    const leftChildPostorder = helper(
+        root.left,
+        traverseResults,
+        loggedSubtreeTraverseResults,
+        duplicateSubtrees,
+    );
+    const rightChildPostorder = helper(
+        root.right,
+        traverseResults,
+        loggedSubtreeTraverseResults,
+        duplicateSubtrees,
+    );
+
+    const postorder = [leftChildPostorder, rightChildPostorder, root.val].join(
+        ',',
+    );
+    if (
+        !loggedSubtreeTraverseResults.has(postorder) &&
+        traverseResults.has(postorder)
+    ) {
+        duplicateSubtrees.push(root);
+        loggedSubtreeTraverseResults.add(postorder);
+    }
+    traverseResults.add(postorder);
+    return postorder;
+}
+// @lc code=end
