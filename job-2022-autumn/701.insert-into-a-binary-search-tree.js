915cb2e41634c168ed169e4bdc999089f0fa7bd7
Soulike <git_anonymous@foxmail.com>
2022-07-27 19:03:11 +0800

    701

---
+++ job-2022-autumn/701.insert-into-a-binary-search-tree.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=701 lang=javascript
+ *
+ * [701] Insert into a Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode | null} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var insertIntoBST = function (root, val) {
+    if (root === null) {
+        return new TreeNode(val);
+    }
+
+    if (val < root.val) {
+        root.left = insertIntoBST(root.left, val);
+    } else {
+        root.right = insertIntoBST(root.right, val);
+    }
+
+    return root;
+};
+// @lc code=end
