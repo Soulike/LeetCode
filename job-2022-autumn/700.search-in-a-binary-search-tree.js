f21cc1a414bcca4d3b6ee17e37d03f11ff152182
Soulike <git_anonymous@foxmail.com>
2022-07-27 08:43:44 +0800

    700

---
+++ job-2022-autumn/700.search-in-a-binary-search-tree.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=700 lang=javascript
+ *
+ * [700] Search in a Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode | null} root
+ * @param {number} val
+ * @return {TreeNode | null}
+ */
+var searchBST = function (root, val) {
+    if (root === null || root.val === val) {
+        return root;
+    }
+
+    return searchBST(val < root.val ? root.left : root.right, val);
+};
+// @lc code=end
