38a4c47151a5d9bdfde0128e1eea36d4396347fe
Soulike <git_anonymous@foxmail.com>
2022-07-29 08:37:03 +0800

    207, 210

---
+++ job-2022-autumn/207.course-schedule.js
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode id=207 lang=javascript
+ *
+ * [207] Course Schedule
+ */
+
+// @lc code=start
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function (numCourses, prerequisites) {
+    const onPathNodes = new Set();
+    const cache = new Map();
+    /** @type {number[][]} */
+    const graph = new Array(numCourses);
+    for (let i = 0; i < numCourses; i++) {
+        graph[i] = [];
+    }
+    for (const [to, from] of prerequisites) {
+        graph[from].push(to);
+    }
+
+    for (let i = 0; i < numCourses; i++) {
+        onPathNodes.clear();
+        if (!canTraverseWithoutLoop(i, graph, onPathNodes, cache)) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+/**
+ * Whether the traverse can finish without encountering any loop
+ * @param {number} node
+ * @param {number[][]} graph
+ * @param {Set<number>} onPathNodes
+ * @param {Map<number, boolean>} cache
+ * @returns {boolean}
+ */
+function canTraverseWithoutLoop(node, graph, onPathNodes, cache) {
+    if (cache.has(node)) {
+        return cache.get(node);
+    }
+    if (onPathNodes.has(node)) {
+        cache.set(node, false);
+        return false;
+    }
+    onPathNodes.add(node);
+    const neighbors = graph[node];
+
+    for (const neighbor of neighbors) {
+        if (!canTraverseWithoutLoop(neighbor, graph, onPathNodes, cache)) {
+            cache.set(node, false);
+            return false;
+        }
+    }
+
+    onPathNodes.delete(node);
+    cache.set(node, true);
+    return true;
+}
+// @lc code=end
---
+++ job-2022-autumn/210.course-schedule-ii.js
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode id=210 lang=javascript
+ *
+ * [210] Course Schedule II
+ */
+
+// @lc code=start
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {number[]}
+ */
+var findOrder = function (numCourses, prerequisites) {
+    /**@type {number[][]} */
+    const graph = new Array(numCourses);
+    for (let i = 0; i < numCourses; i++) {
+        graph[i] = [];
+    }
+
+    for (const [to, from] of prerequisites) {
+        graph[from].push(to);
+    }
+
+    const onPathNodes = new Set();
+    const visited = new Set();
+    /** @type {number[]} */
+    const result = [];
+
+    for (let i = 0; i < numCourses; i++) {
+        onPathNodes.clear();
+        if (!postorderTraverse(i, graph, onPathNodes, visited, result)) {
+            return [];
+        }
+    }
+
+    return result.reverse();
+};
+
+/**
+ * Starts from `node`, traverse the nodes that are not traversed before in post order
+ * @param {number} node
+ * @param {number[][]} graph
+ * @param {Set<number>} onPathNodes
+ * @param {Set<number>} visited
+ * @param {number[]} result
+ * @returns {boolean} - Whether the traverse encounters a loop
+ */
+function postorderTraverse(node, graph, onPathNodes, visited, result) {
+    if (onPathNodes.has(node)) {
+        return false;
+    }
+    onPathNodes.add(node);
+
+    if (visited.has(node)) {
+        onPathNodes.delete(node);
+        return true;
+    }
+    visited.add(node);
+
+    const neighbors = graph[node];
+    for (const neighbor of neighbors) {
+        if (!postorderTraverse(neighbor, graph, onPathNodes, visited, result)) {
+            onPathNodes.delete(node);
+            return false;
+        }
+    }
+
+    result.push(node);
+
+    onPathNodes.delete(node);
+    return true;
+}
+// @lc code=end
+
+console.log(
+    findOrder(4, [
+        [1, 0],
+        [2, 0],
+        [3, 1],
+        [3, 2],
+        [0, 3],
+    ]),
+);
