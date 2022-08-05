a0577626b4a7dff128eb11e9d3fcfcf5e7302132
Soulike <git_anonymous@foxmail.com>
2022-08-05 08:50:33 +0800

    216, 377

---
+++ job-2022-autumn/216.combination-sum-iii.js
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode id=216 lang=javascript
+ *
+ * [216] Combination Sum III
+ */
+
+// @lc code=start
+/**
+ * @param {number} k
+ * @param {number} n
+ * @return {number[][]}
+ */
+var combinationSum3 = function (k, n) {
+    if (n > 55) {
+        return [];
+    }
+
+    /**
+     * @param {number} start
+     * @param {number} k
+     * @param {number} n
+     * @returns {number[][]}
+     */
+    const helper = (start, k, n) => {
+        if (k === 1 && n === start) {
+            return [[start]];
+        }
+
+        if (k === 1 && n !== start) {
+            return [];
+        }
+
+        /** @type {number[][]} */
+        const results = [];
+
+        for (let i = start + 1; i <= 9; i++) {
+            const restResults = helper(i, k - 1, n - start);
+
+            for (const restResult of restResults) {
+                restResult.push(start);
+            }
+
+            results.push(...restResults);
+        }
+
+        return results;
+    };
+
+    /** @type {number[][]} */
+    const result = [];
+
+    for (let i = 1; i <= 9; i++) {
+        result.push(...helper(i, k, n));
+    }
+
+    return result;
+};
+// @lc code=end
+
+console.log(JSON.stringify(combinationSum3(4, 30)));
---
+++ job-2022-autumn/377.combination-sum-iv.js
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=377 lang=javascript
+ *
+ * [377] Combination Sum IV
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var combinationSum4 = function (nums, target) {
+    const cache = new Map();
+
+    /**
+     * @param {number} target
+     * @returns {number}
+     */
+    const helper = (target) => {
+        if (cache.has(target)) {
+            return cache.get(target);
+        }
+
+        if (target < 0) {
+            return 0;
+        }
+        if (target === 0) {
+            return 1;
+        }
+
+        let sum = 0;
+
+        for (const num of nums) {
+            sum += helper(target - num);
+        }
+
+        cache.set(target, sum);
+        return sum;
+    };
+
+    return helper(target);
+};
+// @lc code=end
