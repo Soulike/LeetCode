ace20316be4473f64c1cfc9bd97d263fb35b1a8c
Soulike <git_anonymous@foxmail.com>
2022-08-17 08:30:10 +0800

    263, 264, 387

---
+++ job-2022-autumn/263.ugly-number.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=263 lang=javascript
+ *
+ * [263] Ugly Number
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isUgly = function (n) {
+    if (n <= 0) return false;
+    while (n % 2 === 0) n /= 2;
+    while (n % 3 === 0) n /= 3;
+    while (n % 5 === 0) n /= 5;
+    return n === 1;
+};
+// @lc code=end
---
+++ job-2022-autumn/387.first-unique-character-in-a-string.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=387 lang=javascript
+ *
+ * [387] First Unique Character in a String
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function (s) {
+    /** @type {Map<string, number>} */
+    const letterToCount = new Map();
+
+    for (const c of s) {
+        letterToCount.set(c, (letterToCount.get(c) ?? 0) + 1);
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        const count = letterToCount.get(s[i]);
+        if (count === 1) {
+            return i;
+        }
+    }
+
+    return -1;
+};
+// @lc code=end
