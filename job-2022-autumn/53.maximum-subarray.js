ec14a10048a39729678e240db5b5f83baef83d4b
Soulike <git_anonymous@foxmail.com>
2022-08-22 08:46:57 +0800

    53, 931

---
+++ job-2022-autumn/53.maximum-subarray.js
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=53 lang=javascript
+ *
+ * [53] Maximum Subarray
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+    const n = nums.length;
+    /**
+     * subArrayMaxSum[i] 以 i 结尾的最大连续数组和
+     *
+     * base case
+     * subArrayMaxSum[0] = nums[0]
+     *
+     * subArrayMaxSum[i] = subArrayMaxSum[i-1] > 0 ? subArrayMaxSum[i-1] + nums[i] : nums[i]
+     *
+     * 空间压缩
+     *
+     * currentMaxSum = currentMaxSum > 0 ? currentMaxSum + nums[i] : nums[i]
+     * @type {number}
+     */
+    let currentMaxSum = nums[0];
+    let maxSum = nums[0];
+
+    for (let i = 1; i < n; i++) {
+        currentMaxSum = currentMaxSum > 0 ? currentMaxSum + nums[i] : nums[i];
+        maxSum = Math.max(currentMaxSum, maxSum);
+    }
+
+    return maxSum;
+};
+// @lc code=end
---
+++ job-2022-autumn/931.minimum-falling-path-sum.js
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode id=931 lang=javascript
+ *
+ * [931] Minimum Falling Path Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+var minFallingPathSum = function (matrix) {
+    const n = matrix.length;
+
+    /**
+     * dp[i][j] - the min sum of falling from dp[i][j]
+     *
+     * base case
+     * dp[n-1][j] = matrix[n-1][j]
+     *
+     * dp[i][j] = Math.min(dp[i+1][j-1],dp[i+1][j],dp[i+1][j+1]) + matrix[i][j]
+     *
+     * memory compress
+     *
+     * base case
+     * prevDp[j] = matrix[n-1][j]
+     *
+     * dp[j] =  Math.min(prevDp[j-1],prevDp[j],prevDp[j+1]) + matrix[i][j]
+     */
+
+    /** @type {number[]} */
+    let dp = new Array(n);
+    /** @type {number[]} */
+    let prevDp = [...matrix[n - 1]];
+
+    for (let i = n - 2; i >= 0; i--) {
+        for (let j = 0; j < n; j++) {
+            dp[j] =
+                Math.min(
+                    j - 1 >= 0 ? prevDp[j - 1] : Infinity,
+                    prevDp[j],
+                    j + 1 <= n - 1 ? prevDp[j + 1] : Infinity,
+                ) + matrix[i][j];
+        }
+        [prevDp, dp] = [dp, prevDp];
+    }
+
+    return Math.min(...prevDp);
+};
+// @lc code=end
