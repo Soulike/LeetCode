767ead2b4f5502f70978d686cd673acd244a07c5
Soulike <git_anonymous@foxmail.com>
2022-08-24 08:44:35 +0800

    416, 516, 518, 1312

---
+++ job-2022-autumn/416.partition-equal-subset-sum.js
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode id=416 lang=javascript
+ *
+ * [416] Partition Equal Subset Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canPartition = function (nums) {
+    const sum = nums.reduce((prev, curr) => prev + curr);
+    const target = sum / 2;
+    if (!Number.isInteger(target)) {
+        return false;
+    }
+    const n = nums.length;
+
+    /**
+     * dp[i][j] 对于 [0,i] 的物品，是否有一种方法恰好装满容量 j
+     *
+     * base case
+     * dp[i].fill(false)
+     * dp[i][nums[i]] = true
+     * dp[i][0] = true
+     *
+     * dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i]]
+     *
+     * 空间压缩
+     *
+     * base case
+     * prevDp.fill(false)
+     * prevDp[i] = true
+     * prevDp[0] = false
+     *
+     * dp[j] = prevDp[j] || prevDp[j-nums[i]]
+     *
+     */
+    /** @type {boolean[]} */
+    let prevDp = new Array(target + 1);
+    /** @type {boolean[]} */
+    let dp = new Array(target + 1);
+
+    prevDp.fill(false);
+    prevDp[0] = true;
+    prevDp[nums[0]] = true;
+
+    for (let i = 1; i < n; i++) {
+        for (let j = 1; j <= target; j++) {
+            if (j - nums[i] > 0) {
+                dp[j] = prevDp[j] || prevDp[j - nums[i]];
+            } else {
+                dp[j] = prevDp[j];
+            }
+        }
+        [prevDp, dp] = [dp, prevDp];
+    }
+
+    return prevDp[target];
+};
+// @lc code=end
---
+++ job-2022-autumn/518.coin-change-2.js
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=518 lang=javascript
+ *
+ * [518] Coin Change 2
+ */
+
+// @lc code=start
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function (amount, coins) {
+    /**
+     * dp[i][j] 使用 [0,i-1] 的硬币，组成面额 j 有几种方式
+     *
+     * base case
+     * dp[0][j] = 0
+     * dp[i][0] = 1
+     *
+     * dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j]
+     */
+
+    const n = coins.length;
+
+    /** @type {number[][]} */
+    const dp = new Array(n + 1);
+    for (let i = 0; i < dp.length; i++) {
+        dp[i] = new Array(amount + 1);
+        dp[i].fill(0);
+        dp[i][0] = 1;
+    }
+
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= amount; j++) {
+            if (j - coins[i - 1] >= 0) {
+                dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
+            } else {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+
+    return dp[n][amount];
+};
+// @lc code=end
