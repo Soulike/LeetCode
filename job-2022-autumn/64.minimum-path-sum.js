734b5f36d1894cf4d7c35f0cd42e6efdc891a718
Soulike <git_anonymous@foxmail.com>
2022-08-26 08:56:28 +0800

    64, 174, 869

---
+++ job-2022-autumn/64.minimum-path-sum.js
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=64 lang=javascript
+ *
+ * [64] Minimum Path Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minPathSum = function (grid) {
+    /**
+     * dp[i][j] 从 grid[i][j] 出发，到达右下角的最短路径和
+     *
+     * base case
+     * dp[m-1][n-1] = grid[m-1][n-1]
+     *
+     * dp[i][j] = Math.min(dp[i+1][j], dp[i][j+1]) + grid[i][j]
+     *
+     * return dp[0][0]
+     *
+     * 内存优化
+     *
+     * base case
+     * dp[n-1] = grid[m-1][n-1]
+     *
+     * dp[j] = Math.min(dp[j], dp[j+1]) + grid[i][j]
+     */
+
+    const m = grid.length;
+    const n = grid[0].length;
+
+    /** @type {number[]} */
+    let dp = new Array(m);
+
+    dp[n - 1] = grid[m - 1][n - 1];
+
+    for (let i = m - 1; i >= 0; i--) {
+        for (let j = n - 1; j >= 0; j--) {
+            if (i === m - 1 && j === n - 1) {
+                continue;
+            }
+            dp[j] =
+                Math.min(
+                    i + 1 <= m - 1 ? dp[j] : Infinity,
+                    j + 1 <= n - 1 ? dp[j + 1] : Infinity,
+                ) + grid[i][j];
+        }
+    }
+
+    return dp[0];
+};
+// @lc code=end
---
+++ job-2022-autumn/869.reordered-power-of-2.js
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=869 lang=javascript
+ *
+ * [869] Reordered Power of 2
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var reorderedPowerOf2 = function (n) {
+    /**
+     * @param {string} number
+     * @returns {ReadonlyMap<string, number>}
+     */
+    const countDigits = (number) => {
+        /** @type {Map<string, number>} */
+        const result = new Map();
+        for (const digit of number) {
+            result.set(digit, (result.get(digit) ?? 0) + 1);
+        }
+
+        return result;
+    };
+
+    /**
+     * @param {ReadonlyMap<string, number>} map1
+     * @param {ReadonlyMap<string, number>} map2
+     */
+    const mapEqual = (map1, map2) => {
+        if (map1.size !== map2.size) {
+            return false;
+        }
+        for (const [digit, count] of map1) {
+            if (map2.get(digit) !== count) {
+                return false;
+            }
+        }
+
+        return true;
+    };
+
+    const nDigitsCount = countDigits(n.toString());
+
+    for (let i = 1; ; i *= 2) {
+        if (i.toString().length > n.toString().length) {
+            return false;
+        }
+
+        const iDigitsCount = countDigits(i.toString());
+
+        if (mapEqual(nDigitsCount, iDigitsCount)) {
+            return true;
+        }
+    }
+};
+// @lc code=end
