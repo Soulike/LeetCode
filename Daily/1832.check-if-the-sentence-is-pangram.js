420dfe61bfbabf6fa956714eb8b884b8795db7fd
Soulike <soulike@qq.com>
2022-10-17 11:07:32 +0800

    1832

---
+++ Daily/1832.check-if-the-sentence-is-pangram.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=1832 lang=javascript
+ *
+ * [1832] Check if the Sentence Is Pangram
+ */
+
+// @lc code=start
+/**
+ * @param {string} sentence
+ * @return {boolean}
+ */
+var checkIfPangram = function (sentence) {
+    const alphabetSet = new Set();
+    for (const c of sentence) {
+        alphabetSet.add(c);
+        if (alphabetSet.size === 26) {
+            return true;
+        }
+    }
+    return false;
+};
+// @lc code=end
