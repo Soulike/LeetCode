e9b3e1949ffc4f7f7169d7df3981d27198cadb99
Soulike <soulike@qq.com>
2022-12-05 09:39:49 +0800

    876

---
+++ Daily/876.middle-of-the-linked-list.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=876 lang=javascript
+ *
+ * [876] Middle of the Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function (head) {
+    let slow = head;
+    let fast = head;
+
+    while (fast !== null) {
+        fast = fast.next;
+        if (fast === null) break;
+        fast = fast.next;
+        slow = slow.next;
+    }
+
+    return slow;
+};
+// @lc code=end
