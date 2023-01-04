d23f075761d3dbaf7eab31f26b18b7124cd4649d
Soulike <git_anonymous@foxmail.com>
2023-01-04 15:44:19 +0800

    2244

---
+++ Daily/2244.minimum-rounds-to-complete-all-tasks.js
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=2244 lang=javascript
+ *
+ * [2244] Minimum Rounds to Complete All Tasks
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} tasks
+ * @return {number}
+ */
+var minimumRounds = function (tasks) {
+    /**
+     * @param {number} taskCount
+     * @returns {number} - -1 if impossible
+     */
+    const getMinRoundCount = (taskCount) => {
+        if (taskCount === 1) return -1;
+        let roundCount = Math.floor(taskCount / 3);
+
+        if (taskCount % 3 !== 0) roundCount++;
+        return roundCount;
+    };
+
+    /** @type {{[key:number]: number}} */
+    const taskDifficultyToCounts = {};
+    for (const taskDifficulty of tasks) {
+        taskDifficultyToCounts[taskDifficulty] =
+            (taskDifficultyToCounts[taskDifficulty] ?? 0) + 1;
+    }
+
+    /** @type {number[]} */
+    const taskCounts = Array.from(Object.values(taskDifficultyToCounts));
+    taskCounts.sort((a, b) => a - b);
+
+    let minTotalRoundCount = 0;
+    for (const taskCount of taskCounts) {
+        const minRoundCount = getMinRoundCount(taskCount);
+        if (minRoundCount === -1) return -1;
+        minTotalRoundCount += minRoundCount;
+    }
+
+    return minTotalRoundCount;
+};
+// @lc code=end
