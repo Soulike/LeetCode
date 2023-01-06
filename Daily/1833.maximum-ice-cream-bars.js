ebdbc26230f927d2f741b87abe4106e801d8c7e1
Soulike <soulike@qq.com>
2023-01-06 10:16:50 +0800

    1833

---
+++ Daily/1833.maximum-ice-cream-bars.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=1833 lang=javascript
+ *
+ * [1833] Maximum Ice Cream Bars
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} costs
+ * @param {number} coins
+ * @return {number}
+ */
+var maxIceCream = function (costs, coins) {
+    costs.sort((a, b) => a - b);
+    let count = 0;
+    for (const cost of costs) {
+        if (coins >= cost) {
+            coins -= cost;
+            count++;
+        } else break;
+    }
+    return count;
+};
+// @lc code=end
