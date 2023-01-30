940833703cf0bee44b46ff94df279fa644aca3ac
Soulike <soulike@qq.com>
2023-01-30 08:32:23 +0800

    1137

---
+++ Daily/1137.n-th-tribonacci-number.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=1137 lang=javascript
+ *
+ * [1137] N-th Tribonacci Number
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var tribonacci = function (n) {
+    /** @type {Uint32Array} */
+    const last3Numbers = new Uint32Array([0, 1, 1]);
+    if (n < 3) return last3Numbers[n];
+    for (let i = 3; i <= n; i++) {
+        [last3Numbers[0], last3Numbers[1], last3Numbers[2]] = [
+            last3Numbers[1],
+            last3Numbers[2],
+            last3Numbers[0] + last3Numbers[1] + last3Numbers[2],
+        ];
+    }
+
+    return last3Numbers[2];
+};
+// @lc code=end
