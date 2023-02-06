98e64eaed8d147865c9db720c6a2c79b2576d3b4
Soulike <soulike@qq.com>
2023-02-06 09:30:17 +0800

    1470

---
+++ Daily/1470.shuffle-the-array.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=1470 lang=javascript
+ *
+ * [1470] Shuffle the Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function (nums, n) {
+    /** @type {number[]} */
+    const result = [];
+
+    for (let i = 0; i < n; i++) {
+        result.push(nums[i], nums[i + n]);
+    }
+
+    return result;
+};
+// @lc code=end
