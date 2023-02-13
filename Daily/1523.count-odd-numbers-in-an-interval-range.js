6de52db51f10e9e81ca99d415b744f46d7367248
Soulike <soulike@qq.com>
2023-02-13 14:30:09 +0800

    1523

---
+++ Daily/1523.count-odd-numbers-in-an-interval-range.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=1523 lang=javascript
+ *
+ * [1523] Count Odd Numbers in an Interval Range
+ */
+
+// @lc code=start
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countOdds = function (low, high) {
+    if (isEven(low)) {
+        low++;
+    }
+    if (isEven(high)) {
+        high--;
+    }
+
+    return (high - low) / 2 + 1;
+};
+
+/**
+ * @param {number} number
+ * @returns {boolean}
+ */
+function isEven(number) {
+    return (number & 0b1) === 0;
+}
+// @lc code=end
