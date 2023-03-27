6eacb07dec892317106daf871618478b128b4cee
Soulike <soulike@qq.com>
2023-03-27 08:45:58 +0800

    64

--- Daily/64.minimum-path-sum.js
+++ Daily/64.minimum-path-sum.js
@@ -10,50 +10,38 @@
  * @return {number}
  */
 var minPathSum = function (grid) {
+    /**
+     * dp[i][j] the min path sum starting from grid[i][j]
+     *
+     * base case
+     * dp[M-1][N-1] = grid[M-1][N-1]
+     *
+     * dp[i][j] = Math.min(dp[i+1][j], dp[i][j+1]) + grid[i][j]
+     */
+
     const ROW_NUMBER = grid.length;
     const COL_NUMBER = grid[0].length;
 
-    /**  @type {number[][]} */
-    const minPathSumMemo = [];
+    /** @type {number[][]} */
+    const dp = new Array(ROW_NUMBER);
     for (let i = 0; i < ROW_NUMBER; i++) {
-        minPathSumMemo[i] = new Array(COL_NUMBER);
-        minPathSumMemo[i].fill(Infinity);
+        dp[i] = new Array(COL_NUMBER);
     }
 
-    /**
-     * @param {[row: number, col: number]} position
-     * @returns {number}
-     */
-    const getMinPathSumStartFrom = ([i, j]) => {
-        if (i === ROW_NUMBER - 1 && j === COL_NUMBER - 1) {
-            return grid[i][j];
-        }
-        if (minPathSumMemo[i][j] !== Infinity) {
-            return minPathSumMemo[i][j];
-        }
+    dp[ROW_NUMBER - 1][COL_NUMBER - 1] = grid[ROW_NUMBER - 1][COL_NUMBER - 1];
 
-        let minPathSumStartFrom = Infinity;
-        if (j < COL_NUMBER - 1) {
-            const toRightMinPathSum = getMinPathSumStartFrom([i, j + 1]);
-            minPathSumStartFrom = Math.min(
-                minPathSumStartFrom,
-                toRightMinPathSum,
-            );
+    for (let i = ROW_NUMBER - 1; i >= 0; i--) {
+        for (let j = COL_NUMBER - 1; j >= 0; j--) {
+            if (i === ROW_NUMBER - 1 && j === COL_NUMBER - 1) continue;
+
+            dp[i][j] =
+                Math.min(
+                    i + 1 < ROW_NUMBER ? dp[i + 1][j] : Infinity,
+                    j + 1 < COL_NUMBER ? dp[i][j + 1] : Infinity,
+                ) + grid[i][j];
         }
-        if (i < ROW_NUMBER - 1) {
-            const toDownMinPathSum = getMinPathSumStartFrom([i + 1, j]);
-            minPathSumStartFrom = Math.min(
-                minPathSumStartFrom,
-                toDownMinPathSum,
-            );
-        }
+    }
 
-        minPathSumMemo[i][j] = minPathSumStartFrom + grid[i][j];
-        return minPathSumMemo[i][j];
-    };
-
-    const result = getMinPathSumStartFrom([0, 0]);
-
-    return result;
+    return dp[0][0];
 };
 // @lc code=end
