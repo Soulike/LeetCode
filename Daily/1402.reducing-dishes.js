519d906bf8d259230046c2d852e1cfe53d24c004
Soulike <soulike@qq.com>
2023-03-29 09:24:41 +0800

    1402

--- Daily/1402.reducing-dishes.js
+++ Daily/1402.reducing-dishes.js
@@ -13,24 +13,16 @@ var maxSatisfaction = function (satisfaction) {
     satisfaction.sort((a, b) => a - b);
     if (satisfaction[satisfaction.length - 1] <= 0) return 0;
 
-    /**
-     * @type {number[]}
-     * sum of satisfaction[i] to satisfaction[satisfaction.length - 1]
-     */
-    const postfixSum = new Array(satisfaction.length - 1);
-    postfixSum[satisfaction.length - 1] = satisfaction[satisfaction.length - 1];
-
-    for (let i = satisfaction.length - 2; i >= 0; i--) {
-        postfixSum[i] = postfixSum[i + 1] + satisfaction[i];
-    }
+    let currentSatisfactionSuffixSum = satisfaction[satisfaction.length - 1];
 
     let currentCoefficient = satisfaction[satisfaction.length - 1];
     let maxCoefficient = currentCoefficient;
 
     for (let i = satisfaction.length - 2; i >= 0; i--) {
         currentCoefficient =
-            currentCoefficient + postfixSum[i + 1] + satisfaction[i];
+            currentCoefficient + currentSatisfactionSuffixSum + satisfaction[i];
         maxCoefficient = Math.max(currentCoefficient, maxCoefficient);
+        currentSatisfactionSuffixSum += satisfaction[i];
     }
 
     return maxCoefficient;
