388855cf1fb2a85a3101ead38e9c2f60d48f5a22
Soulike <soulike@qq.com>
2023-04-22 10:50:26 +0800

    1312

--- Daily/1312.minimum-insertion-steps-to-make-a-string-palindrome.js
+++ Daily/1312.minimum-insertion-steps-to-make-a-string-palindrome.js
@@ -15,32 +15,32 @@ var minInsertions = function (s) {
      *
      * base case
      * dp[i][i] = 0
-     * dp[i][i+1] = 0 if s[i] === s[j]
      *
      * dp[i][j] =
      * if s[i] === s[j] dp[i][j] = dp[i+1][j-1]
      * else dp[i][j] = 1+Math.min(dp[i+1][j], dp[i][j-1])
      */
     const N = s.length;
-    /** @type {number[][]} */
-    const dp = new Array(N);
-    for (let i = 0; i < dp.length; i++) {
-        dp[i] = [];
-        dp[i][i] = 0;
-    }
+    /** @type {number[]} */
+    let prevDp = [];
+    /** @type {number[]} */
+    let dp = [];
 
     for (let i = N - 1; i >= 0; i--) {
+        dp[i] = 0;
         for (let j = i + 1; j < N; j++) {
             if (s[i] === s[j]) {
-                dp[i][j] = i + 1 === j ? 0 : dp[i + 1][j - 1];
+                dp[j] = i + 1 === j ? 0 : prevDp[j - 1];
             } else {
-                dp[i][j] = 1 + Math.min(dp[i + 1][j], dp[i][j - 1]);
+                dp[j] = 1 + Math.min(prevDp[j], dp[j - 1]);
             }
         }
+
+        [prevDp, dp] = [dp, prevDp];
     }
 
-    return dp[0][N - 1];
+    return prevDp[N - 1];
 };
 // @lc code=end
 
-minInsertions('leetcode');
+minInsertions('zzazz');
