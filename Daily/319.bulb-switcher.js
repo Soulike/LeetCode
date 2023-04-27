3caed74216c72a249480d54f12c2bf8eb8b2d769
Soulike <soulike@qq.com>
2023-04-27 17:03:24 +0800

    319

---
+++ Daily/319.bulb-switcher.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=319 lang=javascript
+ *
+ * [319] Bulb Switcher
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var bulbSwitch = function (n) {
+    return Math.floor(Math.sqrt(n));
+};
+// @lc code=end
