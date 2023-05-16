2fbe9ec5b1babd069575d27c5b3792fa5fc5991d
Soulike <soulike@qq.com>
2023-05-16 10:54:04 +0800

    24

--- Daily/24.swap-nodes-in-pairs.js
+++ Daily/24.swap-nodes-in-pairs.js
@@ -19,13 +19,33 @@
 var swapPairs = function (head) {
     if (head === null || head.next === null) return head;
 
-    const node1 = head;
-    const node2 = head.next;
-    const nextHead = node2.next;
+    const fakeHead = new ListNode();
+    fakeHead.next = head;
 
-    node2.next = node1;
-    node1.next = swapPairs(nextHead);
+    /** @type {ListNode | null} */
+    let node1 = head;
+    /** @type {ListNode | null} */
+    let node2 = head.next;
 
-    return node2;
+    let prevTail = fakeHead;
+    let nextHead = node2.next;
+
+    while (node1 !== null && node2 !== null) {
+        nextHead = node2.next; // remember next starting node
+
+        // swap node1 and node2
+        prevTail.next = node2;
+        node2.next = node1;
+        node1.next = nextHead;
+
+        // remember last tail
+        prevTail = node1;
+
+        // get swapped nodes in next round
+        node1 = prevTail.next;
+        node2 = prevTail.next?.next ?? null;
+    }
+
+    return fakeHead.next;
 };
 // @lc code=end
