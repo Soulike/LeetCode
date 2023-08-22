8a101c2e301d1d08824780b1085505661b8b5081
Soulike <git_anonymous@foxmail.com>
2023-08-22 01:04:14 +0000

    168

---
+++ Daily/168.excel-sheet-column-title.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=168 lang=javascript
+ *
+ * [168] Excel Sheet Column Title
+ */
+
+// @lc code=start
+/**
+ * @param {number} columnNumber
+ * @return {string}
+ */
+var convertToTitle = function (columnNumber) {
+    /** @type {string[]} */
+    const numToLetter = [];
+    for (let i = 0; i < 26; i++) {
+        numToLetter[i] = String.fromCharCode('A'.charCodeAt(0) + i);
+    }
+
+    /** @type {string[]} */
+    const columnName = [];
+
+    while (columnNumber > 0) {
+        columnNumber--;
+        const remainder = columnNumber % 26;
+        columnNumber = (columnNumber - remainder) / 26;
+        columnName.unshift(numToLetter[remainder]);
+    }
+
+    return columnName.join('');
+};
+// @lc code=end
+convertToTitle(701); // ZY
+
+convertToTitle(245234); // MXTB
