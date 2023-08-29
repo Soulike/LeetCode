aad8a7ee2e28e7e8b7e8cfee3a927123c7c56f08
Soulike <git_anonymous@foxmail.com>
2023-08-29 01:04:58 +0000

    2483

--- Daily/2483.minimum-penalty-for-a-shop.js
+++ Daily/2483.minimum-penalty-for-a-shop.js
@@ -13,74 +13,31 @@ var bestClosingTime = function (customers) {
     const CUSTOMERS = 'Y';
     const NO_CUSTOMERS = 'N';
 
-    /** @type {number[]} */
-    const customersHoursBeforeHour = [];
-    /** @type {number[]} */
-    const noCustomerHoursBeforeHour = [];
+    // At the beginning, the shop closes at 0th hour.
 
-    if (customers[0] === CUSTOMERS) {
-        customersHoursBeforeHour[0] = 1;
-        noCustomerHoursBeforeHour[0] = 0;
-    } else if (customers[0] === NO_CUSTOMERS) {
-        customersHoursBeforeHour[0] = 0;
-        noCustomerHoursBeforeHour[0] = 1;
-    }
+    // The penalty of closing the shop at 0th hour.
+    // Since we don't need the actual penalty, initialize currentPenalty to any value is ok.
+    let currentPenalty = 0;
 
-    for (let i = 1; i < customers.length; i++) {
+    let minPenalty = 0;
+    let minPenaltyCloseHour = 0;
+
+    // At (i)th hour, the shop is open, and will close at (i+1)th hour
+    for (let i = 0; i < customers.length; i++) {
         if (customers[i] === CUSTOMERS) {
-            customersHoursBeforeHour[i] = customersHoursBeforeHour[i - 1] + 1;
-            noCustomerHoursBeforeHour[i] = noCustomerHoursBeforeHour[i - 1];
-        } else if (customers[i] === NO_CUSTOMERS) {
-            customersHoursBeforeHour[i] = customersHoursBeforeHour[i - 1];
-            noCustomerHoursBeforeHour[i] = noCustomerHoursBeforeHour[i - 1] + 1;
+            currentPenalty -= 1;
+        } else {
+            currentPenalty += 1;
+        }
+
+        if (currentPenalty < minPenalty) {
+            minPenalty = currentPenalty;
+            minPenaltyCloseHour = i + 1;
         }
     }
 
-    customersHoursBeforeHour[customers.length] =
-        customersHoursBeforeHour[customers.length - 1];
-    noCustomerHoursBeforeHour[customers.length] =
-        noCustomerHoursBeforeHour[customers.length - 1];
-
-    let minPenalty = Infinity;
-    let minPenaltyHour = -1;
-
-    for (let i = 0; i <= customers.length; i++) {
-        const penalty = getPenaltyAtHour(
-            i,
-            customersHoursBeforeHour,
-            noCustomerHoursBeforeHour,
-        );
-        if (penalty < minPenalty) {
-            minPenalty = penalty;
-            minPenaltyHour = i;
-        }
-    }
-
-    return minPenaltyHour;
+    return minPenaltyCloseHour;
 };
-
-/**
- * @param {number} hour
- * @param {number[]} customersHoursBeforeHour
- * @param {number[]} noCustomerHoursBeforeHour
- * @returns {number}
- */
-function getPenaltyAtHour(
-    hour,
-    customersHoursBeforeHour,
-    noCustomerHoursBeforeHour,
-) {
-    const customersHoursBeforeTheHour =
-        hour === 0 ? 0 : customersHoursBeforeHour[hour - 1];
-    const customersHoursAfterTheHour =
-        customersHoursBeforeHour[customersHoursBeforeHour.length - 1] -
-        customersHoursBeforeTheHour;
-
-    const noCustomerHoursBeforeTheHour =
-        hour === 0 ? 0 : noCustomerHoursBeforeHour[hour - 1];
-
-    return noCustomerHoursBeforeTheHour + customersHoursAfterTheHour;
-}
 // @lc code=end
 
 bestClosingTime('NNNN');
