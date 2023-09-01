ade8ba48a91e3c0925b13ee436011c6ca30d4fbd
Soulike <git_anonymous@foxmail.com>
2023-09-01 01:01:12 +0000

    338

--- Daily/338.counting-bits.js
+++ Daily/338.counting-bits.js
@@ -11,29 +11,16 @@
  */
 var countBits = function (n) {
     /** @type {number[]} */
-    let bitCounts = new Array(n + 1);
-    for (let i = n; i >= 0; i--) {
-        bitCounts[i] = getBitCount(i);
+    const bitCounts = new Array(n + 1);
+    bitCounts[0] = 0;
+
+    for (let i = 1; i <= n; i++) {
+        bitCounts[i] =
+            (i & 0b1) === 0b1
+                ? 1 + bitCounts[Math.floor(i / 2)]
+                : bitCounts[i / 2];
     }
+
     return bitCounts;
 };
-
-/** @type {number[]} */
-const memo = [];
-
-/**
- * @param {number} num
- * @returns {number}
- */
-function getBitCount(num) {
-    if (num === 0) return 0;
-    if (memo[num] !== undefined) return memo[num];
-    let bitCount = 0;
-    if ((num & 0b1) === 0b1) {
-        bitCount++;
-    }
-    bitCount += getBitCount(Math.floor(num / 2));
-    memo[num] = bitCount;
-    return bitCount;
-}
 // @lc code=end
