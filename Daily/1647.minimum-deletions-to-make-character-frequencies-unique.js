35e2ccb013bad2c0dfae0ffeb24ee562d8799fff
Soulike <git_anonymous@foxmail.com>
2023-09-12 00:57:02 +0000

    1647

---
+++ Daily/1647.minimum-deletions-to-make-character-frequencies-unique.js
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=1647 lang=javascript
+ *
+ * [1647] Minimum Deletions to Make Character Frequencies Unique
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minDeletions = function (s) {
+    /** @type {Map<string, number>} */
+    const letterToFreq = new Map();
+    for (const c of s) {
+        letterToFreq.set(c, (letterToFreq.get(c) ?? 0) + 1);
+    }
+
+    const letterFreqArr = Array.from(letterToFreq.entries());
+
+    letterFreqArr.sort(([, freq1], [, freq2]) => freq2 - freq1);
+
+    /** @type {Set<number>} */
+    const usedFreqs = new Set();
+    let deleteCount = 0;
+
+    for (let [letter, freq] of letterFreqArr) {
+        while (freq > 0 && usedFreqs.has(freq)) {
+            deleteCount++;
+            freq--;
+        }
+
+        if (freq > 0) {
+            usedFreqs.add(freq);
+        }
+    }
+
+    return deleteCount;
+};
+// @lc code=end
