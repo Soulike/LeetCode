272c43ffe02c4ad2a983f201221bbc6d83e0a1af
Soulike <git_anonymous@foxmail.com>
2023-09-13 12:30:37 +0000

    135

---
+++ Daily/135.candy.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=135 lang=javascript
+ *
+ * [135] Candy
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} ratings
+ * @return {number}
+ */
+var candy = function (ratings) {
+    const N = ratings.length;
+    const candies = new Array(N);
+    candies.fill(1);
+
+    for (let i = 1; i < N; i++) {
+        if (ratings[i - 1] < ratings[i]) {
+            candies[i] = candies[i - 1] + 1;
+        }
+    }
+
+    for (let i = N - 1; i > 0; i--) {
+        if (ratings[i - 1] > ratings[i]) {
+            candies[i - 1] = Math.max(candies[i] + 1, candies[i - 1]);
+        }
+    }
+
+    return candies.reduce((prev, curr) => prev + curr);
+};
+// @lc code=end
