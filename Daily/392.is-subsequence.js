c41f6f40ad04f723ded11d2295a38660cc9ff443
Soulike <git_anonymous@foxmail.com>
2023-09-24 23:30:47 +0000

    392

---
+++ Daily/392.is-subsequence.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=392 lang=javascript
+ *
+ * [392] Is Subsequence
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function (s, t) {
+    let sIndex = 0;
+
+    for (let i = 0; i < t.length; i++) {
+        if (t[i] === s[sIndex]) sIndex++;
+        if (sIndex === s.length) break;
+    }
+
+    return sIndex === s.length;
+};
+// @lc code=end
