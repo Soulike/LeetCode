0cb2f05b269f33b59b62635bfbb387b25488b85a
Soulike <git_anonymous@foxmail.com>
2023-09-24 23:53:16 +0000

    799

---
+++ Daily/799.champagne-tower.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=799 lang=javascript
+ *
+ * [799] Champagne Tower
+ */
+
+// @lc code=start
+/**
+ * @param {number} poured
+ * @param {number} query_row
+ * @param {number} query_glass
+ * @return {number}
+ */
+var champagneTower = function (poured, query_row, query_glass) {
+    /** @type {number[]} */
+    let prevRow = new Array(1);
+    prevRow[0] = poured;
+
+    for (let i = 1; i <= query_row; i++) {
+        /** @type {number[]} */
+        const currentRow = new Array(i + 1);
+        currentRow.fill(0);
+        for (let j = 0; j < i + 1; j++) {
+            if (prevRow[j] > 1) {
+                const oneSideOverflow = (prevRow[j] - 1) / 2;
+                currentRow[j] += oneSideOverflow;
+                currentRow[j + 1] += oneSideOverflow;
+                prevRow[j] = 1;
+            }
+        }
+        prevRow = currentRow;
+    }
+
+    return Math.min(1, prevRow[query_glass]);
+};
+// @lc code=end
