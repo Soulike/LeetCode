fa0ff33d98f645c376b1d01f216d4162f8e8d715
Soulike <git_anonymous@foxmail.com>
2023-11-08 09:27:58 +0800

    2849

---
+++ Daily/2849.determine-if-a-cell-is-reachable-at-a-given-time.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=2849 lang=javascript
+ *
+ * [2849] Determine if a Cell Is Reachable at a Given Time
+ */
+
+// @lc code=start
+/**
+ * @param {number} sx
+ * @param {number} sy
+ * @param {number} fx
+ * @param {number} fy
+ * @param {number} t
+ * @return {boolean}
+ */
+var isReachableAtTime = function (sx, sy, fx, fy, t) {
+    if (sx === fx && sy === fy) {
+        return t !== 1;
+    }
+    const xDiff = Math.abs(sx - fx);
+    const yDiff = Math.abs(sy - fy);
+
+    const minSeconds = Math.max(xDiff, yDiff);
+    return t >= minSeconds;
+};
+// @lc code=end
