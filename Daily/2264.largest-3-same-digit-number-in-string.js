18873539b5cd2b4d22e4f9c6cf9ad81881bda4fb
Soulike <git_anonymous@foxmail.com>
2023-12-05 07:57:21 +0800

    2264

---
+++ Daily/2264.largest-3-same-digit-number-in-string.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=2264 lang=javascript
+ *
+ * [2264] Largest 3-Same-Digit Number in String
+ */
+
+// @lc code=start
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var largestGoodInteger = function (num) {
+    let maxInteger = -1;
+
+    for (let i = 1; i < num.length - 1; i++) {
+        if (num[i - 1] === num[i] && num[i] === num[i + 1]) {
+            maxInteger = Math.max(maxInteger, Number.parseInt(num[i]));
+        }
+    }
+
+    return maxInteger === -1 ? '' : maxInteger.toString().repeat(3);
+};
+// @lc code=end
