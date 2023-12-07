0b577bb18cbda6d019b5f117340cf9e054b35f4f
Soulike <git_anonymous@foxmail.com>
2023-12-07 08:05:41 +0800

    1903

---
+++ Daily/1903.largest-odd-number-in-string.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=1903 lang=javascript
+ *
+ * [1903] Largest Odd Number in String
+ */
+
+// @lc code=start
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var largestOddNumber = function (num) {
+    for (let i = num.length - 1; i >= 0; i--) {
+        if (Number.parseInt(num[i]) % 2 === 1) {
+            return num.slice(0, i + 1);
+        }
+    }
+
+    return '';
+};
+// @lc code=end
