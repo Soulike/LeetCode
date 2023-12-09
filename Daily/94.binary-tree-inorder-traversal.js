7f131ff00a68b19000a57ac0f47ba0921584f111
Soulike <git_anonymous@foxmail.com>
2023-12-09 08:11:51 +0800

    94

---
+++ Daily/94.binary-tree-inorder-traversal.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=94 lang=javascript
+ *
+ * [94] Binary Tree Inorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+    if (root === null) return [];
+
+    const leftChildInOrder = inorderTraversal(root.left);
+    const rightChildInOrder = inorderTraversal(root.right);
+    return [...leftChildInOrder, root.val, ...rightChildInOrder];
+};
+// @lc code=end
