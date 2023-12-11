4a962565fd60ff23d615a725d48dfe226b544c51
Soulike <git_anonymous@foxmail.com>
2023-12-11 09:18:37 +0800

    1287

---
+++ Daily/1287.element-appearing-more-than-25-in-sorted-array.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=1287 lang=javascript
+ *
+ * [1287] Element Appearing More Than 25% In Sorted Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var findSpecialInteger = function (arr) {
+    const rangeDiff = Math.floor(arr.length / 4);
+    for (let i = 0; i < arr.length - rangeDiff; i++) {
+        if (arr[i] === arr[i + rangeDiff]) return arr[i];
+    }
+};
+// @lc code=end
