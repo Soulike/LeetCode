749684d8953513fec6ffe82dc8c1d5eccbd939ae
Soulike <git_anonymous@foxmail.com>
2023-12-12 08:10:45 +0800

    1464

---
+++ Daily/1464.maximum-product-of-two-elements-in-an-array.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=1464 lang=javascript
+ *
+ * [1464] Maximum Product of Two Elements in an Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function (nums) {
+    let maxNum = 0;
+    let secondMaxNum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > maxNum) {
+            secondMaxNum = maxNum;
+            maxNum = nums[i];
+        } else {
+            secondMaxNum = Math.max(secondMaxNum, nums[i]);
+        }
+    }
+
+    return (maxNum - 1) * (secondMaxNum - 1);
+};
+// @lc code=end
+
+maxProduct([3, 4, 5, 2]);
