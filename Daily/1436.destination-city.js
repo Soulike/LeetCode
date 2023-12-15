234f43c2a5d30a8c3f192bfa93df430958ed9ea6
Soulike <git_anonymous@foxmail.com>
2023-12-15 08:27:14 +0800

    1436

---
+++ Daily/1436.destination-city.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=1436 lang=javascript
+ *
+ * [1436] Destination City
+ */
+
+// @lc code=start
+/**
+ * @param {[from: string, to: string][]} paths
+ * @return {string}
+ */
+var destCity = function (paths) {
+    const fromSet = new Set();
+
+    for (const [from] of paths) {
+        fromSet.add(from);
+    }
+
+    for (const [_, to] of paths) {
+        if (!fromSet.has(to)) return to;
+    }
+};
+// @lc code=end
