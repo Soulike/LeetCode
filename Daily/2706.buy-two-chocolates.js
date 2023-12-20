7d0f7b3fcbf9a1c4d9e8c9e58c8ba0730048fc7f
Soulike <git_anonymous@foxmail.com>
2023-12-20 08:30:10 +0800

    2706

---
+++ Daily/2706.buy-two-chocolates.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=2706 lang=javascript
+ *
+ * [2706] Buy Two Chocolates
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} prices
+ * @param {number} money
+ * @return {number}
+ */
+var buyChoco = function (prices, money) {
+    prices.sort((a, b) => a - b);
+    return money >= prices[0] + prices[1]
+        ? money - prices[0] - prices[1]
+        : money;
+};
+// @lc code=end
