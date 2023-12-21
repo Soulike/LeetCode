249ca2fdf8461ff8a1a4c6b767d1552f005a87a5
Soulike <git_anonymous@foxmail.com>
2023-12-21 08:18:10 +0800

    1637

---
+++ Daily/1637.widest-vertical-area-between-two-points-containing-no-points.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=1637 lang=javascript
+ *
+ * [1637] Widest Vertical Area Between Two Points Containing No Points
+ */
+
+// @lc code=start
+/** @typedef {[x: number, y: number]} Point */
+/**
+ * @param {Point[]} points
+ * @return {number}
+ */
+var maxWidthOfVerticalArea = function (points) {
+    points.sort((a, b) => a[0] - b[0]);
+
+    let maxDiff = -Infinity;
+
+    for (let i = 1; i < points.length; i++) {
+        maxDiff = Math.max(points[i][0] - points[i - 1][0], maxDiff);
+    }
+
+    return maxDiff;
+};
+// @lc code=end
