8fdcb2b9618da4e896d428a75e0188f4f556f702
Soulike <git_anonymous@foxmail.com>
2023-12-25 09:16:01 +0800

    91

--- Daily/91.decode-ways.js
+++ Daily/91.decode-ways.js
@@ -10,49 +10,47 @@
  * @return {number}
  */
 var numDecodings = function (s) {
-    /** @type {Map<number, number>} */
-    const memo = new Map();
     /**
-     * @param {number} startIndex
-     * @returns {number}
+     * dp[i] how many ways to decode s[i] ~ s[s.length - 1]
+     *
+     * base case
+     * dp[s.length - 1] = s[s.length - 1] === 0 ? 0 : 1
+     *
+     * if(s[i] !== 0) dp[i] += dp[i+1]
+     * if(s[i] ~ s[i+1] <= 26) dp[i] += dp[i+2]
      */
-    const getDecodingNum = (startIndex) => {
-        if (memo.has(startIndex)) return memo.get(startIndex);
 
-        let decodeWays = 0;
-        if (startIndex === s.length) {
-            decodeWays++;
-            memo.set(startIndex, decodeWays);
-            return decodeWays;
+    /** @type {number[]} */
+    const dp = new Array(2);
+    dp.fill(0);
+    dp[(s.length - 1) % 2] = s[s.length - 1] === '0' ? 0 : 1;
+
+    for (let i = s.length - 2; i >= 0; i--) {
+        let currentDp = 0;
+        const firstNumber = Number.parseInt(s[i]);
+        if (firstNumber === 0) {
+            dp[i % 2] = 0;
+            continue;
+        }
+        currentDp += dp[(i + 1) % 2];
+
+        if (i + 1 === s.length) {
+            dp[i % 2] = currentDp;
+            continue;
+        }
+        const firstTwoNumbers = Number.parseInt(s.slice(i, i + 2));
+        if (firstTwoNumbers <= 26) {
+            currentDp += dp[(i + 2) % 2];
+            if (i + 1 === s.length - 1) currentDp++;
         }
 
-        const firstOneNumber = Number.parseInt(s[startIndex]);
-        if (firstOneNumber === 0) {
-            memo.set(startIndex, decodeWays);
-            return decodeWays;
-        }
-        decodeWays += getDecodingNum(startIndex + 1);
+        dp[i % 2] = currentDp;
+    }
 
-        if (startIndex + 1 >= s.length) {
-            memo.set(startIndex, decodeWays);
-            return decodeWays;
-        }
-        const firstTwoNumbers = Number.parseInt(
-            s.slice(startIndex, startIndex + 2),
-        );
-        if (firstTwoNumbers > 26) {
-            memo.set(startIndex, decodeWays);
-            return decodeWays;
-        }
-        decodeWays += getDecodingNum(startIndex + 2);
-
-        memo.set(startIndex, decodeWays);
-        return decodeWays;
-    };
-
-    const decodeWays = getDecodingNum(0);
-    return decodeWays;
+    return dp[0];
 };
 // @lc code=end
 
-numDecodings('111111111111111111111111111111111111111111111');
+numDecodings('1201234');
+
+numDecodings('111111111111111111111111111111111111111111111'); // 1836311903
