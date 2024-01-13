9f85685d426d42e86e4590f78af99d371a1ddbe6
Soulike <git_anonymous@foxmail.com>
2024-01-13 08:29:21 +0800

    1347

---
+++ Daily/1347.minimum-number-of-steps-to-make-two-strings-anagram.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=1347 lang=javascript
+ *
+ * [1347] Minimum Number of Steps to Make Two Strings Anagram
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var minSteps = function (s, t) {
+    /** @type {Map<string, number>} */
+    const letterToFrequencyInT = new Map();
+
+    for (const c of t) {
+        letterToFrequencyInT.set(c, (letterToFrequencyInT.get(c) ?? 0) + 1);
+    }
+
+    for (const c of s) {
+        letterToFrequencyInT.set(c, (letterToFrequencyInT.get(c) ?? 0) - 1);
+    }
+
+    let steps = 0;
+
+    for (const [_, value] of letterToFrequencyInT) {
+        if (value < 0) steps += -value;
+    }
+
+    return steps;
+};
+// @lc code=end
