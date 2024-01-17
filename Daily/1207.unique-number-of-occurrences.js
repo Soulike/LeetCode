59cfda4f93813b9d395dd4ec9b005e83532488b1
Soulike <git_anonymous@foxmail.com>
2024-01-17 09:21:51 +0800

    1207

---
+++ Daily/1207.unique-number-of-occurrences.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=1207 lang=javascript
+ *
+ * [1207] Unique Number of Occurrences
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function (arr) {
+    /** @type {Map<number, number>} */
+    const numberFrequencies = new Map();
+    for (const num of arr) {
+        numberFrequencies.set(num, (numberFrequencies.get(num) ?? 0) + 1);
+    }
+
+    const frequencies = Array.from(numberFrequencies.values());
+    const uniqueFrequencies = new Set(frequencies);
+    return uniqueFrequencies.size === frequencies.length;
+};
+// @lc code=end
