dba87a8d94f192c84ab69626b9140c6ac001c1eb
Soulike <git_anonymous@foxmail.com>
2024-01-25 08:28:43 +0800

    1143

--- Daily/1143.longest-common-subsequence.js
+++ Daily/1143.longest-common-subsequence.js
@@ -12,44 +12,36 @@
  */
 var longestCommonSubsequence = function (text1, text2) {
     /**
-     * dp[i][j]
-     *
-     * the longest common subsequence between text1[0,i-1] and text2[0,j-1]
+     * dp[i][j]: The LCS between text1[0~i-1] and text2[0~j-1]
      *
      * base case
-     * dp[0][...] = 0
-     * dp[...][0] = 0
+     * dp[0][j] = 0
+     * dp[i][0] = 0
      *
-     * if text1[i-1] !== text2[j-1]
-     *  dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-1], dp[i][j-1])
+     * if text[i-1] !== text[j-1]
+     *  dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
      * else
-     *  dp[i][j] = dp[i-1][j-1]+1
+     *  dp[i][j] = dp[i-1][j-1] + 1
      */
 
-    [text1, text2] =
-        text1.length < text2.length ? [text1, text2] : [text2, text1];
+    const dp = new Array(2);
+    for (let i = 0; i < dp.length; i++) {
+        dp[i] = new Array(text2.length + 1);
+        dp[i][0] = 0;
+    }
+    dp[0].fill(0);
 
-    const M = text1.length;
-    const N = text2.length;
-
-    let prevDp = new Uint16Array(N + 1);
-    prevDp.fill(0);
-
-    let dp = new Uint16Array(N + 1);
-    dp[0] = 0;
-
-    for (let i = 1; i <= M; i++) {
-        for (let j = 1; j <= N; j++) {
-            dp[j] =
-                text1[i - 1] !== text2[j - 1]
-                    ? Math.max(prevDp[j], dp[j - 1]) // dp[i - 1][j - 1] must be less than the two
-                    : prevDp[j - 1] + 1;
+    for (let i = 1; i <= text1.length; i++) {
+        for (let j = 1; j <= text2.length; j++) {
+            if (text1[i - 1] !== text2[j - 1]) {
+                // dp[i-1][j-1] is not possible to be larger
+                dp[i % 2][j] = Math.max(dp[(i - 1) % 2][j], dp[i % 2][j - 1]);
+            } else {
+                dp[i % 2][j] = dp[(i - 1) % 2][j - 1] + 1;
+            }
         }
-        [prevDp, dp] = [dp, prevDp];
     }
 
-    return prevDp[N];
+    return dp[text1.length % 2][text2.length];
 };
 // @lc code=end
-
-longestCommonSubsequence('c', 'abc');
