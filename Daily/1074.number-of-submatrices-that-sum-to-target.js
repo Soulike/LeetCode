8811e105902b462ae655624b3f0d44525093c595
Soulike <git_anonymous@foxmail.com>
2024-01-28 11:30:26 +0800

    1074

--- Daily/1074.number-of-submatrices-that-sum-to-target.js
+++ Daily/1074.number-of-submatrices-that-sum-to-target.js
@@ -14,16 +14,33 @@ var numSubmatrixSumTarget = function (matrix, target) {
     const M = matrix.length;
     const N = matrix[0].length;
 
-    const prefixSum = getMatrixPrefixSum(matrix);
+    const rowPrefixSums = getMatrixRowPrefixSum(matrix);
 
     let sumToTargetCount = 0;
-    for (let x1 = 0; x1 < M; x1++) {
-        for (let y1 = 0; y1 < N; y1++) {
-            for (let x2 = x1; x2 < M; x2++) {
-                for (let y2 = y1; y2 < N; y2++) {
-                    const sum = getSubMatrixSum(prefixSum, [x1, y1], [x2, y2]);
-                    if (sum === target) sumToTargetCount++;
-                }
+
+    for (let y1 = 0; y1 < N; y1++) {
+        for (let y2 = y1; y2 < N; y2++) {
+            /**
+             * Limit the column range to [y1, y2]
+             * In this column range, iterate x
+             * Calculate row prefix sum of [0, x]
+             * Then the problem becomes https://leetcode.com/problems/subarray-sum-equals-k/
+             */
+
+            let rowPrefixSumInColRange = 0;
+            /** @type {Map<number, number>} */
+            let prefixSumToCount = new Map([[0, 1]]);
+            for (let x = 0; x < M; x++) {
+                rowPrefixSumInColRange +=
+                    rowPrefixSums[x][y2] -
+                    (y1 - 1 >= 0 ? rowPrefixSums[x][y1 - 1] : 0);
+                sumToTargetCount +=
+                    prefixSumToCount.get(rowPrefixSumInColRange - target) ?? 0;
+
+                prefixSumToCount.set(
+                    rowPrefixSumInColRange,
+                    (prefixSumToCount.get(rowPrefixSumInColRange) ?? 0) + 1,
+                );
             }
         }
     }
@@ -35,46 +52,29 @@ var numSubmatrixSumTarget = function (matrix, target) {
  * @param {number[][]} matrix
  * @returns {number[][]}
  */
-function getMatrixPrefixSum(matrix) {
+function getMatrixRowPrefixSum(matrix) {
     const M = matrix.length;
     const N = matrix[0].length;
-    const prefixSum = new Array(M);
-    for (let i = 0; i < prefixSum.length; i++) {
-        prefixSum[i] = new Array(N);
+    const rowPrefixSum = new Array(M);
+    for (let i = 0; i < rowPrefixSum.length; i++) {
+        rowPrefixSum[i] = new Array(N);
     }
 
     for (let i = 0; i < M; i++) {
         for (let j = 0; j < N; j++) {
-            prefixSum[i][j] =
-                (i - 1 >= 0 ? prefixSum[i - 1][j] : 0) +
-                (j - 1 >= 0 ? prefixSum[i][j - 1] : 0) -
-                (i - 1 >= 0 && j - 1 >= 0 ? prefixSum[i - 1][j - 1] : 0) +
-                matrix[i][j];
+            rowPrefixSum[i][j] =
+                (j - 1 >= 0 ? rowPrefixSum[i][j - 1] : 0) + matrix[i][j];
         }
     }
 
-    return prefixSum;
-}
-
-/**
- *
- * @param {number[][]} matrixPrefixSum
- * @param {[x: number, y: number]} coordinate1
- * @param {[x: number, y: number]} coordinate2
- * @returns {number}
- */
-function getSubMatrixSum(matrixPrefixSum, coordinate1, coordinate2) {
-    let [x1, y1] = coordinate1;
-    let [x2, y2] = coordinate2;
-    [x1, x2] = [Math.min(x1, x2), Math.max(x1, x2)];
-    [y1, y2] = [Math.min(y1, y2), Math.max(y1, y2)];
-
-    const subMatrixSum =
-        matrixPrefixSum[x2][y2] -
-        (y1 - 1 >= 0 ? matrixPrefixSum[x2][y1 - 1] : 0) -
-        (x1 - 1 >= 0 ? matrixPrefixSum[x1 - 1][y2] : 0) +
-        (x1 - 1 >= 0 && y1 - 1 >= 0 ? matrixPrefixSum[x1 - 1][y1 - 1] : 0);
-
-    return subMatrixSum;
+    return rowPrefixSum;
 }
 // @lc code=end
+
+numSubmatrixSumTarget(
+    [
+        [1, -1],
+        [-1, 1],
+    ],
+    0,
+);
