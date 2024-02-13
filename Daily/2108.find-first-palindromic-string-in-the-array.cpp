6ad19a081fac49acda616f39e3ad1dcea1650f2f
Soulike <git_anonymous@foxmail.com>
2024-02-13 11:54:05 +0800

    2108

---
+++ Daily/2108.find-first-palindromic-string-in-the-array.cpp
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=2108 lang=cpp
+ *
+ * [2108] Find First Palindromic String in the Array
+ */
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+
+// @lc code=start
+class Solution
+{
+public:
+    string firstPalindrome(vector<string> &words)
+    {
+        for (const auto &word : words)
+        {
+            if (this->isPalindrome(word))
+            {
+                return word;
+            }
+        }
+        return "";
+    }
+
+    bool isPalindrome(const string &str)
+    {
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right)
+        {
+            if (str[left] != str[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
+// @lc code=end
