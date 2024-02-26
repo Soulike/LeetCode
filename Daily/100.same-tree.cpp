339048f77f71e7a168c30e31dd97eb452ca61324
Soulike <git_anonymous@foxmail.com>
2024-02-26 20:18:41 +0800

    100

---
+++ Daily/100.same-tree.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=100 lang=cpp
+ *
+ * [100] Same Tree
+ */
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// @lc code=start
+class Solution
+{
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q)
+    {
+        if (p == nullptr && q != nullptr || p != nullptr && q == nullptr)
+        {
+            return false;
+        }
+        if (p == nullptr && q == nullptr)
+        {
+            return true;
+        }
+        return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+// @lc code=end
