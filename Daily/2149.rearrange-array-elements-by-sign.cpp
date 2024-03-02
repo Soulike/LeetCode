e79e6d231d8f25233b9687be51e0b8305b624256
Soulike <git_anonymous@foxmail.com>
2024-03-02 11:26:55 +0800

    2149

---
+++ Daily/2149.rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=2149 lang=cpp
+ *
+ * [2149] Rearrange Array Elements by Sign
+ */
+#include <vector>
+using std::vector;
+
+// @lc code=start
+class Solution
+{
+public:
+    vector<int> rearrangeArray(vector<int> &nums)
+    {
+        int positiveCurrentIndex = 0;
+        int negativeCurrentIndex = 1;
+
+        vector<int> result(nums.size());
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] < 0)
+            {
+                result[negativeCurrentIndex] = nums[i];
+                negativeCurrentIndex += 2;
+            }
+            else
+            {
+                result[positiveCurrentIndex] = nums[i];
+                positiveCurrentIndex += 2;
+            }
+        }
+
+        return result;
+    }
+};
+// @lc code=end
