7c02ec49c3f38ff3fb3ee28cbfd900ced4fd6176
Soulike <git_anonymous@foxmail.com>
2024-03-02 12:16:37 +0800

    49

---
+++ Daily/49.group-anagrams.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=49 lang=javascript
+ *
+ * [49] Group Anagrams
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+    /** @type {Map<string, string[]>} */
+    const strToAnagrams = new Map();
+
+    for (const str of strs) {
+        const mapKey = str.split('').sort().join('');
+        const anagrams = strToAnagrams.get(mapKey) ?? [];
+        anagrams.push(str);
+        strToAnagrams.set(mapKey, anagrams);
+    }
+
+    return Array.from(strToAnagrams.values());
+};
+// @lc code=end
