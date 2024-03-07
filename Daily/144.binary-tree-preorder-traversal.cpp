c70211eeceb9456331c46f51d0866f1960e03065
Soulike <git_anonymous@foxmail.com>
2024-03-07 08:18:39 +0800

    144

---
+++ Daily/144.binary-tree-preorder-traversal.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=144 lang=cpp
+ *
+ * [144] Middle of the Linked List
+ */
+
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+// @lc code=start
+class Solution {
+ public:
+  ListNode* middleNode(ListNode* head) {
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while (fast != nullptr && fast->next != nullptr)
+    {
+      slow = slow->next;
+      fast = fast->next->next;
+    }
+
+    return slow;
+  }
+};
+// @lc code=end
