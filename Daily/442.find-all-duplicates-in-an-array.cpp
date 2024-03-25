b7160df61e3d80c4fa4b936ce036a86350e1f686
Soulike <git_anonymous@foxmail.com>
2024-03-25 08:22:39 +0800

    442

---
+++ Daily/442.find-all-duplicates-in-an-array.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=442 lang=cpp
+ *
+ * [442] Find All Duplicates in an Array
+ */
+
+#include <cmath>
+#include <vector>
+#include <iostream>
+
+using std::vector;
+
+// @lc code=start
+class Solution {
+ public:
+  vector<int> findDuplicates(vector<int>& nums) {
+    vector<int> duplicates;
+    for (int i = 0; i < nums.size(); i++) {
+      int num = std::abs(nums[i]);
+      int index = num - 1;
+      if (nums[index] < 0) {
+        duplicates.push_back(num);
+      } else {
+        nums[index] *= -1;
+      }
+    }
+
+    return duplicates;
+  }
+};
+// @lc code=end
