eb7748ea4a3fc120feac56bc73d2688e7f72f590
Soulike <git_anonymous@foxmail.com>
2024-04-07 11:36:17 +0800

    678

---
+++ Daily/678.valid-parenthesis-string.cpp
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode id=678 lang=cpp
+ *
+ * [678] Valid Parenthesis String
+ */
+#include <cmath>
+#include <string>
+
+using std::string;
+
+// @lc code=start
+class Solution {
+ public:
+  bool checkValidString(string s) {
+    int minOpenParenthesis = 0;  // try to treat all '*'s as ')'s
+    int maxOpenParenthesis = 0;  // try to treat all '*'s as '('s
+
+    for (auto& c : s) {
+      if (c == '(') {
+        minOpenParenthesis++;
+        maxOpenParenthesis++;
+      } else if (c == ')') {
+        // At this point, if minOpenParenthesis < 0,
+        // then we treat too many '*'s as ')'s,
+        // so set some of them to '',
+        // as we can't use future '('s to match them.
+        minOpenParenthesis = std::max(minOpenParenthesis - 1, 0);
+        maxOpenParenthesis--;
+      } else {
+        // At this point, if minOpenParenthesis < 0,
+        // then we treat too many '*'s as ')'s,
+        // so set some of them to '',
+        // as we can't use future '('s to match them.
+        minOpenParenthesis = std::max(minOpenParenthesis - 1, 0);
+        maxOpenParenthesis++;
+      }
+
+      // Too many ')'s. Treating all '*'s as '('s is still not enough
+      if (maxOpenParenthesis < 0) {
+        return false;
+      }
+    }
+
+    // If minOpenParenthesis > 0,
+    // then there are too many '('s,
+    // and treating all '*'s as ')'s is still not enough
+    return minOpenParenthesis == 0;
+  }
+};
+// @lc code=end
+
+int main() {
+  Solution sol;
+  sol.checkValidString("()");
+}
