e3be8fdda1033aad544c2933356489298d4e99c3
Soulike <git_anonymous@foxmail.com>
2024-04-08 10:04:15 +0800

    1700

--- Daily/1700.number-of-students-unable-to-eat-lunch.cpp
+++ Daily/1700.number-of-students-unable-to-eat-lunch.cpp
@@ -14,27 +14,16 @@ class Solution {
  public:
   int countStudents(vector<int>& students, vector<int>& sandwiches) {
     int sandwichStudentAmount[2] = {0, 0};
-    queue<int> studentQueue;
     for (auto& student : students) {
       sandwichStudentAmount[0] += 1 - student;
       sandwichStudentAmount[1] += student;
-      studentQueue.push(student);
     }
 
-    for (int i = 0; i < sandwiches.size(); i++) {
-      const int& sandwichType = sandwiches[i];
-      if (sandwichStudentAmount[sandwichType] == 0) {
-        return sandwichStudentAmount[1 - sandwichType];
+    for (auto& sandwich : sandwiches) {
+      if (sandwichStudentAmount[sandwich] == 0) {
+        return sandwichStudentAmount[1 - sandwich];
       }
-
-      int sandwichStudent = studentQueue.front();
-      studentQueue.pop();
-      if (sandwichStudent != sandwichType) {
-        studentQueue.push(sandwichStudent);
-        i--;
-      } else {
-        sandwichStudentAmount[sandwichStudent]--;
-      }
+      sandwichStudentAmount[sandwich]--;
     }
     return 0;
   }
@@ -43,7 +32,7 @@ class Solution {
 
 int main() {
   Solution sol;
-  vector<int> students = {1, 1, 1, 0, 0, 1};
-  vector<int> sandwiches = {1, 0, 0, 0, 1, 1};
+  vector<int> students = {1, 1, 1, 1};
+  vector<int> sandwiches = {0, 0, 1, 1};
   sol.countStudents(students, sandwiches);
 }
