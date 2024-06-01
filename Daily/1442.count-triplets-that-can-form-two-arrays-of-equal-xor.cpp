cd3018032c5742cf9fa9b0b534e76d7dc85cda02
Soulike <git_anonymous@foxmail.com>
2024-06-01 10:22:24 +0800

    1442

--- Daily/1442.count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ Daily/1442.count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -13,23 +13,13 @@ class Solution {
  public:
   int countTriplets(vector<int>& arr) {
     const int N = arr.size();
-    vector<int> prefixXor(N, 0);
-    prefixXor[0] = arr[0];
-    for (int i = 1; i < N; i++) {
-      prefixXor[i] = prefixXor[i - 1] ^ arr[i];
-    }
-
     int tripletCount = 0;
-
     for (int i = 0; i < N; i++) {
-      for (int j = i + 1; j < N; j++) {
-        for (int k = j; k < N; k++) {
-          int a = prefixXor[j - 1] ^ (i - 1 >= 0 ? prefixXor[i - 1] : 0);
-          int b = prefixXor[k] ^ prefixXor[j - 1];
-
-          if (a == b) {
-            tripletCount++;
-          }
+      int currentXor = 0;
+      for (int k = i; k < N; k++) {
+        currentXor ^= arr[k];
+        if (currentXor == 0) {
+          tripletCount += k - i;
         }
       }
     }
