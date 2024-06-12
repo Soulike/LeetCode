79f678c03231e01c765acd4e62442e19fed1bf86
Soulike <git_anonymous@foxmail.com>
2024-06-12 09:04:38 +0800

    75

--- Daily/75.sort-colors.cpp
+++ Daily/75.sort-colors.cpp
@@ -3,7 +3,6 @@
  *
  * [75] Sort Colors
  */
-#include <array>
 #include <vector>
 
 // @lc code=start
@@ -11,16 +10,35 @@ class Solution {
  public:
   void sortColors(std::vector<int>& nums) {
     const int N = nums.size();
-    std::array<int, 3> colorToFreqs;
-    for (int i = 0; i < N; i++) {
-      colorToFreqs[nums[i]]++;
-    }
+    int redZoneIndex = 0;
+    int blueZoneIndex = N - 1;
 
-    int numsIndex = 0;
-    for (int i = 0; i < 3; i++) {
-      std::fill_n(nums.begin() + numsIndex, colorToFreqs[i], i);
-      numsIndex += colorToFreqs[i];
+    for (int i = 0; i < N; i++) {
+      const COLOR color = static_cast<COLOR>(nums[i]);
+      if (color == COLOR::RED && i >= redZoneIndex) {
+        std::swap(nums[i], nums[redZoneIndex]);
+        redZoneIndex++;
+        i--;
+      } else if (color == COLOR::BLUE && i <= blueZoneIndex) {
+        std::swap(nums[i], nums[blueZoneIndex]);
+        blueZoneIndex--;
+        i--;
+      }
     }
   }
+
+ private:
+  enum class COLOR : int {
+    RED = 0,
+    WHITE = 1,
+    BLUE = 2,
+  };
 };
 // @lc code=end
+
+int main() {
+  std::vector<int> nums = {2, 0, 2, 1, 1, 0};
+  Solution sol;
+  sol.sortColors(nums);
+  nums;
+}
