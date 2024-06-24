f9cce386a5f66886357e21ddaa87b1c91b94c19a
Soulike <git_anonymous@foxmail.com>
2024-06-24 09:14:44 +0800

    995

---
+++ Daily/995.minimum-number-of-k-consecutive-bit-flips.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=995 lang=cpp
+ *
+ * [995] Minimum Number of K Consecutive Bit Flips
+ */
+
+#include <queue>
+#include <vector>
+
+// @lc code=start
+class Solution {
+ public:
+  int minKBitFlips(std::vector<int>& nums, int k) {
+    std::queue<int> flips;
+    int result = 0;
+    for (auto i = 0; i < nums.size(); i++) {
+      if (nums[i] != (flips.size() % 2 ? 0 : 1)) {
+        result++;
+        flips.push(i + k - 1);
+      }
+      if (!flips.empty() && flips.front() <= i) {
+        flips.pop();
+      }
+    }
+    return flips.empty() ? result : -1;
+  }
+};
+// @lc code=end
