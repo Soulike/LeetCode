c949f99cd9f0227d2769702f8aa06e4f7dc21b4a
Soulike <git_anonymous@foxmail.com>
2024-07-30 20:15:44 +0800

    1653

--- Daily/1653.minimum-deletions-to-make-string-balanced.cpp
+++ Daily/1653.minimum-deletions-to-make-string-balanced.cpp
@@ -4,27 +4,37 @@
  * [1653] Minimum Deletions to Make String Balanced
  */
 
-#include <stack>
+#include <algorithm>
 #include <string>
+#include <vector>
 
 // @lc code=start
 class Solution {
  public:
   int minimumDeletions(const std::string& s) {
-    const int n = s.length();
-    std::stack<char> charStack;
-    int deleteCount = 0;
+    const int N = s.size();
+    int previousBCount = 0;
+    /*
+     * dp[i] - minimum deletions at s[i - 1]
+     * */
+    std::vector<int> dp(2, 0);
 
-    for (int i = 0; i < n; i++) {
-      if (!charStack.empty() && charStack.top() == 'b' && s[i] == 'a') {
-        charStack.pop();
-        deleteCount++;
+    for (int i = 0; i < N; i++) {
+      // At this point, s[0] - s[i] is already a balanced string
+      // like aaaaa or aaaabbbb
+      if (s[i] == 'b') {
+        dp[(i + 1) % 2] = dp[i % 2];
+        previousBCount++;
       } else {
-        charStack.push(s[i]);
+        // s[i] == 'a'
+        dp[(i + 1) % 2] =
+            std::min(dp[i % 2] + 1,  // Delete this `a`
+                     previousBCount  // or remove all previous `b`s
+            );
       }
     }
 
-    return deleteCount;
+    return dp[N % 2];
   }
 };
 // @lc code=end
