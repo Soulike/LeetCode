79936a377fe4f442a0b9b56847047a9ec3930cb2
Soulike <git_anonymous@foxmail.com>
2024-08-21 09:03:59 +0800

    664

--- Daily/664.strange-printer.cpp
+++ Daily/664.strange-printer.cpp
@@ -9,50 +9,48 @@
 // @lc code=start
 class Solution {
  public:
-  int strangePrinter(const std::string& s) {
-    std::string sWithoutConsecutiveDuplicates;
-    for (const char c : s) {
-      if (sWithoutConsecutiveDuplicates.empty() ||
-          sWithoutConsecutiveDuplicates.back() != c) {
-        sWithoutConsecutiveDuplicates.push_back(c);
+  int strangePrinter(std::string s) {
+    // Consecutive duplicates do not affect steps. So remove them.
+    int noConsecutiveDuplicateBorderIndex = 0;
+    for (int i = 1; i < s.length(); i++) {
+      if (s[i] != s[noConsecutiveDuplicateBorderIndex]) {
+        noConsecutiveDuplicateBorderIndex++;
+        s[noConsecutiveDuplicateBorderIndex] = s[i];
+      }
+    }
+    s.resize(noConsecutiveDuplicateBorderIndex + 1);
+
+    const int sLength = static_cast<int>(s.size());
+
+    // dp[i][j] - The minimum steps to print from s[i] to s[j]
+    std::vector<std::vector<int>> dp(sLength, std::vector<int>(sLength, -1));
+    for (int i = 0; i < sLength; i++) {
+      dp[i][i] = 1;
+    }
+
+    for (int i = sLength - 1; i >= 0; i--) {
+      for (int j = i + 1; j < sLength; j++) {
+        dp[i][j] = 1 + dp[i + 1][j];
+
+        for (int k = i + 1; k <= j; k++) {
+          // Here, if s[i] == s[k], we try to print s[i] along with
+          // s[k], and make following prints overwrite it.
+          // This can reduce the steps to
+          // print s[k] and s[i] respectively.
+          if (s[k] == s[i]) {
+            dp[i][j] = std::min(dp[i][j],
+                                dp[i][k - 1] + (k + 1 <= j ? dp[k + 1][j] : 0));
+          }
+        }
       }
     }
 
-    const int length = static_cast<int>(sWithoutConsecutiveDuplicates.size());
-
-    std::vector<std::vector<int>> memo(length, std::vector<int>(length, -1));
-
-    int result = helper(sWithoutConsecutiveDuplicates, 0, length - 1, memo);
-    return result;
-  }
-
- private:
-  int helper(const std::string& s,
-             int startIndex,
-             int endIndex,
-             std::vector<std::vector<int>>& memo) {
-    if (startIndex > endIndex) {
-      return 0;
-    }
-
-    if (memo[startIndex][endIndex] != -1) {
-      return memo[startIndex][endIndex];
-    }
-
-    int steps = 1 + helper(s, startIndex + 1, endIndex, memo);
-    for (int i = startIndex + 1; i <= endIndex; i++) {
-      if (s[i] == s[startIndex]) {
-        // Here, if s[i] == s[startIndex], we try to print s[i] along with
-        // s[startIndex], and make following prints overwrite it.
-        // This can reduce the steps to
-        // print s[startIndex] and s[i] respectively.
-        steps = std::min(steps, helper(s, startIndex, i - 1, memo) +
-                                    helper(s, i + 1, endIndex, memo));
-      }
-    }
-
-    memo[startIndex][endIndex] = steps;
-    return steps;
+    return dp[0][sLength - 1];
   }
 };
 // @lc code=end
+
+int main() {
+  Solution sol;
+  sol.strangePrinter("aba");
+}
