2fb4d92b51a2ab50df01eef76c247df814105fb3
Soulike <git_anonymous@foxmail.com>
2024-09-21 10:32:33 +0800

    386

--- Daily/386.lexicographical-numbers.cpp
+++ Daily/386.lexicographical-numbers.cpp
@@ -10,28 +10,31 @@ class Solution {
  public:
   std::vector<int> lexicalOrder(int n) {
     std::vector<int> results;
-    int currentPrefix = 0;
-    recursive(n, results, currentPrefix);
+    int currentNumber = 1;
+
+    // Have at most `n` digits
+    for (int i = 0; i < n; i++) {
+      results.push_back(currentNumber);
+
+      if (currentNumber * 10 <= n) {
+        // If we can multiply it by 10, do it to maintain order
+        currentNumber *= 10;
+      } else {
+        // If we can't multiply it by 10, remove its last digit
+        // and increment it to maintain order
+        while (currentNumber % 10 == 9 || currentNumber >= n) {
+          currentNumber /= 10;
+        }
+        currentNumber++;
+      }
+    }
+
     return results;
-  }
-
- private:
-  void recursive(int n, std::vector<int>& results, const int& currentPrefix) {
-    if (currentPrefix * 10 > n) {
-      return;
-    }
-
-    for (int i = 0; i <= 9; i++) {
-      const int num = currentPrefix * 10 + i;
-      if (num == 0) {
-        continue;
-      }
-      if (num > n) {
-        break;
-      }
-      results.push_back(num);
-      recursive(n, results, num);
-    }
   }
 };
 // @lc code=end
+
+int main() {
+  Solution sol;
+  sol.lexicalOrder(13);
+}
