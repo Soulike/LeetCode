3ab30aa21df3f7bdcdd93fbcdb99a77536b84e3a
Soulike <git_anonymous@foxmail.com>
2024-11-05 06:59:09 +0800

    3163

--- Daily/3163.string-compression-iii.cpp
+++ Daily/3163.string-compression-iii.cpp
@@ -10,25 +10,25 @@
 class Solution {
  public:
   std::string compressedString(std::string word) {
-    int currentCharNumber = 1;
+    int currentCharNumber = '1';
     char currentChar = word[0];
 
     std::string result;
     result.reserve(word.size());
 
     for (int i = 1; i < word.size(); i++) {
-      if (word[i] == currentChar && currentCharNumber < 9) {
+      if (word[i] == currentChar && currentCharNumber < '9') {
         currentCharNumber++;
       } else {
-        result += std::to_string(currentCharNumber);
+        result += currentCharNumber;
         result += currentChar;
 
-        currentCharNumber = 1;
+        currentCharNumber = '1';
         currentChar = word[i];
       }
     }
 
-    result += std::to_string(currentCharNumber);
+    result += currentCharNumber;
     result += currentChar;
 
     return result;
