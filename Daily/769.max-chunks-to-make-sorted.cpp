b86cd337e94b57f6893fe8e432e2d38cdfcc7e45
Soulike <git_anonymous@foxmail.com>
2024-12-19 09:09:42 +0800

    769

--- Daily/769.max-chunks-to-make-sorted.cpp
+++ Daily/769.max-chunks-to-make-sorted.cpp
@@ -11,43 +11,21 @@
 class Solution {
  public:
   int maxChunksToSorted(const std::vector<int>& arr) {
-    std::vector<Chunk> chunks;
+    int currentEnd = -1;
+    int chunkNumber = 0;
+
     for (int i = 0; i < arr.size(); i++) {
       const int expectedIndex = arr[i];
       const int chunkBegin = std::min(i, expectedIndex);
       const int chunkEnd = std::max(i, expectedIndex);
-      chunks.emplace_back(chunkBegin, chunkEnd);
-    }
 
-    // Here, chunks are already sorted by `begin`.
-    return chunkNumberAfterMerge(chunks);
-  }
-
- private:
-  class Chunk {
-   public:
-    int begin;
-    int end;
-  };
-
-  static int chunkNumberAfterMerge(
-      const std::vector<Chunk>& chunksSortedByBegin) {
-    if (chunksSortedByBegin.empty()) {
-      return 0;
-    }
-
-    int currentEnd = chunksSortedByBegin[0].end;
-    int chunkNumber = 1;
-
-    for (int i = 1; i < chunksSortedByBegin.size(); i++) {
-      const Chunk& currentChunk = chunksSortedByBegin[i];
-      if (currentChunk.begin > currentEnd) {
+      if (chunkBegin > currentEnd) {
         // Can not merge. New chunk
         chunkNumber++;
-        currentEnd = currentChunk.end;
+        currentEnd = chunkEnd;
       } else {
         // Can merge. Extend current chunk if possible.
-        currentEnd = std::max(currentChunk.end, currentEnd);
+        currentEnd = std::max(chunkEnd, currentEnd);
       }
     }
 
