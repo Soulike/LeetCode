e0b963f1a268b1c9fe4b624cb5ce57d915f7a674
Soulike <git_anonymous@foxmail.com>
2024-12-26 08:59:10 +0800

    494

--- Daily/494.target-sum.cpp
+++ Daily/494.target-sum.cpp
@@ -4,38 +4,63 @@
  * [494] Target Sum
  */
 
-#include <unordered_map>
+#include <numeric>
 #include <vector>
 
 // @lc code=start
 class Solution {
  public:
   int findTargetSumWays(const std::vector<int>& nums, const int target) {
-    std::unordered_map<std::string, int> memo;
-    return dp(nums, 0, target, memo);
+    const int numsSum = std::accumulate(nums.cbegin(), nums.cend(), 0);
+
+    if (!isTargetPossible(target, numsSum)) {
+      return 0;
+    }
+
+    /*
+     * dp[i][numsSum+t] - How many ways can `nums[0]`~`nums[i]` get target `t`
+     *
+     * base case
+     * dp[0][numsSum+nums[0]] = 1
+     * dp[0][numsSum-nums[0]] += 1
+     *
+     * dp[i][numsSum+t] = dp[i-1][numsSum+t+nums[i]] +
+     * dp[i-1][numsSum+t-nums[i]]
+     */
+
+    // `2 * numsSum + 1`: Possible target range is `[-numsSum, numsSum]`. To
+    // keep the index positive, we add `numsSum` and get range `[0, 2 *
+    // numsSum]`.
+    std::vector<std::vector<int>> dp(nums.size(),
+                                     std::vector<int>(2 * numsSum + 1, 0));
+    dp[0][numsSum + nums[0]] = 1;
+    // It is possible that `nums[0] == 0`, in which case we have 2 ways to get
+    // `t == 0`.
+    dp[0][numsSum - nums[0]] += 1;
+
+    for (int i = 1; i < nums.size(); i++) {
+      for (int t = -numsSum; t <= numsSum; t++) {
+        dp[i][numsSum + t] = (isTargetPossible(t + nums[i], numsSum)
+                                  ? dp[i - 1][numsSum + t + nums[i]]
+                                  : 0) +
+                             (isTargetPossible(t - nums[i], numsSum)
+                                  ? dp[i - 1][numsSum + t - nums[i]]
+                                  : 0);
+      }
+    }
+
+    return dp[nums.size() - 1][numsSum + target];
   }
 
  private:
-  /**
-   * Starts from `index`, how many ways we can get `target`.
-   * If not possible, returns `0`.
-   */
-  int dp(const std::vector<int>& nums,
-         const int index,
-         const int target,
-         std::unordered_map<std::string, int>& memo) {
-    if (index == nums.size()) {
-      return target == 0;
-    }
-    const std::string memoKey =
-        std::to_string(index) + '-' + std::to_string(target);
-    if (memo.contains(memoKey)) {
-      return memo[memoKey];
-    }
-    const int result = dp(nums, index + 1, target + nums[index], memo) +
-                       dp(nums, index + 1, target - nums[index], memo);
-    memo[memoKey] = result;
-    return result;
+  static bool isTargetPossible(const int target, const int numsSum) {
+    // It is impossible to get target outside [-numsSum, numsSum];
+    return -numsSum <= target && target <= numsSum;
   }
 };
 // @lc code=end
+
+int main() {
+  Solution sol;
+  sol.findTargetSumWays({1}, 2);
+}
