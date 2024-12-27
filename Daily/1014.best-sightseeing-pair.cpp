5de95e76649f9adc24966938b764f44d22f91951
Soulike <git_anonymous@foxmail.com>
2024-12-27 08:32:58 +0800

    1014

--- Daily/1014.best-sightseeing-pair.cpp
+++ Daily/1014.best-sightseeing-pair.cpp
@@ -16,24 +16,29 @@ class Solution {
      * = `values[i] + values[j] - j + i`
      * = `(values[i] + i) + (values[j] - j)`
      * We call `(values[i] + i)` as "left value",
-     * and `(values[j] - j)` as right value.
-     * So, given `j`, we should find maximum left value where `i < j`.
+     * and `(values[j] - j)` as "right value".
+     * So, given left value, we should find maximum left value.
      */
 
-    // maxLeftValueBeforeIndex[j] the maximum left value where `i<j`
-    // DP: maxLeftValueBeforeIndex[j] = std::max(maxLeftValueBeforeIndex[j - 1],
-    // values[j - 1] + (j - 1));
-    std::vector<int> maxLeftValueBeforeIndex(values.size(), INT_MIN);
-    // `j = 0` is not a valid index since `j >= 1`
-    for (int j = 1; j < values.size(); j++) {
-      maxLeftValueBeforeIndex[j] =
-          std::max(maxLeftValueBeforeIndex[j - 1], values[j - 1] + (j - 1));
-    }
+    /**
+     * maxLeftValueBeforeIndex[j] the maximum left value where `i < j`
+     *
+     * Dynamic programming:
+     * maxLeftValueBeforeIndex[j] =
+     * std::max(maxLeftValueBeforeIndex[j - 1] + values[j - 1] + (j - 1));
+     *
+     * Since only one max left value is needed every calculation,
+     * we can optimize memory.
+     */
+    int maxLeftValueBefore = INT_MIN;
 
     int maximumScore = INT_MIN;
+    // `j = 0` is not a valid index since `j > i > 0`
     for (int j = 1; j < values.size(); j++) {
+      maxLeftValueBefore =
+          std::max(maxLeftValueBefore, values[j - 1] + (j - 1));
       maximumScore =
-          std::max(maximumScore, maxLeftValueBeforeIndex[j] + (values[j] - j));
+          std::max(maximumScore, maxLeftValueBefore + (values[j] - j));
     }
 
     return maximumScore;
