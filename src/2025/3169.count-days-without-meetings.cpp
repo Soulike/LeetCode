f21cd2aaa44b3df5f88dd3dab503123bbbcb016e
Soulike <git_anonymous@foxmail.com>
2025-03-24 09:04:38 +0800

    3169

--- src/2025/3169.count-days-without-meetings.cpp
+++ src/2025/3169.count-days-without-meetings.cpp
@@ -4,48 +4,71 @@
  * [3169] Count Days Without Meetings
  */
 
-#include <map>
+#include <algorithm>
 #include <vector>
 
 // @lc code=start
 class Solution {
  public:
-  int countDays(const int days, const std::vector<std::vector<int>>& meetings) {
-    std::map<int, int> meetingDiffs;
-    meetingDiffs.insert({days + 1, 0});
+  int countDays(const int days, std::vector<std::vector<int>>& meetings) {
+    std::sort(
+        meetings.begin(), meetings.end(),
+        [](const std::vector<int>& meeting1, const std::vector<int>& meeting2) {
+          if (meeting1[0] != meeting2[0]) {
+            return meeting1[0] < meeting2[0];
+          }
+          return meeting1[1] > meeting2[1];
+        });
 
-    for (const auto& meeting : meetings) {
-      const int beginDay = meeting[0];
-      const int endDay = meeting[1];
-      meetingDiffs[beginDay]++;
-      meetingDiffs[endDay + 1]--;
-    }
-
-    int currentMeetingNumber = 0;
+    const std::vector<std::vector<int>> mergedMeetings =
+        mergeIntervals(meetings);
     int freeDays = 0;
-    int lastFreeDay = 0;
-    for (const auto& meetingDiff : meetingDiffs) {
-      const int day = meetingDiff.first;
-      const int diff = meetingDiff.second;
+    freeDays += mergedMeetings[0][0] - 1;
+    for (int i = 1; i < mergedMeetings.size(); i++) {
+      freeDays += mergedMeetings[i][0] - mergedMeetings[i - 1][1] - 1;
+    }
+    freeDays += days - mergedMeetings[mergedMeetings.size() - 1][1];
 
-      if (currentMeetingNumber == 0) {
-        // Free days end
-        freeDays += day - lastFreeDay;
-      }
+    return freeDays;
+  }
 
-      currentMeetingNumber += diff;
+ private:
+  // `intervals` should be first sorted by begins (increasing), then sorted by
+  // ends (decreasing).
+  static std::vector<std::vector<int>> mergeIntervals(
+      const std::vector<std::vector<int>>& intervals) {
+    if (intervals.empty()) {
+      return {};
+    }
+    constexpr int kIntervalBeginIndex = 0;
+    constexpr int kIntervalEndIndex = 1;
 
-      if (currentMeetingNumber == 0) {
-        lastFreeDay = day;
+    int currentIntervalBegin = intervals[0][kIntervalBeginIndex];
+    int currentIntervalEnd = intervals[0][kIntervalEndIndex];
+    std::vector<std::vector<int>> mergedIntervals;
+
+    for (const auto& interval : intervals) {
+      if (currentIntervalEnd >= interval[kIntervalBeginIndex]) {
+        // Overlap
+        currentIntervalEnd =
+            std::max(currentIntervalEnd, interval[kIntervalEndIndex]);
+      } else {
+        // Not overlap
+        mergedIntervals.push_back({currentIntervalBegin, currentIntervalEnd});
+        currentIntervalBegin = interval[kIntervalBeginIndex];
+        currentIntervalEnd = interval[kIntervalEndIndex];
       }
     }
 
-    return freeDays - 1;
+    mergedIntervals.push_back({currentIntervalBegin, currentIntervalEnd});
+
+    return mergedIntervals;
   }
 };
 // @lc code=end
 
 int main() {
   Solution sol;
-  sol.countDays(5, {{2, 4}, {1, 3}});
+  std::vector<std::vector<int>> meetings = {{1, 3}, {2, 4}};
+  sol.countDays(5, meetings);
 }
