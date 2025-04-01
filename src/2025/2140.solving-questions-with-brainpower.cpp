21badaa2a9e8bff8c8533d4ae6aa6b3b23fe7baf
Soulike <git_anonymous@foxmail.com>
2025-04-01 08:25:28 +0800

    2140

--- src/2025/2140.solving-questions-with-brainpower.cpp
+++ src/2025/2140.solving-questions-with-brainpower.cpp
@@ -10,42 +10,24 @@
 class Solution {
  public:
   long long mostPoints(const std::vector<std::vector<int>>& questions) {
-    return MostPointsFromIndex(questions, 0);
+    std::vector<long long> dp(questions.size());
+    dp[questions.size() - 1] = questions[questions.size() - 1][kPointsIndex];
+
+    for (int i = static_cast<int>(questions.size()) - 2; i >= 0; i--) {
+      const int next_question_index_if_solve =
+          i + questions[i][kBrainPowerIndex] + 1;
+      dp[i] = std::max(questions[i][kPointsIndex] +
+                           (next_question_index_if_solve < questions.size()
+                                ? dp[next_question_index_if_solve]
+                                : 0),
+                       dp[i + 1]);
+    }
+
+    return dp[0];
   }
 
  private:
   constexpr static int kPointsIndex = 0;
   constexpr static int kBrainPowerIndex = 1;
-  constexpr static long long kMemoNotExist = -1;
-
-  long long MostPointsFromIndex(const std::vector<std::vector<int>>& questions,
-                                const int index) {
-    std::vector<long long> memo(questions.size(), kMemoNotExist);
-    return MostPointsFromIndexImpl(questions, index, memo);
-  }
-
-  long long MostPointsFromIndexImpl(
-      const std::vector<std::vector<int>>& questions,
-      const int index,
-      std::vector<long long>& memo) {
-    if (index >= questions.size()) {
-      return 0;
-    }
-
-    if (memo[index] != kMemoNotExist) {
-      return memo[index];
-    }
-
-    const long long solve_current_question_points =
-        questions[index][kPointsIndex] +
-        MostPointsFromIndexImpl(
-            questions, index + questions[index][kBrainPowerIndex] + 1, memo);
-    const long long skip_current_question_points =
-        MostPointsFromIndexImpl(questions, index + 1, memo);
-    const long long max_points =
-        std::max(solve_current_question_points, skip_current_question_points);
-    memo[index] = max_points;
-    return max_points;
-  }
 };
 // @lc code=end
