1b032eb262f222a2ff804ed8f94f50f891ffb698
Soulike <git_anonymous@foxmail.com>
2025-04-02 08:39:55 +0800

    2873

--- src/2025/2873.maximum-value-of-an-ordered-triplet-i.cpp
+++ src/2025/2873.maximum-value-of-an-ordered-triplet-i.cpp
@@ -11,15 +11,18 @@ class Solution {
  public:
   long long maximumTripletValue(const std::vector<int>& nums) {
     long long result = 0;
-    long long max_value_before_j = nums[0];
-    for (int j = 1; j < nums.size(); j++) {
-      max_value_before_j =
-          std::max(max_value_before_j, static_cast<long long>(nums[j - 1]));
-      for (int k = j + 1; k < nums.size(); k++) {
-        result = std::max(
-            result,
-            static_cast<long long>(max_value_before_j - nums[j]) * nums[k]);
-      }
+    // Maximum nums[i] before k
+    long long i_max_value = 0;
+    // Maximum nums[i] - nums[j] before k
+    long long i_j_max_delta = 0;
+
+    for (int k = 0; k < nums.size(); k++) {
+      result = std::max(result, i_j_max_delta * nums[k]);
+
+      // Treat k as j, get maximum nums[i] - nums[j] so far.
+      i_j_max_delta = std::max(i_j_max_delta, i_max_value - nums[k]);
+      // Treat k as i, get maximum nums[i] so far.
+      i_max_value = std::max(i_max_value, static_cast<long long>(nums[k]));
     }
 
     return result;
