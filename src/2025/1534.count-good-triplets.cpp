0ab241329c9b658a354ded60a6bc0383008b659a
Soulike <git_anonymous@foxmail.com>
2025-04-14 08:28:50 +0800

    1534

--- src/2025/1534.count-good-triplets.cpp
+++ src/2025/1534.count-good-triplets.cpp
@@ -17,9 +17,12 @@ class Solution {
     int count = 0;
     for (int i = 0; i < arr.size(); i++) {
       for (int j = i + 1; j < arr.size(); j++) {
+        if (std::abs(arr[i] - arr[j]) > a) {
+          continue;
+        }
+
         for (int k = j + 1; k < arr.size(); k++) {
-          if (std::abs(arr[i] - arr[j]) <= a &&
-              std::abs(arr[j] - arr[k]) <= b &&
+          if (std::abs(arr[j] - arr[k]) <= b &&
               std::abs(arr[i] - arr[k]) <= c) {
             count++;
           }
