32e63799bd07f0e3e8dddd433d52b73e059d70fb
Soulike <git_anonymous@foxmail.com>
2025-06-02 09:22:23 +0800

    135

--- src/2025/135.candy.cpp
+++ src/2025/135.candy.cpp
@@ -11,24 +11,40 @@
 class Solution {
  public:
   int candy(const std::vector<int>& ratings) {
-    const int kChildrenCount = static_cast<int>(ratings.size());
+    const int kRatingsCount = static_cast<int>(ratings.size());
 
-    std::vector<int> candy_counts(kChildrenCount, 1);
+    // Length of increasing sequence. Not including valley.
+    int increasing_sequence_length = 0;
+    // Length of decreasing sequence. Not including peak.
+    int decreasing_sequence_length = 0;
+    int last_increasing_sequence_length = 0;
+    int sum = 1;  // 1 candy for ratings[0]
 
-    for (int i = 1; i < kChildrenCount; i++) {
+    for (int i = 1; i < kRatingsCount; i++) {
       if (ratings[i - 1] < ratings[i]) {
-        candy_counts[i] = candy_counts[i - 1] + 1;
+        increasing_sequence_length++;
+        last_increasing_sequence_length = increasing_sequence_length;
+        decreasing_sequence_length = 0;
+        sum += 1 + increasing_sequence_length;
+      } else if (ratings[i - 1] == ratings[i]) {
+        // Treat ratings[i] as valley with last increasing sequence length 0.
+        increasing_sequence_length = 0;
+        decreasing_sequence_length = 0;
+        last_increasing_sequence_length = 0;
+        sum++;
+      } else {
+        increasing_sequence_length = 0;
+        decreasing_sequence_length++;
+        sum += decreasing_sequence_length;
+
+        // If the length of decreasing sequence is longer than previous
+        // increasing sequence, we need to increase the value of peak to ensure
+        // valley get at least 1.
+        sum += (decreasing_sequence_length > last_increasing_sequence_length);
       }
     }
 
-    for (int i = kChildrenCount - 1; i >= 1; i--) {
-      if (ratings[i - 1] > ratings[i]) {
-        candy_counts[i - 1] =
-            std::max(candy_counts[i - 1], candy_counts[i] + 1);
-      }
-    }
-
-    return std::accumulate(candy_counts.cbegin(), candy_counts.cend(), 0);
+    return sum;
   }
 };
 // @lc code=end
