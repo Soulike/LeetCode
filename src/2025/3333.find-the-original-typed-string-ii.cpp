dd560d1ff71812274fd66a2218e938ad32f1ca44
Soulike <git_anonymous@foxmail.com>
2025-07-02 15:56:57 +0800

    3333

--- src/2025/3333.find-the-original-typed-string-ii.cpp
+++ src/2025/3333.find-the-original-typed-string-ii.cpp
@@ -7,7 +7,6 @@
 #include <algorithm>
 #include <numeric>
 #include <string>
-#include <unordered_map>
 #include <vector>
 
 // @lc code=start
@@ -21,43 +20,45 @@ class Solution {
       return 1;
     }
 
-    std::vector<int> letter_repeats;
-    letter_repeats.push_back(1);
+    std::vector<int> letter_segments_length;
+    letter_segments_length.push_back(1);
     for (int i = 1; i < word.size(); i++) {
       if (word[i - 1] != word[i]) {
-        letter_repeats.push_back(1);
+        letter_segments_length.push_back(1);
       } else {
-        letter_repeats.back()++;
+        letter_segments_length.back()++;
       }
     }
 
-    const int all_possible_string_count =
-        std::accumulate(letter_repeats.cbegin(), letter_repeats.cend(), 1,
-                        [](const std::int64_t prev, const int curr) {
-                          return (prev * curr) % kMod;
-                        });
-    if (letter_repeats.size() >= k) {
+    const int all_possible_string_count = std::accumulate(
+        letter_segments_length.cbegin(), letter_segments_length.cend(), 1,
+        [](const std::int64_t prev, const int curr) {
+          return (prev * curr) % kMod;
+        });
+
+    if (letter_segments_length.size() >= k) {
       return all_possible_string_count;
     }
 
     /**
-     * dp[i][j] Ways of using letter_repeats[0..i-1] to form a string of length
-     * j
+     * dp[i][j] Ways of using letter_segments_length[0..i-1] to form a string of
+     * length j
      *
      * base case
      * dp[0][j] = 0
      * dp[0][0] = 1
      *
      * formula
-     * dp[i][j] = sum(for l from 1 to letter_repeats[i-1], dp[i-1][j-l])
+     * dp[i][j] = sum(for l from 1 to letter_segments_length[i-1], dp[i-1][j-l])
+     * Use prefix sum of dp[i-1] to optimize
      *
      * result
-     * sum of dp[letter_repeats.size()][0...k-1]
+     * sum of dp[letter_segments_length.size()][0...k-1]
      */
     std::vector<std::vector<int>> dp(2, std::vector<int>(k, 0));
     dp[0][0] = 1;
 
-    for (int i = 1; i <= letter_repeats.size(); i++) {
+    for (int i = 1; i <= letter_segments_length.size(); i++) {
       std::vector<int> prefix_sum(k + 1, 0);  // prefix sum of dp[i-1]
       prefix_sum[0] = 0;
       for (int j = 0; j < k; j++) {
@@ -67,18 +68,19 @@ class Solution {
       std::ranges::fill(dp[i % 2], 0);
 
       for (int j = 1; j < k; j++) {
-        const int max_l = std::min(static_cast<int>(letter_repeats[i - 1]), j);
+        const int max_l =
+            std::min(static_cast<int>(letter_segments_length[i - 1]), j);
         dp[i % 2][j] = (prefix_sum[j] - prefix_sum[j - max_l] + kMod) % kMod;
       }
     }
 
-    const int result = (kMod + all_possible_string_count -
-                        std::accumulate(dp[letter_repeats.size() % 2].cbegin(),
-                                        dp[letter_repeats.size() % 2].cend(), 0,
-                                        [](const int prev, const int curr) {
-                                          return (prev + curr) % kMod;
-                                        })) %
-                       kMod;
+    const int invalid_string_count = std::accumulate(
+        dp[letter_segments_length.size() % 2].cbegin(),
+        dp[letter_segments_length.size() % 2].cend(), 0,
+        [](const int prev, const int curr) { return (prev + curr) % kMod; });
+
+    const int result =
+        (kMod + all_possible_string_count - invalid_string_count) % kMod;
     return result;
   }
 
