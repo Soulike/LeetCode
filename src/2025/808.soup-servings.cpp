4be01456412746945c12d5dad10d3f9f57a14951
Soulike <git_anonymous@foxmail.com>
2025-08-09 10:33:23 +0800

    808

--- src/2025/808.soup-servings.cpp
+++ src/2025/808.soup-servings.cpp
@@ -6,50 +6,53 @@
 
 #include <algorithm>
 #include <cmath>
+#include <functional>
 #include <vector>
 
 // @lc code=start
 class Solution {
  public:
   double soupServings(const int n) {
-    const int total_serve_count = static_cast<int>(std::ceil(n / 25.0));
-
-    if (total_serve_count > 5000) {
+    if (n > 5000) {
       return 1.0;
     }
 
-    /**
-     * dp[i][j] - The result for the problem when A has i serves and B has j
-     * serves. 1 serve = 25ml soup.
-     *
-     * base cases
-     * dp[0][j] = 1.0
-     * dp[i][0] = 0.0
-     * dp[0][0] = 0.5
-     *
-     * formula
-     * dp[i][j] = 0.25 * (dp[i-4][j] + dp[i-3][j-1] + dp[i-2][j-2] +
-     * dp[i-1][j-3])
-     */
-    std::vector<std::vector<double>> dp(
-        4, std::vector<double>(total_serve_count + 1, 0.0));
-    std::ranges::fill(dp[0], 1.0);
-    dp[0][0] = 0.5;
+    const int total_serve_count = static_cast<int>(std::ceil(n / 25.0));
 
-    for (int i = 1; i <= total_serve_count; i++) {
-      dp[(i - 1) % 4][0] = 0;
-      if (i == 1) [[unlikely]] {
-        dp[0][0] = 0.5;
-      }
-      for (int j = 1; j <= total_serve_count; j++) {
-        dp[i % 4][j] = 0.25 * (dp[std::max(0, i - 4) % 4][j] +
-                               dp[std::max(0, i - 3) % 4][j - 1] +
-                               dp[std::max(0, i - 2) % 4][std::max(0, j - 2)] +
-                               dp[std::max(0, i - 1) % 4][std::max(0, j - 3)]);
-      }
+    return GetProbability(total_serve_count);
+  }
+
+ private:
+  static double GetProbability(const int serve_count) {
+    std::vector<std::vector<double>> probabilities(
+        serve_count + 1, std::vector<double>(serve_count + 1, -1));
+    return GetProbabilityHelper(serve_count, serve_count, probabilities);
+  }
+
+  static double GetProbabilityHelper(const int a_serve_count,
+                                     const int b_serve_count,
+                                     std::vector<std::vector<double>>& memo) {
+    if (a_serve_count <= 0 && b_serve_count <= 0) {
+      return 0.5;
+    }
+    if (a_serve_count <= 0) {
+      return 1.0;
+    }
+    if (b_serve_count <= 0) {
+      return 0.0;
+    }
+    if (memo[a_serve_count][b_serve_count] != -1) {
+      return memo[a_serve_count][b_serve_count];
     }
 
-    return dp[total_serve_count % 4][total_serve_count];
+    const double result =
+        0.25 *
+        (GetProbabilityHelper(a_serve_count - 4, b_serve_count, memo) +
+         GetProbabilityHelper(a_serve_count - 3, b_serve_count - 1, memo) +
+         GetProbabilityHelper(a_serve_count - 2, b_serve_count - 2, memo) +
+         GetProbabilityHelper(a_serve_count - 1, b_serve_count - 3, memo));
+    memo[a_serve_count][b_serve_count] = result;
+    return result;
   }
 };
 // @lc code=end
