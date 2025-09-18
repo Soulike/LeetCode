139ec367c11a51f1261646e7a8a9c5c9fd7971bb
Soulike <git_anonymous@foxmail.com>
2025-09-18 21:42:19 +0800

    3408

---
+++ src/2025/3408.design-task-manager.cpp
@@ -0,0 +1,110 @@
+/*
+ * @lc app=leetcode id=3408 lang=cpp
+ *
+ * [3408] Design Task Manager
+ */
+
+#include <queue>
+#include <unordered_map>
+#include <vector>
+
+// @lc code=start
+class TaskManager {
+ private:
+  using UserId = int;
+  using TaskId = int;
+  using Priority = int;
+
+ public:
+  explicit TaskManager(const std::vector<std::vector<int>>& tasks)
+      : task_max_heap_(kTaskHeapComp) {
+    for (const std::vector<int>& task : tasks) {
+      const UserId user_id = task[0];
+      const TaskId task_id = task[1];
+      const Priority priority = task[2];
+      add(user_id, task_id, priority);
+    }
+  }
+
+  void add(const UserId userId, const TaskId taskId, const Priority priority) {
+    task_max_heap_.emplace(userId, taskId, priority);
+    task_id_to_task_info_[taskId].priority = priority;
+    task_id_to_task_info_[taskId].user_id = userId;
+  }
+
+  void edit(const TaskId taskId, const Priority newPriority) {
+    const UserId user_id = task_id_to_task_info_.at(taskId).user_id;
+    task_max_heap_.emplace(user_id, taskId, newPriority);
+    task_id_to_task_info_.at(taskId).priority = newPriority;
+  }
+
+  void rmv(const TaskId taskId) {
+    if (!task_id_to_task_info_.contains(taskId)) {
+      return;
+    }
+    task_id_to_task_info_.at(taskId).user_id = kInvalidUserId;
+    task_id_to_task_info_.at(taskId).priority = kInvalidPriority;
+  }
+
+  UserId execTop() {
+    if (task_max_heap_.empty()) {
+      return -1;
+    }
+    while (!task_max_heap_.empty() && !IsTaskValid(task_max_heap_.top())) {
+      task_max_heap_.pop();
+    }
+    if (task_max_heap_.empty()) {
+      return -1;
+    }
+    const Task top_task = task_max_heap_.top();
+    task_max_heap_.pop();
+
+    task_id_to_task_info_.erase(top_task.task_id);
+    return top_task.user_id;
+  }
+
+ private:
+  struct Task {
+    UserId user_id;
+    TaskId task_id;
+    Priority priority;
+  };
+
+  struct TaskInfo {
+    UserId user_id;
+    Priority priority;
+  };
+
+  static constexpr Priority kInvalidPriority = -1;
+  static constexpr UserId kInvalidUserId = -1;
+
+  static constexpr auto kTaskHeapComp = [](const Task& task1,
+                                           const Task& task2) {
+    if (task1.priority != task2.priority) {
+      return task1.priority < task2.priority;
+    }
+    return task1.task_id < task2.task_id;
+  };
+
+  bool IsTaskValid(const Task& task) const {
+    if (!task_id_to_task_info_.contains(task.task_id)) {
+      return false;
+    }
+    return task_id_to_task_info_.at(task.task_id).priority == task.priority &&
+           task_id_to_task_info_.at(task.task_id).user_id == task.user_id;
+  }
+
+  std::unordered_map<TaskId, TaskInfo> task_id_to_task_info_;
+  std::priority_queue<Task, std::vector<Task>, decltype(kTaskHeapComp)>
+      task_max_heap_;
+};
+
+/**
+ * Your TaskManager object will be instantiated and called as such:
+ * TaskManager* obj = new TaskManager(tasks);
+ * obj->add(userId,taskId,priority);
+ * obj->edit(taskId,newPriority);
+ * obj->rmv(taskId);
+ * int param_4 = obj->execTop();
+ */
+// @lc code=end
