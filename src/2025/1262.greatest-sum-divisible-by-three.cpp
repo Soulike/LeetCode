09ccc20e8cc4be3e32557d3be2f52fc8458ae00f
Soulike <git_anonymous@foxmail.com>
2025-11-23 11:01:36 +0800

    1262

--- src/2025/1262.greatest-sum-divisible-by-three.cpp
+++ src/2025/1262.greatest-sum-divisible-by-three.cpp
@@ -20,9 +20,13 @@ class Solution {
     for (size_t i = 1; i < nums.size(); i++) {
       const int remainder = nums[i] % 3;
       for (size_t r = 0; r < 3; r++) {
-        dp[i % 2][r] = dp[(i - 1) % 2][(3 + r - remainder) % 3] + nums[i];
-        if (dp[i % 2][r] % 3 != r) {
-          dp[i % 2][r] = 0;
+        if (
+            // If the prev value is 0, then it is an invalid value
+            dp[(i - 1) % 2][(3 + r - remainder) % 3] > 0 ||
+            // If the prev value is invalid, only when nums[i] % 3 == r we can
+            // set it in dp.
+            nums[i] % 3 == r) {
+          dp[i % 2][r] = dp[(i - 1) % 2][(3 + r - remainder) % 3] + nums[i];
         }
         dp[i % 2][r] = std::max(dp[i % 2][r], dp[(i - 1) % 2][r]);
       }
