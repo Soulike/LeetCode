6ef8cb16a79b5cfdaf72c424721bd43bf1b126fc
Soulike <git_anonymous@foxmail.com>
2025-11-23 10:47:40 +0800

    1262

---
+++ src/2025/1262.greatest-sum-divisible-by-three.cpp
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=1262 lang=cpp
+ *
+ * [1262] Greatest Sum Divisible by Three
+ */
+
+#include <array>
+#include <vector>
+
+// @lc code=start
+class Solution {
+ public:
+  int maxSumDivThree(const std::vector<int>& nums) {
+    /**
+     * dp[i][r] - ends with nums[i], the largest sum with remainder r mod 3
+     */
+    std::array<std::array<int, 3>, 2> dp = {};
+    dp[0][nums[0] % 3] = nums[0];
+
+    for (size_t i = 1; i < nums.size(); i++) {
+      const int remainder = nums[i] % 3;
+      for (size_t r = 0; r < 3; r++) {
+        dp[i % 2][r] = dp[(i - 1) % 2][(3 + r - remainder) % 3] + nums[i];
+        if (dp[i % 2][r] % 3 != r) {
+          dp[i % 2][r] = 0;
+        }
+        dp[i % 2][r] = std::max(dp[i % 2][r], dp[(i - 1) % 2][r]);
+      }
+    }
+
+    return dp[(nums.size() - 1) % 2][0];
+  }
+};
+// @lc code=end
+
+int main() {
+  Solution sol;
+  sol.maxSumDivThree({1, 2, 3, 4, 4});
+}
