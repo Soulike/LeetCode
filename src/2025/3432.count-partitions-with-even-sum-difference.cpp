441cc833d5f1ae93e2f10fe0e7964ad47462d322
Soulike <git_anonymous@foxmail.com>
2025-12-05 08:21:29 +0800

    3432

--- src/2025/3432.count-partitions-with-even-sum-difference.cpp
+++ src/2025/3432.count-partitions-with-even-sum-difference.cpp
@@ -12,15 +12,7 @@ class Solution {
  public:
   int countPartitions(const std::vector<int>& nums) {
     const int sum = std::accumulate(nums.cbegin(), nums.cend(), 0);
-    int left_sum = 0;
-    int result = 0;
-    for (int i = 0; i < nums.size() - 1; i++) {
-      left_sum += nums[i];
-      const int right_sum = sum - left_sum;
-      const int diff = std::abs(right_sum - left_sum);
-      result += diff % 2 == 0;
-    }
-    return result;
+    return sum % 2 == 0 ? nums.size() - 1 : 0;
   }
 };
 // @lc code=end
