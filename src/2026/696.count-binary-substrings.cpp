3d92bf93c285aa0df12cd191b688138f633a22a0
Soulike <git_anonymous@foxmail.com>
2026-02-19 14:38:56 +0800

    696

--- src/2026/696.count-binary-substrings.cpp
+++ src/2026/696.count-binary-substrings.cpp
@@ -4,77 +4,36 @@
  * [696] Count Binary Substrings
  */
 
-#include <memory>
-#include <stack>
 #include <string>
 
 // @lc code=start
 class Solution {
  public:
   int countBinarySubstrings(const std::string& s) {
-    const int zero_leading_substring_count =
-        CountZeroLeadingBinarySubstrings(s);
-    const int one_leading_substring_count = CountOneLeadingBinarySubstrings(s);
-    return zero_leading_substring_count + one_leading_substring_count;
-  }
+    char prev_bit = 0;
+    int prev_bit_count = 0;
+    char current_bit = 0;
+    int current_bit_count = 0;
 
- private:
-  // Count 000...111
-  static int CountZeroLeadingBinarySubstrings(const std::string& s) {
-    auto stack = std::make_unique<std::stack<char>>();
-    int current_substring_count = 0;
-    int total_substring_count = 0;
+    int binary_substring_count = 0;
 
     for (const char c : s) {
-      if (c == '0') {
-        if (current_substring_count > 0) {
-          // Substrings are formed. Starting new substrings.
-          stack = std::make_unique<std::stack<char>>();
-          total_substring_count += current_substring_count;
-          current_substring_count = 0;
-        }
-        stack->push(c);
+      if (c != current_bit) {
+        binary_substring_count += std::min(prev_bit_count, current_bit_count);
+
+        prev_bit = current_bit;
+        current_bit = c;
+
+        prev_bit_count = current_bit_count;
+        current_bit_count = 1;
       } else {
-        // c == '1'
-        if (stack->empty()) {
-          continue;
-        }
-        stack->pop();
-        current_substring_count++;
+        current_bit_count++;
       }
     }
 
-    total_substring_count += current_substring_count;
-    return total_substring_count;
-  }
+    binary_substring_count += std::min(prev_bit_count, current_bit_count);
 
-  // Count 111...000
-  static int CountOneLeadingBinarySubstrings(const std::string& s) {
-    auto stack = std::make_unique<std::stack<char>>();
-    int current_substring_count = 0;
-    int total_substring_count = 0;
-
-    for (const char c : s) {
-      if (c == '1') {
-        if (current_substring_count > 0) {
-          // Substrings are formed. Starting new substrings.
-          stack = std::make_unique<std::stack<char>>();
-          total_substring_count += current_substring_count;
-          current_substring_count = 0;
-        }
-        stack->push(c);
-      } else {
-        // c == '0'
-        if (stack->empty()) {
-          continue;
-        }
-        stack->pop();
-        current_substring_count++;
-      }
-    }
-
-    total_substring_count += current_substring_count;
-    return total_substring_count;
+    return binary_substring_count;
   }
 };
 // @lc code=end
